agorakit/agorakit

commit ef1572a689f72576efc1488aa672a7b585053946

actions will soon land

app/File.php
@@ -12,7 +12,7 @@ class File extends Model
 {
   use ValidatingTrait;
 
-  
+
   protected $rules = [
     'path' => 'required',
     'user_id' => 'required',
@@ -36,25 +36,5 @@ class File extends Model
   public function group()
   {
     return $this->belongsTo('App\Group');
-  }
-
-  public function setFileContent($file_content, $filename, $extension, $mime)
-  {
-    // path would be in the form storage/app/group/{group_id}/{file_id}.jpg for a jpeg file
-    $path = 'groups/'.$this->group_id.'/'.$this->id.'.'.$extension;
-
-    $this->path = $path;
-    $this->original_filename = $filename; // we never know it might be useful
-    $this->original_extension = $extension;  // we never know it might be useful
-    $this->mime = $mime;
-
-    return (Storage::put($path,  $file_content));
-  }
-
-  public function getFileContent()
-  {
-    $file = Storage::disk('local')->get($this->path);
-
-    return (new Response($file, 200, ['Content-Type', $entry->mime]));
   }
 }

config/app.php
@@ -151,10 +151,10 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         // additional providers
-        Illuminate\Html\HtmlServiceProvider::class,
         Potsky\LaravelLocalizationHelpers\LaravelLocalizationHelpersServiceProviderLaravel5::class,
-
-        Intervention\Image\ImageServiceProvider::class
+        Intervention\Image\ImageServiceProvider::class,
+        MaddHatter\LaravelFullcalendar\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -203,10 +203,14 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
-        'Form'	    => Illuminate\Html\FormFacade::class,
-        'HTML'      => Illuminate\Html\HtmlFacade::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Calendar' => MaddHatter\LaravelFullcalendar\Facades\Calendar::class
+
 
     ],
 

resources/views/files/index.blade.php
@@ -18,7 +18,7 @@
 
 <div class="container">
 
-
+@if ($upload_allowed)
   {!! Form::open(['url' => action('FileController@create', $group->id), 'class' => 'dropzone', 'files'=>true, 'id'=>'real-dropzone']) !!}
 
   <div class="fallback">
@@ -27,11 +27,11 @@
   </div>
 
   {!! Form::close() !!}
-
+@endif
 
 
   <table class="table table-hover">
-    @foreach( $files as $file )
+    @forelse( $files as $file )
     <tr>
 
       <td>
@@ -56,7 +56,10 @@
         {{ $file->created_at->diffForHumans() }}
       </td>
     </tr>
-    @endforeach
+    @empty
+    No files yet
+
+    @endforelse
   </table>
 
   {!! $files->render() !!}

resources/views/actions/index.blade.php
@@ -1,0 +1,74 @@
+@extends('app')
+
+
+@section('head')
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+@stop
+
+@section('content')
+
+@include('partials.grouptab')
+
+<div class="container">
+  <h2>Agenda and actions of this group <a class="btn btn-primary btn-xs" href="{{ action('ActionController@create', $group->id ) }}">New action</a></h2>
+
+
+
+  {!! $calendar->calendar() !!}
+    {!! $calendar->script() !!}
+
+
+
+  <table class="table table-hover">
+    <tr>
+      <th>Name</th>
+      <th>Starts</th>
+      <th>Stops</th>
+      <th>Location</th>
+      <th>Author</th>
+      <th>Posted</th>
+    </tr>
+    @foreach( $actions as $action )
+    <tr>
+      <td>
+        <a href="{{ action('ActionController@show', [$group->id, $action->id]) }}">{{ $action->name }}</a>
+      </td>
+
+      <td>
+        <a href="{{ action('ActionController@show', [$group->id, $action->id]) }}">{{ $action->start }}</a>
+      </td>
+
+      <td>
+        <a href="{{ action('ActionController@show', [$group->id, $action->id]) }}">{{ $action->stop }}</a>
+      </td>
+
+      <td>
+        <a href="{{ action('ActionController@show', [$group->id, $action->id]) }}">{{ $action->location }}</a>
+      </td>
+
+      <td>
+        @unless (is_null ($action->user))
+        <a href="{{ action('UserController@show', $action->user->id) }}">{{ $action->user->name }}</a>
+        @endunless
+      </td>
+
+      <td>
+        <a href="{{ action('ActionController@show', [$group->id, $action->id]) }}">{{ $action->updated_at->diffForHumans() }}</a>
+      </td>
+
+    </tr>
+    @endforeach
+  </table>
+
+
+
+
+</div>
+
+
+
+
+@endsection

resources/views/groups/index.blade.php
@@ -16,7 +16,7 @@
       <div class="col-sm-4 col-md-3">
         <div class="thumbnail">
           <a href="{{ action('GroupController@show', $group->id) }}">
-          <img src="http://lorempixel.com/242/150/cats/{{ $group->id }}"/>
+          <img src="http://lorempixel.com/242/150/?id={{$group->id}}"/>
         </a>
           <div class="caption">
             <h4><a href="{{ action('GroupController@show', $group->id) }}">{{ $group->name }}</a></h4>

app/Action.php
@@ -5,23 +5,73 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Action extends Model {
+class Action extends Model implements \MaddHatter\LaravelFullcalendar\Event
+{
+    protected $table = 'actions';
+    public $timestamps = true;
 
-	protected $table = 'actions';
-	public $timestamps = true;
+    use SoftDeletes;
 
-	use SoftDeletes;
+    protected $dates = ['deleted_at', 'start', 'stop'];
 
-	protected $dates = ['deleted_at'];
+		/**
+		* Get the event's id number
+		*
+		* @return int
+		*/
+	 public function getId() {
+			 return $this->id;
+	 }
 
-	public function group()
-	{
-		return $this->belongsTo('Group');
-	}
+	 /**
+		* Get the event's title
+		*
+		* @return string
+		*/
+	 public function getTitle()
+	 {
+			 return $this->name;
+	 }
 
-	public function votes()
-	{
-		return $this->morphMany('Vote', 'votable');
-	}
+	 /**
+		* Is it an all day event?
+		*
+		* @return bool
+		*/
+	 public function isAllDay()
+	 {
+		 return false;
+			 //return (bool)$this->all_day;
+	 }
 
+	 /**
+		* Get the start time
+		*
+		* @return DateTime
+		*/
+	 public function getStart()
+	 {
+			 return $this->start;
+	 }
+
+	 /**
+		* Get the end time
+		*
+		* @return DateTime
+		*/
+	 public function getEnd()
+	 {
+			 return $this->stop;
+	 }
+
+
+    public function group()
+    {
+        return $this->belongsTo('Group');
+    }
+
+    public function votes()
+    {
+        return $this->morphMany('Vote', 'votable');
+    }
 }

app/Discussion.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Watson\Validating\ValidatingTrait;
+use Auth;
 
 //use Comment;
 
@@ -45,6 +46,13 @@ class Discussion extends Model
       // The discussion list should load a count of unread replies instead, in a single query.
       // because else we do a query for eavery discussion to know the count. It smells
       // and we cannot order by unread discussions first...
+
+
+
+      if (\Auth::guest())
+      {
+        return 0;
+      }
       if (is_null($this->unreadcounter))
       {
 

app/Group.php
@@ -41,6 +41,17 @@ class Group extends Model
       return $this->hasMany('App\Discussion');
     }
 
+
+    /**
+     * Returns all the actions belonging to this group
+     *
+     */
+    public function actions()
+    {
+      return $this->hasMany('App\Action');
+    }
+
+
     public function files()
     {
       return $this->hasMany('App\File');
@@ -53,6 +64,8 @@ class Group extends Model
     */
     public function isMember(User $user = null)
     {
+
+
       if (is_null ($user))
       {
         $user = $user = Auth::user();
@@ -63,7 +76,7 @@ class Group extends Model
       if ($user)
       {
         $membership = \App\Membership::where('user_id', $user->id)->where('group_id', $this->id)->first();
-        
+
         if ($membership && $membership->membership > 1)
         {
           return true;

commit d3ce3939589551a43a980af6f5d7c9747415ded8

need to investigate. Files are broken since ef1572a689f72576efc1488aa672a7b585053946

app/File.php
@@ -37,4 +37,5 @@ class File extends Model
   {
     return $this->belongsTo('App\Group');
   }
+
 }

config/app.php
@@ -110,10 +110,8 @@ return [
 
     'providers' => [
 
-
         // seems like it must be declared first
         Barryvdh\Debugbar\ServiceProvider::class,
-
 
         /*
          * Laravel Framework Service Providers...
@@ -171,47 +169,46 @@ return [
 
     'aliases' => [
 
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Bus'       => Illuminate\Support\Facades\Bus::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Gate'      => Illuminate\Support\Facades\Gate::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Input'     => Illuminate\Support\Facades\Input::class,
+        'App' => Illuminate\Support\Facades\App::class,
+        'Artisan' => Illuminate\Support\Facades\Artisan::class,
+        'Auth' => Illuminate\Support\Facades\Auth::class,
+        'Blade' => Illuminate\Support\Facades\Blade::class,
+        'Bus' => Illuminate\Support\Facades\Bus::class,
+        'Cache' => Illuminate\Support\Facades\Cache::class,
+        'Config' => Illuminate\Support\Facades\Config::class,
+        'Cookie' => Illuminate\Support\Facades\Cookie::class,
+        'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'DB' => Illuminate\Support\Facades\DB::class,
+        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+        'Event' => Illuminate\Support\Facades\Event::class,
+        'File' => Illuminate\Support\Facades\File::class,
+        'Gate' => Illuminate\Support\Facades\Gate::class,
+        'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
         'Inspiring' => Illuminate\Foundation\Inspiring::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
+        'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Log' => Illuminate\Support\Facades\Log::class,
+        'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Password' => Illuminate\Support\Facades\Password::class,
+        'Queue' => Illuminate\Support\Facades\Queue::class,
+        'Redirect' => Illuminate\Support\Facades\Redirect::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
+        'Request' => Illuminate\Support\Facades\Request::class,
+        'Response' => Illuminate\Support\Facades\Response::class,
+        'Route' => Illuminate\Support\Facades\Route::class,
+        'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Session' => Illuminate\Support\Facades\Session::class,
+        'Storage' => Illuminate\Support\Facades\Storage::class,
+        'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
+        'View' => Illuminate\Support\Facades\View::class,
 
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
         'Calendar' => MaddHatter\LaravelFullcalendar\Facades\Calendar::class,
-        'Date' => Jenssegers\Date\Date::class
-
+        'Date' => Jenssegers\Date\Date::class,
 
     ],
 

config/image.php
@@ -1,20 +1,0 @@
-<?php
-
-return array(
-
-    /*
-    |--------------------------------------------------------------------------
-    | Image Driver
-    |--------------------------------------------------------------------------
-    |
-    | Intervention Image supports "GD Library" and "Imagick" to process images
-    | internally. You may choose one of them according to your PHP
-    | configuration. By default PHP's "GD Library" implementation is used.
-    |
-    | Supported: "gd", "imagick"
-    |
-    */
-
-    'driver' => 'gd'
-
-);

resources/views/files/index.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('footer')
-{!! Html::script('/packages/dropzone/dropzone.js') !!}
+{!! Html::script('/packages/dropzone/dropzones.js') !!}
 {!! Html::script('/js/dropzone-config.js') !!}
 @stop
 