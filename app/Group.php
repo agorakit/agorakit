bd07156aa83677b778400738c35205c4cd58a535
philippejadin <philippe.jadin@gmail.com>
2015-11-20 00:24:52 +0100

    simplier colors

--- app/Group.php
+++ app/Group.php
@@ -46,49 +46,12 @@ class Group extends Model
     }
     else
     {
-
-      //$this->color = 'rgb(' . rand(0, 200) . ' , ' . rand(0, 200) . ' , ' . rand(0, 200) . ')';
-      $this->color = 'rgb('. $this->hsvToRGB(rand (0, 360), 60, 75) . ')';
+      $this->color = 'rgb(' . rand(0, 200) . ' , ' . rand(0, 200) . ' , ' . rand(0, 200) . ')';
       $this->save();
       return $this->color;
     }
   }
 
-  function hsvToRGB($iH, $iS, $iV) {
-        if($iH < 0)   $iH = 0;   // Hue:
-        if($iH > 360) $iH = 360; //   0-360
-        if($iS < 0)   $iS = 0;   // Saturation:
-        if($iS > 100) $iS = 100; //   0-100
-        if($iV < 0)   $iV = 0;   // Lightness:
-        if($iV > 100) $iV = 100; //   0-100
-        $dS = $iS/100.0; // Saturation: 0.0-1.0
-        $dV = $iV/100.0; // Lightness:  0.0-1.0
-        $dC = $dV*$dS;   // Chroma:     0.0-1.0
-        $dH = $iH/60.0;  // H-Prime:    0.0-6.0
-        $dT = $dH;       // Temp variable
-        while($dT >= 2.0) $dT -= 2.0; // php modulus does not work with float
-        $dX = $dC*(1-abs($dT-1));     // as used in the Wikipedia link
-        switch(floor($dH)) {
-            case 0:
-                $dR = $dC; $dG = $dX; $dB = 0.0; break;
-            case 1:
-                $dR = $dX; $dG = $dC; $dB = 0.0; break;
-            case 2:
-                $dR = 0.0; $dG = $dC; $dB = $dX; break;
-            case 3:
-                $dR = 0.0; $dG = $dX; $dB = $dC; break;
-            case 4:
-                $dR = $dX; $dG = 0.0; $dB = $dC; break;
-            case 5:
-                $dR = $dC; $dG = 0.0; $dB = $dX; break;
-            default:
-                $dR = 0.0; $dG = 0.0; $dB = 0.0; break;
-        }
-        $dM  = $dV - $dC;
-        $dR += $dM; $dG += $dM; $dB += $dM;
-        $dR *= 255; $dG *= 255; $dB *= 255;
-        return round($dR).",".round($dG).",".round($dB);
-    }
 
   /**
   * Returns all the users of this group
