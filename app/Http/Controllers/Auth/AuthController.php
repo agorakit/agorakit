865cd851e9a6cd48634ba5ab24db3a1805cb4fe8
philippejadin <philippe.jadin@gmail.com>
2016-01-25 12:00:08 +0100

    confirm email can be done even if you are already logged in

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
   */
   public function __construct()
   {
-    $this->middleware('guest', ['except' => 'logout']);
+    $this->middleware('guest', ['except' => ['logout', 'confirmEmail']]);
   }
 
   /**
