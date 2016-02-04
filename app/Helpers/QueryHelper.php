5e59b42f9b04de748221295c06906590391aa61d
philippejadin <philippe.jadin@gmail.com>
2016-02-04 21:12:51 +0100

    query helper bugfix, only auth user can see/query unread stuff

--- app/Helpers/QueryHelper.php
+++ app/Helpers/QueryHelper.php
@@ -39,47 +39,56 @@ class QueryHelper
   */
   public static function getUnreadDiscussionsCount()
   {
-    $total = DB::select('select sum(total_comments) as count from discussions where discussions.group_id in
-    (select id from groups where groups.id in
-    (select group_id from membership where user_id = ? and membership.membership >= 10)
-    )', [Auth::user()->id]);
+    if (Auth::check())
+    {
+      $total = DB::select('select sum(total_comments) as count from discussions where discussions.group_id in
+      (select id from groups where groups.id in
+      (select group_id from membership where user_id = ? and membership.membership >= 10)
+      )', [Auth::user()->id]);
 
-    $read = DB::select('select sum(read_comments) as count from user_read_discussion where user_id = ? and discussion_id in
-    (select id from discussions where group_id in
-    (select group_id from membership where user_id = ? and membership.membership >= 10)
-    )
-    ', [Auth::user()->id, Auth::user()->id]);
+      $read = DB::select('select sum(read_comments) as count from user_read_discussion where user_id = ? and discussion_id in
+      (select id from discussions where group_id in
+      (select group_id from membership where user_id = ? and membership.membership >= 10)
+      )
+      ', [Auth::user()->id, Auth::user()->id]);
 
-    $count = $total[0]->count - $read[0]->count;
-    if ($count < 0) {
-      $count = 0; // it might happens, it's bad, but not too bad so we fail silently
+      $count = $total[0]->count - $read[0]->count;
+      if ($count < 0) {
+        $count = 0; // it might happens, it's bad, but not too bad so we fail silently
+      }
+
+      return $count;
     }
-
-    return $count;
+    
+    return 0;
   }
 
   /**
   * Returns a list of the 50 latest unread discussions for the current user
   */
-  public static function  getUnreadDiscussions()
+  public static function getUnreadDiscussions()
   {
-    $discussions = DB::select('select * from
-    (
-    select *, (select read_comments from user_read_discussion where discussion_id = discussions.id and user_id = ?) as read_comments
-    from discussions where discussions.group_id in
-    (select id from groups where groups.id in
-    (select group_id from membership where user_id = ? and membership.membership >= 10)
-    )
-    ) as discussions
+    if (Auth::check())
+    {
+      $discussions = DB::select('select * from
+      (
+      select *, (select read_comments from user_read_discussion where discussion_id = discussions.id and user_id = ?) as read_comments
+      from discussions where discussions.group_id in
+      (select id from groups where groups.id in
+      (select group_id from membership where user_id = ? and membership.membership >= 10)
+      )
+      ) as discussions
 
-    where discussions.total_comments > read_comments or read_comments is null
-    order by updated_at desc
-    limit 0, 50
+      where discussions.total_comments > read_comments or read_comments is null
+      order by updated_at desc
+      limit 0, 50
 
-    ', [Auth::user()->id, Auth::user()->id]);
+      ', [Auth::user()->id, Auth::user()->id]);
 
 
-    return $discussions;
+      return $discussions;
+    }
+    return false;
 
   }
 
