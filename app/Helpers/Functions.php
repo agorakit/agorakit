03f48e770cbb3a75c16cd60ad2eaacf42c603a4d
Philippe Jadin <philippe.jadin@gmail.com>
2016-05-09 18:47:20 +0200

    vimeo embedin filtered content

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -8,7 +8,6 @@
 */
 function summary($text, $length = 200)
 {
-
     if (strlen(str_limit(strip_tags($text))) > $length)
     {
         $post = ' ...'; // append if it's longer than length
@@ -21,7 +20,7 @@ function summary($text, $length = 200)
 }
 
 /**
-* Filters the passed text to remove nasty html and turns urls to html links
+* Filters the passed text to remove nasty html and turns urls to html links and embeds youtube and vimeo links
 * @param  [type] $content [description]
 * @return [type]          [description]
 */
@@ -32,11 +31,21 @@ function filter($content)
 
     // convert links to embedable content TODO much more that that is needed
     // taken from http://stackoverflow.com/questions/19050890/find-youtube-link-in-php-string-and-convert-it-into-embed-code
+    // and from http://stackoverflow.com/questions/28563706/how-to-convert-vimeo-url-to-embed-without-letting-go-of-the-text-around-it
+
+    // 1. Youtube
     $content = preg_replace(
     "/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
     "<div class=\"embed-responsive embed-responsive-16by9\">
     <iframe src=\"//www.youtube.com/embed/$2\" allowfullscreen frameborder=\"0\" class=\"embed-responsive-item\">
     </iframe></div>", $content);
+
+    // 2. Vimeo
+    $content = preg_replace('#https?://(www\.)?vimeo\.com/(\d+)#',
+    '<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="videoFrame" src="//player.vimeo.com/video/$2" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="embed-responsive-item">
+    </iframe>
+    </div>',$content);
 
 
     // add links and returns
@@ -52,7 +61,7 @@ function safe_html($content)
 
 
 /**
-* returns the value of $name setting as stored in DB
+* returns the value of $name setting as stored in DB // TODO refactor
 */
 function setting($name, $default = false)
 {
