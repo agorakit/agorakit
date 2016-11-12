c2db8139142aac7ef04aff5d74791a79c58d89f1
Philippe Jadin <philippe.jadin@gmail.com>
2016-11-12 11:44:21 +0100

    translation corrections

--- resources/lang/de/documentation.php
+++ resources/lang/de/documentation.php
@@ -5,5 +5,4 @@ return [
         'my_groups' => 'Meine Gruppen',
     ],
     'intro'         => 'Unten finden Sie sind alle eingerichteten Gruppen. Sie können Gruppen Ihrer Wahl beitreten und folgen, nachdem Sie sich registriert haben.',
-//After you have to be registered - translated as if After you have registered
 ];
