6e358420ca53b98a127175599b40934db7d73fdb
Philippe Jadin <philippe.jadin@gmail.com>
2017-08-09 22:57:15 +0200

    remove big ugly add group button

--- resources/views/dashboard/groups.blade.php
+++ resources/views/dashboard/groups.blade.php
@@ -54,163 +54,111 @@
                     {{trans('group.no_group_yet')}}
                 @endforelse
 
-                <div class="col-xs-6 col-md-3">
-                    <div class="thumbnail group">
-                        <a href="{{ action('GroupController@create') }}">
-                            <div style="margin: auto; text-align: center; width: 100%; height:auto;"><i class="fa fa-plus-circle" style="font-size: 150px;" aria-hidden="true"></i></div>
-
-                        </a>
-                        <div class="caption">
-                            <h4><a href="{{ action('GroupController@create') }}">{{ trans('group.your_group_here') }}</a></h4>
-                            <p class="summary">{{ trans('group.create_a_group_intro') }}</p>
-                            <p>
-
-                                <a class="btn btn-primary" href="{{ action('GroupController@create') }}">{{ trans('group.create') }}</a>
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </div>
-    @endif
+
+        @endif
 
 
-    @if ($my_groups)
-        <h2>{{ trans('messages.my_groups') }}</h2>
-        <div class="row">
-            @forelse( $my_groups as $group )
-                <div class="col-xs-6 col-md-3">
-                    <div class="thumbnail group">
-                        <a href="{{ action('GroupController@show', $group->id) }}">
-                            <img src="{{action('GroupController@cover', $group->id)}}"/>
-                        </a>
-                        <div class="caption">
-                            <h4>
-                                <a href="{{ action('GroupController@show', $group->id) }}">{{ $group->name }}
-                                    @if ($group->isPublic())
-                                        <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
-                                    @else
-                                        <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
-                                    @endif
-
-                                </a>
-                            </h4>
-                            <p class="summary">{{ summary($group->body, 150) }}</p>
-                            <p>
-
-
-
-                                @unless ($group->isMember())
-                                    @can ('join', $group)
-                                        <a class="btn btn-primary" href="{{ action('MembershipController@join', $group->id) }}"><i class="fa fa-sign-in"></i>
-                                            {{ trans('group.join') }}
-                                        </a>
-                                    @endcan
-
-                                @endunless
-
-                                <a class="btn btn-primary" href="{{ action('GroupController@show', $group->id) }}">{{ trans('group.visit') }}</a>
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            @empty
-                {{trans('group.no_group_yet')}}
-            @endforelse
-
-            <div class="col-xs-6 col-md-3">
-                <div class="thumbnail group">
-                    <a href="{{ action('GroupController@create') }}">
-                        <div style="margin: auto; text-align: center; width: 100%; height:auto;"><i class="fa fa-plus-circle" style="font-size: 150px;" aria-hidden="true"></i></div>
-
-                    </a>
-                    <div class="caption">
-                        <h4><a href="{{ action('GroupController@create') }}">{{ trans('group.your_group_here') }}</a></h4>
-                        <p class="summary">{{ trans('group.create_a_group_intro') }}</p>
-                        <p>
-
-                            <a class="btn btn-primary" href="{{ action('GroupController@create') }}">{{ trans('group.create') }}</a>
-
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-
-
-
-@if ($other_groups)
-    <h2>{{ trans('messages.other_groups') }}</h2>
-    <div class="row">
-        @forelse( $other_groups as $group )
-            <div class="col-xs-6 col-md-3">
-                <div class="thumbnail group">
-                    <a href="{{ action('GroupController@show', $group->id) }}">
-                        <img src="{{action('GroupController@cover', $group->id)}}"/>
-                    </a>
-                    <div class="caption">
-                        <h4>
-                            <a href="{{ action('GroupController@show', $group->id) }}">{{ $group->name }}
-                                @if ($group->isPublic())
-                                    <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
-                                @else
-                                    <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
-                                @endif
-
+        @if ($my_groups)
+            <h2>{{ trans('messages.my_groups') }}</h2>
+            <div class="row">
+                @forelse( $my_groups as $group )
+                    <div class="col-xs-6 col-md-3">
+                        <div class="thumbnail group">
+                            <a href="{{ action('GroupController@show', $group->id) }}">
+                                <img src="{{action('GroupController@cover', $group->id)}}"/>
                             </a>
-                        </h4>
-                        <p class="summary">{{ summary($group->body, 150) }}</p>
-                        <p>
+                            <div class="caption">
+                                <h4>
+                                    <a href="{{ action('GroupController@show', $group->id) }}">{{ $group->name }}
+                                        @if ($group->isPublic())
+                                            <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
+                                        @else
+                                            <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
+                                        @endif
 
-
-
-                            @unless ($group->isMember())
-                                @can ('join', $group)
-                                    <a class="btn btn-primary" href="{{ action('MembershipController@join', $group->id) }}"><i class="fa fa-sign-in"></i>
-                                        {{ trans('group.join') }}
                                     </a>
-                                @endcan
+                                </h4>
+                                <p class="summary">{{ summary($group->body, 150) }}</p>
+                                <p>
 
-                            @endunless
 
-                            <a class="btn btn-primary" href="{{ action('GroupController@show', $group->id) }}">{{ trans('group.visit') }}</a>
 
-                        </p>
+                                    @unless ($group->isMember())
+                                        @can ('join', $group)
+                                            <a class="btn btn-primary" href="{{ action('MembershipController@join', $group->id) }}"><i class="fa fa-sign-in"></i>
+                                                {{ trans('group.join') }}
+                                            </a>
+                                        @endcan
+
+                                    @endunless
+
+                                    <a class="btn btn-primary" href="{{ action('GroupController@show', $group->id) }}">{{ trans('group.visit') }}</a>
+
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                @empty
+                    <a href="{{ action('DashboardController@index')}}">{{ trans('membership.not_subscribed_to_group_yet') }}</a>
+                @endforelse
             </div>
-        @empty
-            {{trans('group.no_group_yet')}}
-        @endforelse
 
-        <div class="col-xs-6 col-md-3">
-            <div class="thumbnail group">
-                <a href="{{ action('GroupController@create') }}">
-                    <div style="margin: auto; text-align: center; width: 100%; height:auto;"><i class="fa fa-plus-circle" style="font-size: 150px;" aria-hidden="true"></i></div>
+        @endif
 
-                </a>
-                <div class="caption">
-                    <h4><a href="{{ action('GroupController@create') }}">{{ trans('group.your_group_here') }}</a></h4>
-                    <p class="summary">{{ trans('group.create_a_group_intro') }}</p>
-                    <p>
 
-                        <a class="btn btn-primary" href="{{ action('GroupController@create') }}">{{ trans('group.create') }}</a>
 
-                    </p>
-                </div>
+        @if ($other_groups)
+            <h2>{{ trans('messages.other_groups') }}</h2>
+            <div class="row">
+                @forelse( $other_groups as $group )
+                    <div class="col-xs-6 col-md-3">
+                        <div class="thumbnail group">
+                            <a href="{{ action('GroupController@show', $group->id) }}">
+                                <img src="{{action('GroupController@cover', $group->id)}}"/>
+                            </a>
+                            <div class="caption">
+                                <h4>
+                                    <a href="{{ action('GroupController@show', $group->id) }}">{{ $group->name }}
+                                        @if ($group->isPublic())
+                                            <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
+                                        @else
+                                            <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
+                                        @endif
+
+                                    </a>
+                                </h4>
+                                <p class="summary">{{ summary($group->body, 150) }}</p>
+                                <p>
+
+
+
+                                    @unless ($group->isMember())
+                                        @can ('join', $group)
+                                            <a class="btn btn-primary" href="{{ action('MembershipController@join', $group->id) }}"><i class="fa fa-sign-in"></i>
+                                                {{ trans('group.join') }}
+                                            </a>
+                                        @endcan
+
+                                    @endunless
+
+                                    <a class="btn btn-primary" href="{{ action('GroupController@show', $group->id) }}">{{ trans('group.visit') }}</a>
+
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    {{trans('group.no_group_yet')}}
+                @endforelse
+
             </div>
-        </div>
+
+        @endif
+
+        
+
+
     </div>
-</div>
-@endif
-
-
-
-
-</div>
 
 @endsection
