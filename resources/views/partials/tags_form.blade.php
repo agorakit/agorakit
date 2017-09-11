504956354e60405fdc5b4991d760af096fa80a9a
Philippe Jadin <philippe.jadin@gmail.com>
2017-09-11 17:56:41 +0200

    more cleanup in the js packages not needed

--- resources/views/partials/tags_form.blade.php
+++ resources/views/partials/tags_form.blade.php
@@ -1,3 +1,31 @@
+@include ('partials.selectize')
+
+
+@push('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" />
+@endpush
+
+
+@push('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+    <script>
+    $( document ).ready(function() {
+        $('.tags').selectize({
+            delimiter: ',',
+            persist: false,
+            create: function(input) {
+                return {
+                    value: input,
+                    text: input
+                }
+            }
+        });
+    });
+    </script>
+@endpush
+
+
 <div class="form-group">
     <label for="tags">{{trans('messages.tags')}}</label>
     <select class="form-control tags" name="tags[]" multiple="multiple">
@@ -13,21 +41,3 @@
     </select>
     <span id="tagshelp" class="help-block">{{trans('messages.tags_help')}}</span>
 </div>
-
-
-
-@push('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" />
-@endpush
-
-
-@push('js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-
-    <script>
-    $(".tags").select2({
-        tags: true,
-        tokenSeparators: [',']
-    })
-    </script>    
-@endpush
