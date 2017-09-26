279dcbd82a90e9517ad14aee195d66c6fa1bb4e4
Philippe <philippe.jadin@gmail.com>
2017-09-26 23:35:38 +0200

    ES translation

--- config/app.php
+++ config/app.php
@@ -81,7 +81,7 @@ return [
 
     'locale' => env('APP_DEFAULT_LOCALE', 'en'),
 
-    'locales' => ['fr', 'nl', 'de', 'en'],
+    'locales' => ['fr', 'nl', 'de', 'en', 'es'],
 
     /*
     |--------------------------------------------------------------------------
---
+++ resources/lang/es/discussion.php
@@ -0,0 +1,14 @@
+<?php
+/*************************************************************************
+ Generated via "php artisan localization:missing" at 2017/09/18 18:35:15 
+*************************************************************************/
+
+return array (
+  //==================================== Translations ====================================//
+  'create_one_button' => 'Start a new one',
+  //================================== Obsolete strings ==================================//
+  'LLH:obsolete' => 
+  array (
+    'all_in_this_group' => 'All discussionns in this group',
+  ),
+);
