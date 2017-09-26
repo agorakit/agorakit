9c6e934440fa007f24a7f6ee8129fb9f245a28cb
Philippe <philippe.jadin@gmail.com>
2017-09-26 23:35:38 +0200

    ES translation

---
+++ resources/lang/es/action.php
@@ -0,0 +1,14 @@
+<?php
+/*************************************************************************
+ Generated via "php artisan localization:missing" at 2017/09/18 18:35:15 
+*************************************************************************/
+
+return array (
+  //==================================== Translations ====================================//
+  'create_one_button' => 'Add an event to the agenda',
+  //================================== Obsolete strings ==================================//
+  'LLH:obsolete' => 
+  array (
+    'agenda_of_this_group' => 'Agenda of this group',
+  ),
+);
---
+++ resources/lang/es/group.php
@@ -0,0 +1,37 @@
+<?php
+/*************************************************************************
+ Generated via "php artisan localization:missing" at 2017/09/18 18:35:15 
+*************************************************************************/
+
+return array (
+  //==================================== Translations ====================================//
+  'closed' => 'Closed group',
+  'cover' => 'Cover picture',
+  'create_a_group_button' => 'Start a group',
+  'create_button' => 'Create the group',
+  'create_group_title' => 'Create a new group',
+  'description' => 'Group description',
+  'history_of_this_group' => 'Edit history of this group',
+  'join' => 'Join',
+  'latest_discussions' => 'Latest discussions',
+  'latest_files' => 'Latest files',
+  'name' => 'Group name',
+  'open' => 'Public group',
+  'type' => 'Group type',
+  //================================== Obsolete strings ==================================//
+  'LLH:obsolete' => 
+  array (
+    'about_this_group' => 'About this group',
+    'create' => 'Create',
+    'create_a_group_intro' => 'Create a group easily here',
+    'latest_actions' => 'Latest actions',
+    'leave' => 'Leave',
+    'no_group_joined_yet' => 'No group joined yet',
+    'no_group_joined_yet_act_now' => 'No group joined yet act now',
+    'no_group_yet' => 'No group yet',
+    'private' => 'Closed group',
+    'public' => 'Public group',
+    'visit' => 'Visit',
+    'your_group_here' => 'Your group here',
+  ),
+);
