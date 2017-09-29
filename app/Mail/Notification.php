3432bfa321534813e89118da515ff0b8cd8d359d
Philippe <philippe.jadin@gmail.com>
2017-09-29 10:57:58 +0200

    correct notification email without the use of env()

--- app/Mail/Notification.php
+++ app/Mail/Notification.php
@@ -21,37 +21,27 @@ class Notification extends Mailable
     public $last_notification;
 
     /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
+    * Create a new message instance.
+    *
+    * @return void
+    */
     public function __construct()
     {
         //
     }
 
     /**
-     * Build the message.
-     *
-     * @return $this
-     */
+    * Build the message.
+    *
+    * @return $this
+    */
     public function build()
     {
-        \App::setLocale(env('APP_DEFAULT_LOCALE', 'en'));
+        \App::setLocale(config('app.locale'));
 
         return $this->markdown('emails.notification')
-        ->subject('['.env('APP_NAME').'] '.trans('messages.news_from_group_email_subject').' "'.$this->group->name.'"')
-        ->from(env('MAIL_NOREPLY', 'noreply@example.com'), env('APP_NAME', 'Laravel'));
-
-/*
-        Mail::send('emails.notification', ['user' => $user, 'group' => $group, 'membership' => $membership, 'discussions' => $discussions,
-        'files'                                   => $files, 'users' => $users, 'actions' => $actions, 'last_notification' => $last_notification, ], function ($message) use ($user, $group) {
-            $message->from(env('MAIL_NOREPLY', 'noreply@example.com'), env('APP_NAME', 'Laravel'))
-            ->to($user->email)
-            ->subject('['.env('APP_NAME').'] '.trans('messages.news_from_group_email_subject').' "'.$group->name.'"');
-        });
-        */
-
+        ->subject('['.config('app.name').'] '.trans('messages.news_from_group_email_subject').' "'.$this->group->name.'"')
+        ->from(config('mail.from.email'), config('mail.from.email'));
 
         return $this->view('view.name');
     }
