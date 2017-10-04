1991848053e1e9101d4e0ad9a84f122ee37be185
Philippe <philippe.jadin@gmail.com>
2017-10-04 22:49:43 +0200

    better email subject

--- app/Notifications/Mention.php
+++ app/Notifications/Mention.php
@@ -44,7 +44,7 @@ class Mention extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-        ->subject(trans('messages.you_have_been_mentionned_by'). ' ' . $this->user->name)
+        ->subject('['. $this->comment->discussion->group->name . '] ' . trans('messages.you_have_been_mentionned_by'). ' ' . $this->user->name)
         ->line(trans('messages.you_have_been_mentionned_by'). ' ' . $this->user->name . ' ' . trans('messages.in_the_discussion') . ' ' . $this->comment->discussion->name . ' : ')
         ->line($this->comment->body)
         ->action(trans('messages.reply'), action('DiscussionController@show', [$this->comment->discussion->group, $this->comment->discussion]))
