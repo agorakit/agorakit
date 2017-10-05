cc5ea7ebec0c8a4ef7f9feb51aee2196c2e8be23
Philippe <philippe.jadin@gmail.com>
2017-10-05 22:36:07 +0200

    autologin for notifications

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -3,14 +3,14 @@
 
 <strong>{{trans('messages.hello')}} {{$user->name}},</strong>
 <p>
-{{trans('messages.here_are_the_latest_news_of')}} "<a href="{{action('GroupController@show', $group->id)}}">{{$group->name}}</a>"
+{{trans('messages.here_are_the_latest_news_of')}} "<a href="{{action('GroupController@show', $group)}}">{{$group->name}}</a>"
 </p>
 
 
 @if ($discussions->count() > 0)
 <h2>{{trans('messages.latest_discussions')}}</h2>
 @foreach($discussions as $discussion)
-<h3><a href="{{action('DiscussionController@show', [$group->id, $discussion->id])}}">{{$discussion->name}} </a></h3>
+<h3><a href="{{action('DiscussionController@show', [$group, $discussion])}}">{{$discussion->name}} </a></h3>
 <p>
 @if ($discussion->comments->count() > 0)
 {{ summary($discussion->body, 1000) }}
@@ -23,14 +23,14 @@
 @if ($comment->created_at > $last_notification)
 <div style="border:1px solid #aaa; border-radius: 3px; margin-bottom: 1em; margin-left: 1em; padding: 1em">
 <p>
-<a href="{{ action('DiscussionController@show', [$group->id, $discussion->id]) }}#comment_{{$comment->id}}">{{$comment->user->name}}</a> ({{$comment->created_at->diffForHumans()}}):
+<a href="{{ action('DiscussionController@show', [$group, $discussion]) }}#comment_{{$comment->id}}">{{$comment->user->name}}</a> ({{$comment->created_at->diffForHumans()}}):
 {!! filter($comment->body) !!}
 </p>
 </div>
 @endif
 @endforeach
 
-@component('mail::button', ['url' => action('DiscussionController@show', [$group, $discussion])])
+@component('mail::button', ['url' => Autologin::to($user, action('DiscussionController@show', [$group, $discussion]))])
 {{trans('messages.reply')}}
 @endcomponent
 
@@ -42,7 +42,7 @@
 @if ($actions->count() > 0)
 <h2>{{trans('messages.next_actions')}}</h2>
 @foreach($actions as $action)
-<strong><a href="{{action('ActionController@show', [$group->id, $action->id])}}">{{$action->name}}</a></strong>
+<strong><a href="{{action('ActionController@show', [$group, $action])}}">{{$action->name}}</a></strong>
 <p>{!!filter($action->body) !!}</p>
 <p>
 {{$action->start->format('d/m/Y H:i')}}
@@ -58,7 +58,7 @@
 @if ($users->count() > 0)
 <h2>{{trans('messages.latest_users')}}</h2>
 @foreach($users as $user)
-<a href="{{action('UserController@show', $user->id)}}">{{$user->name}}</a>
+<a href="{{action('UserController@show', $user)}}">{{$user->name}}</a>
 <br/>
 @endforeach
 @endif
@@ -67,7 +67,7 @@
 @if ($files->count() > 0)
 <h2>{{trans('messages.latest_files')}}</h2>
 @foreach($files as $file)
-<a href="{{action('FileController@show', [$group->id, $file->id])}}"><img src="{{action('FileController@thumbnail', [$group->id, $file->id])}}" style="width: 24px; height:24px"/>{{$file->name}}</a>
+<a href="{{action('FileController@show', [$group, $file])}}"><img src="{{action('FileController@thumbnail', [$group, $file])}}" style="width: 24px; height:24px"/>{{$file->name}}</a>
 <br/>
 @endforeach
 @endif
@@ -78,7 +78,7 @@
 <p style="margin-top: 5em; font-size: 0.8em">
 {{trans('messages.you_receive_this_email_from_the_group')}} "{{$group->name}}", {{trans('messages.because_you_asked_for_it')}}.
 <br/>
-{{trans('messages.if_you_dont_want_news_anymore')}}, <a href="{{action('MembershipController@preferencesForm', $group->id)}}">{{trans('messages.click_here')}}</a>.
+{{trans('messages.if_you_dont_want_news_anymore')}}, <a href="{{Autologin::to($user, action('MembershipController@preferencesForm', $group))}}">{{trans('messages.click_here')}}</a>.
 </p>
 
 
