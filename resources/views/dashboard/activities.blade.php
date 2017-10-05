b7a6dffed9a15d796503dee66d34c95bb80a1c70
Philippe <philippe.jadin@gmail.com>
2017-10-05 20:28:11 +0200

    Nicer activity page (still wip)

--- resources/views/dashboard/activities.blade.php
+++ resources/views/dashboard/activities.blade.php
@@ -16,14 +16,99 @@
         @isset($activities)
             <ul class="list-group">
                 @foreach ($activities as $activity)
-                    <li class="list-group-item">
-                        <span class="avatar"><img src="{{$activity->user->avatar()}}" class="img-circle"/></span>
-                        <a href="{{action('UserController@show', $activity->user)}}">{{$activity->user->name}}</a>
-                         {{trans($activity->action)}}
-                         <a href="{{$activity->linkToModel()}}">{{$activity->model->name}}</a>
-                         {{$activity->created_at->diffForHumans()}}
-                         in <a href="{{action('GroupController@show', $activity->group)}}">{{$activity->group->name}}</a>
-                    </li>
+                    @if ($activity->getType() == 'discussion')
+                        <div class="activity">
+                            <div class="header">
+                                <span class="avatar"><img src="{{$activity->user->avatar()}}" class="img-circle"/></span>
+                                <a href="{{action('UserController@show', $activity->user)}}">{{$activity->user->name}}</a>
+                                {{trans('messages.activity_' . $activity->action)}}
+                                <a href="{{$activity->linkToModel()}}">{{$activity->model->name}}</a>
+                                {{$activity->created_at->diffForHumans()}}
+                                {{trans('messages.in')}}
+                                <a href="{{action('GroupController@show', $activity->group)}}">{{$activity->group->name}}</a>
+                            </div>
+
+                            <div class="detail">
+                                {{summary($activity->model->body)}}
+                            </div>
+
+                            <div class="action">
+                                <a href="" class="btn btn-primary btn-sm">{{trans('messages.visit')}}</a>
+                            </div>
+
+                        </div>
+                    @endif
+
+
+                    @if ($activity->getType() == 'action')
+                        <div class="activity">
+                            <div class="header">
+                                <span class="avatar"><img src="{{$activity->user->avatar()}}" class="img-circle"/></span>
+                                <a href="{{action('UserController@show', $activity->user)}}">{{$activity->user->name}}</a>
+                                {{trans('messages.activity_' . $activity->action)}}
+                                <a href="{{$activity->linkToModel()}}">{{$activity->model->name}}</a>
+                                {{$activity->created_at->diffForHumans()}}
+                                {{trans('messages.in')}}
+                                <a href="{{action('GroupController@show', $activity->group)}}">{{$activity->group->name}}</a>
+                            </div>
+
+                            <div class="detail">
+                                {{summary($activity->model->body)}}
+                            </div>
+
+                            <div class="action">
+                                <a href="" class="btn btn-primary btn-sm">{{trans('messages.visit')}}</a>
+                            </div>
+
+                        </div>
+                    @endif
+
+                    @if ($activity->getType() == 'file')
+                        <div class="activity">
+                            <div class="header">
+                                <span class="avatar"><img src="{{$activity->user->avatar()}}" class="img-circle"/></span>
+                                <a href="{{action('UserController@show', $activity->user)}}">{{$activity->user->name}}</a>
+                                {{trans('messages.activity_' . $activity->action)}}
+                                <a href="{{$activity->linkToModel()}}">{{$activity->model->name}}</a>
+                                {{$activity->created_at->diffForHumans()}}
+                                {{trans('messages.in')}}
+                                <a href="{{action('GroupController@show', $activity->group)}}">{{$activity->group->name}}</a>
+                            </div>
+
+                            <div class="detail">
+                                <img src="{{action('FileController@preview', [$activity->group, $activity->model])}}"/>
+                            </div>
+
+                            <div class="action">
+                                <a href="" class="btn btn-primary btn-sm">{{trans('messages.visit')}}</a>
+                            </div>
+
+                        </div>
+                    @endif
+
+
+                    @if ($activity->getType() == 'comment')
+                        <div class="activity">
+                            <div class="header">
+                                <span class="avatar"><img src="{{$activity->user->avatar()}}" class="img-circle"/></span>
+                                <a href="{{action('UserController@show', $activity->user)}}">{{$activity->user->name}}</a>
+                                {{trans('messages.activity_' . $activity->action)}}
+                                <a href="{{$activity->linkToModel()}}">{{$activity->model->discussion->name}}</a>
+                                {{$activity->created_at->diffForHumans()}}
+                                {{trans('messages.in')}}
+                                <a href="{{action('GroupController@show', $activity->group)}}">{{$activity->group->name}}</a>
+                            </div>
+
+                            <div class="detail">
+                                {{summary($activity->model->body)}}
+                            </div>
+
+                            <div class="action">
+                                <a href="" class="btn btn-primary btn-sm">{{trans('messages.reply')}}</a>
+                            </div>
+                        </div>
+                    @endif
+
                 @endforeach
 
             </ul>
