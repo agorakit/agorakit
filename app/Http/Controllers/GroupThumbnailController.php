cd44fe562f7aa1517cd3925df88827452361a797
Philippe <philippe.jadin@gmail.com>
2017-10-10 10:41:51 +0200

    separate controller for group thumbnails

---
+++ app/Http/Controllers/GroupThumbnailController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Group;
+use Auth;
+use File;
+use Gate;
+use Illuminate\Http\Request;
+use Image;
+use Storage;
+
+class GroupThumbnailController extends Controller
+{
+    public function cover(Group $group)
+    {
+        $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
+
+        if (File::exists($path)) {
+            $cachedImage = Image::cache(function ($img) use ($path) {
+                return $img->make($path)->widen(600);
+            }, 60000, true);
+
+            return $cachedImage->response();
+        } else {
+            return Image::canvas(600, 350)->fill('#cccccc')->response(); // TODO caching or default group image instead
+        }
+    }
+
+    public function avatar(Group $group)
+    {
+        $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
+
+        if (File::exists($path)) {
+            $cachedImage = Image::cache(function ($img) use ($path) {
+                return $img->make($path)->fit(32, 32);
+            }, 60000, true);
+
+            return $cachedImage->response();
+        } else {
+            return Image::canvas(600, 350)->fill('#cccccc')->response(); // TODO caching or default group image instead
+        }
+    }
+}
