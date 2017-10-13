f454cad1c9fc0f9c1abf1a004a91cdada702a9eb
Philippe <philippe.jadin@gmail.com>
2017-10-13 18:52:44 +0200

    Use Trumbowyg instead of ckeditor

--- resources/views/comments/form.blade.php
+++ resources/views/comments/form.blade.php
@@ -9,63 +9,24 @@
 
 @push ('js')
     <script>
-    var all_config = [
-        {
-            at: "@",
-            data: '{{route('groups.users.mention', $group)}}',
-            insertTpl: "<a href=\"${url}\" data-mention-user-id=\"${id}\">${atwho-at}${name}</a>",
-        },
-        {
-            at: "f:",
-            data: '{{route('groups.files.mention', $group)}}',
-            insertTpl: "<a href=\"${url}\" data-mention-file-id=\"${id}\">${name} (${atwho-at}${id})</a>",
-        },
-        {
-            at: "d:",
-            data: '{{route('groups.discussions.mention', $group)}}',
-            insertTpl: "<a href=\"${url}\" data-mention-discussion-id=\"${id}\">${name} (${atwho-at}${id})</a>",
-        }
-    ];
 
-    // Bind to every CKEditor instance that'll load in the future
-    CKEDITOR.on('instanceReady', function(event) {
-
-        var editor = event.editor;
-
-        // Switching from and to source mode
-        editor.on('mode', function(e) {
-            load_atwho(this, all_config);
-
-        });
-
-        // First load
-        load_atwho(editor, all_config);
-
+    $('.trumbowyg-editor').atwho({
+        at: "@",
+        data: '{{route('groups.users.mention', $group)}}',
+        insertTpl: "<a href=\"${url}\" data-mention-user-id=\"${id}\">${atwho-at}${name}</a>",
     });
 
-    function load_atwho(editor, at_config) {
+    $('.trumbowyg-editor').atwho({
+        at: "f:",
+        data: '{{route('groups.files.mention', $group)}}',
+        insertTpl: "<a href=\"${url}\" data-mention-file-id=\"${id}\">${name} (${atwho-at}${id})</a>",
+    });
 
-        // WYSIWYG mode when switching from source mode
-        if (editor.mode != 'source') {
-
-            editor.document.getBody().$.contentEditable = true;
-
-            var my_element = $(editor.document.getBody().$)
-            .atwho('setIframe', editor.window.getFrame().$);
-
-
-            $.each(at_config, function(key, value) {
-                my_element.atwho(value);
-            });
-        }
-        // Source mode when switching from WYSIWYG
-        else {
-            var my_element = $(editor.container.$).find(".cke_source");
-            $.each(at_config, function(key, value) {
-                my_element.atwho(value);
-            });
-        }
-    }
+    $('.trumbowyg-editor').atwho({
+        at: "d:",
+        data: '{{route('groups.discussions.mention', $group)}}',
+        insertTpl: "<a href=\"${url}\" data-mention-discussion-id=\"${id}\">${name} (${atwho-at}${id})</a>",
+    });
 
     </script>
 @endpush
