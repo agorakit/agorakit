969821892de810b540bfa9c8759713152de28f4b
Philippe <philippe.jadin@gmail.com>
2017-10-27 21:47:06 +0200

    nicer admin settings page, \App\Setting now allow default settings to be stored in config/agorakit.php, and overwritten in the db

---
+++ resources/views/admin/settings/index.blade.php
@@ -0,0 +1,55 @@
+@extends('app')
+
+@section('content')
+
+    <h1> Settings</h1>
+
+    {!! Form::open(['action' => 'Admin\SettingsController@update']) !!}
+
+    <div class="alert alert-info">This admin setting page is a work in progress. Some settings are not used yet (but all are saved for future use)</div>
+
+    <h2>Home page presentation text : for anonymous users</h2>
+    <div class="form-group">
+        {!! Form::textarea('homepage_presentation', \App\Setting::get('homepage_presentation'), ['class' => 'form-control wysiwyg']) !!}
+    </div>
+
+    <h2>Home page presentation text : for logged-in users</h2>
+    <p>You can use it for announcements for example</p>
+    <div class="form-group">
+        {!! Form::textarea('homepage_presentation_for_members', \App\Setting::get('homepage_presentation_for_members'), ['class' => 'form-control wysiwyg']) !!}
+    </div>
+
+    <h2>Help page</h2>
+    <p>A single help page you can customize, available on the user menu, for logged in user</p>
+    <div class="form-group">
+        {!! Form::textarea('help_text', \App\Setting::get('help_text'), ['class' => 'form-control wysiwyg']) !!}
+    </div>
+
+
+    <h2>Group creation</h2>
+    <div class="form-group">
+        {!! Form::checkbox('user_can_create_groups',1 ,\App\Setting::get('user_can_create_groups')) !!}
+        Allow users to create groups
+    </div>
+
+    <div class="form-group">
+        {!! Form::checkbox('notify_admins_on_group_create',1,  \App\Setting::get('notify_admins_on_group_create')) !!}
+        Notify administrators when a new group is created
+    </div>
+
+
+
+    <div class="form-group">
+        {!! Form::submit(trans('messages.save'), ['class' => 'btn btn-lg btn-primary']) !!}
+    </div>
+
+
+    {!! Form::close() !!}
+
+
+</div>
+
+@endsection
+
+
+@include('partials.wysiwyg')
