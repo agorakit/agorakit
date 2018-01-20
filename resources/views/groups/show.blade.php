6fe884f438c040f4755d69d65ac2394b34d861fc
philippe <philippe.jadin@gmail.com>
2018-01-20 16:45:26 +0100

    nicer actions wip

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -118,46 +118,11 @@
                                 </a>
                             @endcan
                         </h2>
-                        <table class="table table-hover special">
-                            <thead>
-                                <tr>
-                                    <th class="avatar"></th>
-                                    <th class="summary"></th>
-                                    <th class="date"></th>
-                                    <th class="unread"></th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                @foreach( $discussions as $discussion )
-                                    <tr>
-
-                                        <td class="avatar"><span class="avatar"><img src="{{$discussion->user->avatar()}}" class="img-circle"/></span></td>
-                                        <td class="content">
-                                            <a href="{{ route('groups.discussions.show', [$discussion->group_id, $discussion->id]) }}">
-                                                <span class="name">{{ $discussion->name }}</span>
-                                                <span class="summary">{{summary($discussion->body) }}</span>
-                                                <br/>
-                                            </a>
-                                            <span class="group-name"><a href="{{ route('groups.show', [$discussion->group]) }}">{{ $discussion->group->name }}</a></span>
-                                        </td>
-
-                                        <td class="date">
-                                            {{ $discussion->updated_at->diffForHumans() }}
-                                        </td>
-
-                                        <td>
-                                            @if ($discussion->unReadCount() > 0)
-                                                <i class="fa fa-comment"></i>
-                                                <span class="badge">{{ $discussion->unReadCount() }}</span>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-
-
+                        <div class="discussions">
+                            @foreach( $discussions as $discussion )
+                                @include('discussions.discussion')
+                            @endforeach
+                        </div>
                     @endif
                 @endif
 
