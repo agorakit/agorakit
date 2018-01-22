5a48f1f635714c12d70633a5e2d5ff5e1cf3376b
philippe <philippe.jadin@gmail.com>
2018-01-22 23:45:45 +0100

    revert useless group filtering system

--- resources/views/dashboard/groups.blade.php
+++ resources/views/dashboard/groups.blade.php
@@ -10,113 +10,104 @@
 
     <div class="tab_content">
 
-
+        {{--
         @push ('js')
 
-            <script>
-            $(document).ready(function(){
-                @foreach ($all_tags as $tag)
-                $("#toggle-tag-{{$tag->tag_id}}").click(function(){
-                    $(".tag-group").hide();
-                    $(".tag-{{$tag->tag_id}}").show();
-                    $(".tag-toggle").removeClass('active');
-                    $(this).addClass('active');
-
-                });
-                @endforeach
-
-                $("#toggle-tag-all").click(function(){
-                    $(".tag-group").show();
-                    $(".tag-toggle").removeClass('active');
-                    $(this).addClass('active');
-                });
-            });
-
-            </script>
-        @endpush
-
-
-
-
+        <script>
+        $(document).ready(function(){
         @foreach ($all_tags as $tag)
-            <a class="btn btn-primary btn-sm tag-toggle" id="toggle-tag-{{$tag->tag_id}}">{{$tag->name}}</a>
-        @endforeach
+        $("#toggle-tag-{{$tag->tag_id}}").click(function(){
+        $(".tag-group").hide();
+        $(".tag-{{$tag->tag_id}}").show();
+        $(".tag-toggle").removeClass('active');
+        $(this).addClass('active');
 
-        <a class="btn btn-primary btn-sm tag-toggle active" id="toggle-tag-all">{{trans('messages.show_all')}}</a>
+    });
+@endforeach
 
+$("#toggle-tag-all").click(function(){
+$(".tag-group").show();
+$(".tag-toggle").removeClass('active');
+$(this).addClass('active');
+});
+});
 
-
-        <div class="tab_content">
-            @if ($groups)
-                <table class="table table-hover special">
-                    <thead>
-                        <tr>
-                            <th class="avatar"></th>
-                            <th class="summary"></th>
-                            <th style="width:100px" class="hidden-xs"></th>
-                            <th style="width:100px" class=""></th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        @forelse( $groups as $group )
-                            <tr class="tag-group @foreach ($group->tags as $tag)tag-{{$tag->tag_id}} @endforeach">
-                                <td class="avatar"><span class="avatar"><img src="{{ route('groups.cover.small', $group)}}" class="rounded"/></span></td>
-                                <td class="content">
-                                    <a href="{{ route('groups.show',  $group->id) }}">
-                                        <span class="name">{{ $group->name }}
-
-                                            @if ($group->isPublic())
-                                                <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
-                                            @else
-                                                <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
-                                            @endif</span>
-
-                                            <span class="summary">{{summary($group->body) }}</span>
-                                            <br/>
-                                        </a>
-                                        <span class="group-name">
-                                            @foreach ($group->tags as $tag)
-                                                <span class="label label-default">{{$tag->name}}</span>
-                                            @endforeach
-                                        </span>
-
-                                    </td>
-
-                                    <td class="date hidden-xs">
-                                        {{ $group->updated_at->diffForHumans() }}
-                                    </td>
-
-                                    <td>
-                                        @unless ($group->isMember())
-                                            @can ('join', $group)
-                                                <a class="btn btn-primary btn-sm" href="{{ action('MembershipController@store', $group->id) }}"><i class="fa fa-sign-in"></i>
-                                                    {{ trans('group.join') }}
-                                                </a>
-                                            @endcan
-
-                                        @endunless
-
-                                    </td>
-
-                                </tr>
-                            @empty
-                                {{trans('messages.nothing_yet')}}
-                            @endforelse
-                        </tbody>
-                    </table>
+</script>
+@endpush
 
 
 
-                @else
-                    {{trans('messages.nothing_yet')}}
+
+@foreach ($all_tags as $tag)
+<a class="btn btn-primary btn-sm tag-toggle" id="toggle-tag-{{$tag->tag_id}}">{{$tag->name}}</a>
+@endforeach
+
+<a class="btn btn-primary btn-sm tag-toggle active" id="toggle-tag-all">{{trans('messages.show_all')}}</a>
+--}}
+
+
+<div class="tab_content">
+    @if ($groups)
+
+        <div class="row">
+            @forelse( $groups as $group )
+                <div class="col-md-4">
+                    <div class="card tag-group @foreach ($group->tags as $tag)tag-{{$tag->tag_id}} @endforeach">
+                        <img class="card-img-top" src="{{ route('groups.cover', $group)}}" />
+
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $group->name }}
+                                @if ($group->isPublic())
+                                    <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
+                                @else
+                                    <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
+                                @endif
+                            </h5>
+                            <p class="card-text">
+                                {{summary($group->body) }}
+                                @foreach ($group->tags as $tag)
+                                    <span class="label label-default">{{$tag->name}}</span>
+                                @endforeach
+                            </p>
+                            <a class="btn btn-primary" href="{{ action('GroupController@show', $group) }}"></i>
+                                {{ trans('messages.visit') }}
+                            </a>
+
+                            @unless ($group->isMember())
+                                @can ('join', $group)
+                                    <a class="btn btn-primary" href="{{ action('MembershipController@store', $group->id) }}">
+                                        {{ trans('group.join') }}
+                                    </a>
+                                @endcan
+                            @endunless
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+                @if ($loop->iteration % 3 == 0)
+                </div>
+                <div class="row">
                 @endif
-            </div>
 
 
-
-
+            @empty
+                {{trans('messages.nothing_yet')}}
+            @endforelse
 
         </div>
+    @else
+        {{trans('messages.nothing_yet')}}
+    @endif
+</div>
 
-    @endsection
+
+
+
+
+</div>
+
+@endsection
