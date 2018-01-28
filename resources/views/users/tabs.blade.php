80d9a989a14c3be2b13db803683bdccc5a7c5274
philippe <philippe.jadin@gmail.com>
2018-01-28 11:58:14 +0100

    correct user tabs

--- resources/views/users/tabs.blade.php
+++ resources/views/users/tabs.blade.php
@@ -1,34 +1,33 @@
 <div class="page_header">
     <h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i>
-
-        {{ $user->name }}</h1>
-    </div>
-
-    @if (Auth::user())
-        <ul class="nav nav-tabs">
-            <li role="presentation" @if (isset($tab) && ($tab == 'profile')) class="active" @endif>
-                <a href="{{ route('users.show', $user->id) }}">
-                    <i class="fa fa-user"></i> {{ trans('messages.user_profile') }}
-                </a>
-            </li>
+        {{ $user->name }}
+    </h1>
+</div>
 
 
+<ul class="nav nav-tabs">
 
-            <li role="presentation" @if (isset($tab) && ($tab == 'contact')) class="active" @endif>
-                <a href="{{route('users.contactform', $user->id)}}"><i class="fa fa-envelope-o"></i>
-                    {{trans('messages.contact_this_user')}}</a>
-                </a>
-            </li>
+    <li class="nav-item">
+        <a href="{{ route('users.show', $user->id) }}" class="nav-link @if (isset($tab) && ($tab == 'profile')) active @endif">
+            <i class="fa fa-user"></i> {{ trans('messages.user_profile') }}
+        </a>
+    </li>
 
-            @can('update', $user)
-                <li role="presentation" @if (isset($tab) && ($tab == 'edit')) class="active" @endif>
-                    <a href="{{ route('users.edit', $user->id) }}">
-                        <i class="fa fa-pencil"></i>
-                        {{trans('messages.edit')}}
-                    </a>
-                </li>
-            @endcan
 
-        @endif
 
-    </ul>
+    <li class="nav-item">
+        <a href="{{route('users.contactform', $user->id)}}" class="nav-link @if (isset($tab) && ($tab == 'contact')) active @endif">
+            <i class="fa fa-envelope-o"></i> {{trans('messages.contact_this_user')}}</a>
+        </a>
+    </li>
+
+    @can('update', $user)
+        <li class="nav-item">
+            <a href="{{ route('users.edit', $user->id) }}" class="nav-link @if (isset($tab) && ($tab == 'edit')) active @endif">
+                <i class="fa fa-pencil"></i>
+                {{trans('messages.edit')}}
+            </a>
+        </li>
+    @endcan
+
+</ul>
