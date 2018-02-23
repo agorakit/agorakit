b34685871f956c4f2e3a9c557ee07d2f8d58929b
philippe <philippe.jadin@gmail.com>
2018-02-23 13:00:33 +0100

    Better ui to add events. It seems to be working fine, some more polishing needed

--- resources/views/actions/form.blade.php
+++ resources/views/actions/form.blade.php
@@ -10,76 +10,53 @@
 
 <div class="form-group">
     {!! Form::label('location', trans('messages.location')) !!}
-    {!! Form::textarea('location', null, ['class' => 'form-control']) !!}
+    {!! Form::textarea('location', null, ['class' => 'form-control', 'rows'=>4]) !!}
 </div>
 
-<div class="row">
 
-    <div class='col-md-6'>
-        <div class="form-group">
-            {!! Form::label('start', trans('messages.starts')) !!}<br/>
-            <div class='input-group date' id='start'>
-                @if (isset($action->start))
-                    {!! Form::text('start', $action->start->format('Y-m-d H:i') , ['class' => 'form-control',  'required']) !!}
-                @else
-                    {!! Form::text('start', \Carbon\Carbon::now()->format('Y-m-d H:i') , ['class' => 'form-control',  'required']) !!}
-                @endif
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
-        </div>
-    </div>
-    <div class='col-md-6'>
-        <div class="form-group">
-            {!! Form::label('stop', trans('messages.ends')) !!}<br/>
-            <div class='input-group date' id='stop'>
-                @if (isset($action->stop))
-                    {!! Form::text('stop', $action->stop->format('Y-m-d H:i') , ['class' => 'form-control',  'required']) !!}
-                @else
-                    {!! Form::text('stop', null , ['class' => 'form-control', 'required']) !!}
-                @endif
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
-        </div>
-    </div>
+<div class="form-group">
+    {{trans('messages.start_date')}}
+    {!! Form::text('start_date', $action->start->format('Y-m-d') , ['class' => 'form-control datepicker']) !!}
 </div>
+
+<div class="form-group">
+    {{trans('messages.start_time')}}
+    {!! Form::text('start_time', $action->start->format('H:i') , ['class' => 'form-control', 'required']) !!}
+</div>
+
+<div class="form-group">
+    {{trans('messages.stop_time')}}
+    @if (isset($action->stop))
+        {!! Form::text('stop_time', $action->stop->format('H:i') , ['class' => 'form-control', 'required']) !!}
+    @else
+        {!! Form::text('stop_time', null , ['class' => 'form-control']) !!}
+    @endif
+</div>
+
+<div class="form-group">
+    {{trans('messages.stop_date')}}
+    @if ((isset($action->stop)) && ($action->stop->format('Y-m-d') <> $action->start->format('Y-m-d')))
+        {!! Form::text('stop_date', $action->stop->format('Y-m-d') , ['class' => 'form-control datepicker']) !!}
+    @else
+        {!! Form::text('stop_date', null , ['class' => 'form-control datepicker']) !!}
+    @endif
+</div>
+
+
 
 @include ('partials.wysiwyg')
 
 
 @section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker3.min.css" />
 @endsection
 
 @push('js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment-with-locales.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
 
-
-    <script type="text/javascript">
-    $(function () {
-        $('#start').datetimepicker({
-            locale: '{{App::getLocale()}}',
-            sideBySide: true,
-            format: 'YYYY-MM-DD HH:mm'
-        });
-        $('#stop').datetimepicker({
-            useCurrent: false, //Important! See issue #1075
-            locale: '{{App::getLocale()}}',
-            sideBySide: true,
-            format: 'YYYY-MM-DD HH:mm'
-        });
-        $("#start").on("dp.change", function (e) {
-            $('#stop').data("DateTimePicker").minDate(e.date);
-        });
-        $("#stop").on("dp.change", function (e) {
-            $('#start').data("DateTimePicker").maxDate(e.date);
-        });
+    <script>
+    $('.datepicker').datepicker({
+        format: 'yyyy-mm-dd'
     });
     </script>
-
-
 @endpush
