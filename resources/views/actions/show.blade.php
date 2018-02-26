1df953179da0b3f07c33cbc0bf2f090aa494fa53
philippe <philippe.jadin@gmail.com>
2018-02-26 21:30:44 +0100

    user attending system - wip

--- resources/views/actions/show.blade.php
+++ resources/views/actions/show.blade.php
@@ -2,43 +2,79 @@
 
 @section('content')
 
-  @include('groups.tabs')
-  <div class="tab_content">
+    @include('groups.tabs')
 
-    <div class="action">
-      <h2 class="name">
-        <a href="{{ route('groups.actions.index', [$group->id]) }}">{{trans('messages.actions')}}</a> <i class="fa fa-angle-right"></i>
-        {{ $action->name }} <a href="{{ route('groups.actions.edit', [$group->id, $action->id]) }}" class="btn btn-primary btn-xs">{{trans('messages.edit')}}</a>
-      </h2>
 
-      <div class="meta">{{trans('messages.started_by')}} <span class="user"><a href="{{ route('users.show', [$action->user->id]) }}">{{ $action->user->name}}</a></span>, {{trans('messages.in')}} <a href="{{ route('groups.actions.index', [$group->id]) }}">{{ $action->group->name}}</a> {{ $action->created_at->diffForHumans()}} </div>
+    <div class="content">
 
-      <h4>{{trans('messages.what')}} ?</h4>
+        <div class="meta">{{trans('messages.started_by')}} <span class="user"><a href="{{ route('users.show', [$action->user->id]) }}">{{ $action->user->name}}</a></span>, {{trans('messages.in')}} <a href="{{ route('groups.actions.index', [$group->id]) }}">{{ $action->group->name}}</a> {{ $action->created_at->diffForHumans()}} </div>
 
-      <p class="body">
-        {!! filter($action->body) !!}
-      </p>
+        <div class="d-flex justify-content-between">
+            <h3>
+                {{ $action->name }}
+            </h3>
 
-      <h4>{{trans('messages.when')}} ?</h4>
-      <p>{{trans('messages.begins')}} : {{$action->start->format('d/m/Y H:i')}}</p>
-      <p>{{trans('messages.ends')}} : {{$action->stop->format('d/m/Y H:i')}}</p>
+            <div class="ml-4 dropdown">
+                <button class="btn btn-outline-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-wrench" aria-hidden="true"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
 
-      <h4>{{trans('messages.where')}} ?</h4>
-      <p>{{$action->location}}</p>
+                    @can('update', $action)
+                        <a class="dropdown-item" href="{{ route('groups.actions.edit', [$group->id, $action->id]) }}">
+                            <i class="fa fa-pencil"></i>
+                            {{trans('messages.edit')}}
+                        </a>
+                    @endcan
 
-      @if ($action->revisionHistory->count() > 0)
-        <a class="btn btn-primary btn-xs" href="{{route('groups.actions.history', [$group->id, $action->id])}}"><i class="fa fa-history"></i> {{trans('messages.show_history')}}</a>
-      @endif
+                    @can('delete', $action)
+                        <a class="dropdown-item" href="{{ route('groups.actions.deleteconfirm', [$group->id, $action->id]) }}">
+                            <i class="fa fa-trash"></i>
+                            {{trans('messages.delete')}}
+                        </a>
+                    @endcan
 
-      @can('delete', $action)
-        <a class="btn btn-primary btn-xs" href="{{ route('groups.actions.deleteconfirm', [$group->id, $action->id]) }}"><i class="fa fa-trash"></i>
-          {{trans('messages.delete')}}
-        </a>
-      @endcan
+                    @if ($action->revisionHistory->count() > 0)
+                        <a class="dropdown-item" href="{{route('groups.actions.history', [$group->id, $action->id])}}"><i class="fa fa-history"></i> {{trans('messages.show_history')}}</a>
+                    @endif
+                </div>
+            </div>
+
+        </div>
+
+        <p class="body">
+            {{$action->start->format('d/m/Y H:i')}} - {{$action->stop->format('d/m/Y H:i')}}
+            {!! filter($action->body) !!}
+        </p>
+
+        <h4>{{trans('messages.when')}} ?</h4>
+        <p>{{trans('messages.begins')}} : {{$action->start->format('d/m/Y H:i')}}</p>
+        <p>{{trans('messages.ends')}} : {{$action->stop->format('d/m/Y H:i')}}</p>
+
+        <h4>{{trans('messages.where')}} ?</h4>
+        <p>{{$action->location}}</p>
+
+        <h4>
+            {{trans('messages.user_attending')}}
+            @if (Auth::user() && Auth::user()->isAttending($action))
+                {!! Form::open(['route' => ['groups.actions.unattend', $group, $action]]) !!}
+                {!! Form::submit(trans('messages.unattend'), ['class' => 'btn btn-primary']) !!}
+                {!! Form::close() !!}
+            @else
+                {!! Form::open(['route' => ['groups.actions.attend', $group, $action]]) !!}
+                {!! Form::submit(trans('messages.attend'), ['class' => 'btn btn-primary']) !!}
+                {!! Form::close() !!}
+            @endif
+        </h4>
+
+        @foreach ($action->users as $user)
+            <div class="avatar"><img src="{{$user->avatar()}}" class="rounded-circle"/>{{$user->name}}</div>
+        @endforeach
 
     </div>
 
-  </div>
+</div>
+
 
 
 @endsection
