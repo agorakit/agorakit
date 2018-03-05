ac1935db6a3171afc598a76290252cc12df6c92f
philippe <philippe.jadin@gmail.com>
2018-03-05 17:40:46 +0100

    big wip

--- resources/views/dashboard/files.blade.php
+++ resources/views/dashboard/files.blade.php
@@ -1,18 +1,5 @@
 @extends('app')
 
-@include('partials.datatables')
-
-@push('js')
-    <script>
-    $(document).ready(function(){
-        $('.files-grid').DataTable( {
-            paging: false,
-            order: []
-        });
-    });
-    </script>
-@endpush
-
 
 @section('content')
     <div class="page_header">
@@ -20,60 +7,16 @@
     </div>
 
 
-    @include('dashboard.tabs')
 
-
-    <div class="tab_content">
-        @if ($files)
-            <table class="table table-hover special">
-                <thead>
-                    <tr>
-                        <th class="avatar"></th>
-                        <th class="summary"></th>
-                        <th></th>
-                        <th style="width:100px" class="date hidden-xs"></th>
-
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @forelse( $files as $file )
-
-
-                        <tr onclick="document.location = '{{ route('groups.files.show', [$file->group_id, $file->id]) }}';">
-                            <td class="avatar"><span class="avatar"><img src="{{ route('groups.files.thumbnail', [$file->group_id, $file->id]) }}"/></span></td>
-                            <td class="content">
-                                <a href="{{ route('groups.files.show', [$file->group_id, $file->id]) }}">
-                                    <span class="name">{{ $file->name }}</span>
-                                    <span class="summary">{{summary($file->body) }}</span>
-                                    <br/>
-                                </a>
-                                <span class="group-name"><a href="{{ route('groups.show', [$file->group_id]) }}">{{ $file->group->name }}</a></span>
-                            </td>
-
-                            <td>
-                                @if ($file->isFile()){{sizeForHumans($file->filesize)}}@endif
-                            </td>
-
-                            <td class="date hidden-xs">
-                                {{ $file->created_at->diffForHumans() }}
-                            </td>
-
-
-
-                        </tr>
-
-                    @empty
-                        {{trans('messages.nothing_yet')}}
-                    @endforelse
-                </tbody>
-            </table>
-
-
-            {!! $files->render() !!}
-        @else
+    <div class="files mt-5">
+        @forelse( $files as $file )
+            @include('files.file')
+        @empty
             {{trans('messages.nothing_yet')}}
-        @endif
+        @endforelse
+
+        {!! $files->render() !!}
     </div>
+    
 
 @endsection
