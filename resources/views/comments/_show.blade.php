1dacf09e01cefc4d9d098280027117a827b2c4cc
philippe <philippe.jadin@gmail.com>
2018-03-09 15:48:35 +0100

    nicer tags

--- resources/views/comments/_show.blade.php
+++ resources/views/comments/_show.blade.php
@@ -1,42 +1,50 @@
 <a name="comment_{{$comment->id}}"></a>
 
-<div class="comment @if ($comment_key + 2 > $read_comments) unread @else read @endif"
+<div class="comment
+@if ($comment_key + 2 > $read_comments) unread @else read @endif"
     @if ($comment_key + 2 == $read_comments) id="unread" @endif>
 
-        <div class="avatar"><img src="{{$comment->user->avatar()}}" class="rounded-circle"/></div>
+        <div class="d-flex">
 
-        <div class="user"><a href="{{ route('users.show', [$comment->user->id]) }}">{{$comment->user->name}}</a></div>
+            <div class="avatar mr-3"><img src="{{$comment->user->avatar()}}" class="rounded-circle"/></div>
 
-        <div class="body">{!! filter($comment->body) !!}</div>
+            <div class="w-100">
+                <div class="user"><a href="{{ route('users.show', [$comment->user->id]) }}">{{$comment->user->name}}</a></div>
 
-
-
-        <div class="d-flex justify-content-between">
-            <div class="created">{{$comment->created_at->diffForHumans()}}</div>
+                <div class="body">{!! filter($comment->body) !!}</div>
 
 
-            <div class="ml-4 dropdown">
-                <button class="btn btn-secondary-outline dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fa fa-wrench" aria-hidden="true"></i>
 
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <div class="d-flex justify-content-between">
+                    <div class="meta">{{$comment->created_at->diffForHumans()}}</div>
+
 
                     @can('update', $comment)
-                        <a class="dropdown-item" href="{{ action('CommentController@edit', [$group->id, $discussion->id, $comment->id]) }}"><i class="fa fa-pencil"></i>
-                            {{trans('messages.edit')}}
-                        </a>
-                    @endcan
+                    <div class="ml-4 dropdown">
+                        <button class="btn btn-secondary-outline dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-wrench" aria-hidden="true"></i>
 
-                    @can('delete', $comment)
-                        <a class="dropdown-item" href="{{ action('CommentController@destroyConfirm', [$group->id, $discussion->id, $comment->id]) }}"><i class="fa fa-trash"></i>
-                            {{trans('messages.delete')}}
-                        </a>
-                    @endcan
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 
-                    @if ($comment->revisionHistory->count() > 0)
-                        <a class="dropdown-item" href="{{action('CommentController@history', [$group->id, $discussion->id, $comment->id])}}"><i class="fa fa-history"></i> {{trans('messages.show_history')}}</a>
-                    @endif
+                            @can('update', $comment)
+                                <a class="dropdown-item" href="{{ action('CommentController@edit', [$group->id, $discussion->id, $comment->id]) }}"><i class="fa fa-pencil"></i>
+                                    {{trans('messages.edit')}}
+                                </a>
+                            @endcan
+
+                            @can('delete', $comment)
+                                <a class="dropdown-item" href="{{ action('CommentController@destroyConfirm', [$group->id, $discussion->id, $comment->id]) }}"><i class="fa fa-trash"></i>
+                                    {{trans('messages.delete')}}
+                                </a>
+                            @endcan
+
+                            @if ($comment->revisionHistory->count() > 0)
+                                <a class="dropdown-item" href="{{action('CommentController@history', [$group->id, $discussion->id, $comment->id])}}"><i class="fa fa-history"></i> {{trans('messages.show_history')}}</a>
+                            @endif
+                        </div>
+                    </div>
+                @endcan
                 </div>
             </div>
         </div>
--- resources/views/dashboard/discussions.blade.php
+++ resources/views/dashboard/discussions.blade.php
@@ -1,30 +1,30 @@
 @extends('app')
 
 @section('content')
-    <div class="page_header">
+    <div class="page_header d-flex mb-3">
         <h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ trans('messages.discussions') }}</h1>
-    </div>
 
-
-    @include('dashboard.tabs')
-
-    <div class="toolbox">
-        <a class="btn btn-primary" href="{{ route('discussions.create') }}">
-            <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
-        </a>
-    </div>
-
-    <div class="tab_content">
-
-        <div class="discussions">
-            @forelse( $discussions as $discussion )
-                @include('discussions.discussion')
-            @empty
-                {{trans('messages.nothing_yet')}}
-            @endforelse
-            {!! $discussions->render() !!}
+        <div class="ml-auto">
+            <a class="btn btn-primary" href="{{ route('discussions.create') }}">
+                <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
+            </a>
         </div>
 
     </div>
+
+
+
+
+
+    <div class="discussions">
+        @forelse( $discussions as $discussion )
+            @include('discussions.discussion')
+        @empty
+            {{trans('messages.nothing_yet')}}
+        @endforelse
+        {!! $discussions->render() !!}
+    </div>
+
+
 
 @endsection
