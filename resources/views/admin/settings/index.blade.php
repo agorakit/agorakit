887e05175df2fec13ccc1f1de93260e6d1fc9d29
philippe <philippe.jadin@gmail.com>
2018-03-11 21:28:12 +0100

    use the setting() helper everywhere

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -10,30 +10,30 @@
 
     <h2>Home page presentation text : for anonymous users</h2>
     <div class="form-group">
-        {!! Form::textarea('homepage_presentation', \App\Setting::get('homepage_presentation'), ['class' => 'form-control wysiwyg']) !!}
+        {!! Form::textarea('homepage_presentation', setting('homepage_presentation'), ['class' => 'form-control wysiwyg']) !!}
     </div>
 
     <h2>Home page presentation text : for logged-in users</h2>
     <p>You can use it for announcements for example</p>
     <div class="form-group">
-        {!! Form::textarea('homepage_presentation_for_members', \App\Setting::get('homepage_presentation_for_members'), ['class' => 'form-control wysiwyg']) !!}
+        {!! Form::textarea('homepage_presentation_for_members', setting('homepage_presentation_for_members'), ['class' => 'form-control wysiwyg']) !!}
     </div>
 
     <h2>Help page</h2>
     <p>A single help page you can customize, available on the user menu, for logged in user</p>
     <div class="form-group">
-        {!! Form::textarea('help_text', \App\Setting::get('help_text'), ['class' => 'form-control wysiwyg']) !!}
+        {!! Form::textarea('help_text', setting('help_text'), ['class' => 'form-control wysiwyg']) !!}
     </div>
 
 
     <h2>Group creation</h2>
     <div class="form-group">
-        {!! Form::checkbox('user_can_create_groups',1 ,\App\Setting::get('user_can_create_groups')) !!}
+        {!! Form::checkbox('user_can_create_groups',1 , setting('user_can_create_groups')) !!}
         Allow regular users to create groups (if you uncheck this box, only admins will be able to create groups)
     </div>
 
     <div class="form-group">
-        {!! Form::checkbox('notify_admins_on_group_create',1,  \App\Setting::get('notify_admins_on_group_create')) !!}
+        {!! Form::checkbox('notify_admins_on_group_create',1 , setting('notify_admins_on_group_create')) !!}
         Notify administrators when a new group is created
     </div>
 
