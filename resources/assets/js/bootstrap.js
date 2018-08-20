dc54139dc1809b8302f37b0c8f9b6aa64d7e8ee4
Philippe Jadin <philippe.jadin@gmail.com>
2018-08-20 15:38:17 +0200

    update node packages

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,4 +1,3 @@
-
 window._ = require('lodash');
 
 window.Popper = require('popper.js/dist/umd/popper');
@@ -21,15 +20,19 @@ try {
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
  * a simple convenience so we don't have to attach every token manually.
  */
+
 
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
@@ -38,6 +41,7 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,5 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.browserSync({proxy: 'localhost:8000'});
+mix.browserSync({proxy: 'agorakit.test'});
+//mix.browserSync();
