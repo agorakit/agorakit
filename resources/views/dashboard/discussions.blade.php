3ab765e06d17c69e57f769025dfbcde1f971de47
Philippe Jadin <philippe.jadin@gmail.com>
2018-08-21 12:25:35 +0200

    filter by tag on global discussions list, yeah!

--- resources/views/dashboard/discussions.blade.php
+++ resources/views/dashboard/discussions.blade.php
@@ -1,13 +1,21 @@
 @extends('app')
 
 @section('content')
-    <div class=" d-flex mb-3">
-        <h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ trans('messages.discussions') }}</h1>
-        <div class="ml-auto">
-            <a class="btn btn-primary" href="{{ route('discussions.create') }}">
-                <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
-            </a>
-        </div>
+
+
+<h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ trans('messages.discussions') }}</h1>
+
+
+  <div class="toolbox d-md-flex">
+      <div class="d-flex mb-2">
+          @include('partials.tags_filter')
+      </div>
+
+      <div class="ml-auto">
+        <a class="btn btn-primary" href="{{ route('discussions.create') }}">
+          <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
+        </a>
+      </div>
     </div>
 
     @include ('partials.preferences-show')
