b37ba6e673e5da0479fcfff06eeb333a769561e9
Philippe Jadin <philippe.jadin@gmail.com>
2018-08-21 11:17:27 +0200

    new de tools

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,4 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
 
-mix.browserSync({proxy: 'agorakit.test'});
-//mix.browserSync();
+mix.browserSync({proxy: 'localhost:8000'});
