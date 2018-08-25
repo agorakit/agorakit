01f5a5feb157704f4753dbdfa3c5829bf823767b
Philippe Jadin <philippe.jadin@gmail.com>
2018-08-25 20:32:52 +0200

    More logical group cover system, including default image if no image is found

--- app/Http/Controllers/GroupCoverController.php
+++ app/Http/Controllers/GroupCoverController.php
@@ -10,39 +10,14 @@ use Illuminate\Http\Request;
 use Image;
 use Storage;
 
-class GroupThumbnailController extends Controller
+
+/*
+Handle group cover image
+*/
+class GroupCoverController extends Controller
 {
-    public function cover(Group $group)
-    {
-        $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
 
-        if (File::exists($path)) {
-            $cachedImage = Image::cache(function ($img) use ($path) {
-                return $img->make($path)->widen(600);
-            }, 60000, true);
-
-            return $cachedImage->response();
-        } else {
-            return Image::canvas(600, 350)->fill('#cccccc')->response(); // TODO caching or default group image instead
-        }
-    }
-
-    public function carousel(Group $group)
-    {
-        $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
-
-        if (File::exists($path)) {
-            $cachedImage = Image::cache(function ($img) use ($path) {
-                return $img->make($path)->fit(1200, 500);
-            }, 60000, true);
-
-            return $cachedImage->response();
-        } else {
-            return Image::canvas(1200, 500)->fill('#cccccc')->response(); // TODO caching or default group image instead
-        }
-    }
-
-    public function avatar(Group $group)
+    public function small(Group $group)
     {
         $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
 
@@ -53,7 +28,39 @@ class GroupThumbnailController extends Controller
 
             return $cachedImage->response();
         } else {
-            return Image::canvas(600, 350)->fill('#cccccc')->response(); // TODO caching or default group image instead
+            abort(404);
         }
     }
+
+    public function medium(Group $group)
+    {
+        $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
+
+        if (File::exists($path)) {
+            $cachedImage = Image::cache(function ($img) use ($path) {
+                return $img->make($path)->fit(400, 300);
+            }, 60000, true);
+
+            return $cachedImage->response();
+        } else {
+            abort(404);
+        }
+    }
+
+    public function large(Group $group)
+    {
+        $path = storage_path().'/app/groups/'.$group->id.'/cover.jpg';
+
+        if (File::exists($path)) {
+            $cachedImage = Image::cache(function ($img) use ($path) {
+                return $img->make($path)->fit(800, 600);
+            }, 60000, true);
+
+            return $cachedImage->response();
+        } else {
+            abort(404);
+        }
+    }
+
+
 }
