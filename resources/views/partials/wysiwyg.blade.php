3a9f7a246e77620e09e9f85de1a338de263c7885
Philippe Jadin <philippe.jadin@gmail.com>
2018-09-05 18:34:19 +0200

    keep only useful wysiwyg buttons

--- resources/views/partials/wysiwyg.blade.php
+++ resources/views/partials/wysiwyg.blade.php
@@ -1,17 +1,30 @@
 @push('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.8.1/ui/trumbowyg.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.10.0/ui/trumbowyg.min.css" />
 @endpush
 
 
 @push('js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.8.1/trumbowyg.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.8.1/plugins/upload/trumbowyg.upload.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.10.0/trumbowyg.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Trumbowyg/2.10.0/plugins/upload/trumbowyg.upload.min.js"></script>
 
 
 
     <script>
     $.trumbowyg.svgPath = '/svg/icons.svg';
-    $('.wysiwyg').trumbowyg()
+    $('.wysiwyg').trumbowyg({
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em'],
+            ['link'],
+            ['insertImage'],
+            ['unorderedList', 'orderedList'],
+            ['removeformat'],
+            ['fullscreen']
+        ],
+        minimalLinks: true
+    })
 
 
     </script>
