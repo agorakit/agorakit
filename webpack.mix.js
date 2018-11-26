b3e6a212424eb2e183a0596eadba44403ef86425
Philippe Jadin <philippe.jadin@gmail.com>
2018-11-26 12:04:46 +0100

    upgrade to laravel 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,19 @@
 let mix = require('laravel-mix');
+require('laravel-mix-purgecss')();;
 
 /*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+|--------------------------------------------------------------------------
+| Mix Asset Management
+|--------------------------------------------------------------------------
+|
+| Mix provides a clean, fluent API for defining some Webpack build steps
+| for your Laravel application. By default, we are compiling the Sass
+| file for the application as well as bundling up all the JS files.
+|
+*/
 
-mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('resources/js/app.js', 'public/js')
+.sass('resources/sass/app.scss', 'public/css');
 
 mix.browserSync({proxy: 'localhost:8000'});
