0531df4cfb354ec224af177557abea12e5ccda4c
Philippe Jadin <philippe.jadin@gmail.com>
2018-11-26 15:18:06 +0100

    update js-css mix infra

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,9 @@
 let mix = require('laravel-mix');
-require('laravel-mix-purgecss')();;
-
-/*
-|--------------------------------------------------------------------------
-| Mix Asset Management
-|--------------------------------------------------------------------------
-|
-| Mix provides a clean, fluent API for defining some Webpack build steps
-| for your Laravel application. By default, we are compiling the Sass
-| file for the application as well as bundling up all the JS files.
-|
-*/
+require('laravel-mix-purgecss');
 
 
 mix.js('resources/js/app.js', 'public/js')
-.sass('resources/sass/app.scss', 'public/css');
+.sass('resources/sass/app.scss', 'public/css')
+.purgeCss();;
 
-mix.browserSync({proxy: 'localhost:8000'});
+mix.browserSync({proxy: '127.0.0.1:8000'});
