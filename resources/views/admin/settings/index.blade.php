8a6d9026a3c6e0f8629ed9984300e1cafa326352
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-01 21:58:14 +0200

    settings page made translatable

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -7,42 +7,42 @@
 
   {!! Form::open(['action' => 'Admin\SettingsController@update', 'files' => true]) !!}
 
-  <div class="alert alert-info">This admin setting page is a work in progress. Some settings are not used yet (but all are saved for future use)</div>
+  <div class="alert alert-info">{{ __('This admin setting page is a work in progress. Some settings are not used yet (but all are saved for future use)')}}</div>
 
   <div class="setting">
-    <h2>Application name</h2>
+    <h2>{{ __('Application name')}}</h2>
     <div class="form-group">
       <div class="setting-help">
-        The name of your application. Used everywhere, in titles, in emails, etc... Choose a short and catchy name
+        {{ __('The name of your application. Used everywhere, in titles, in emails, etc... Choose a short and catchy name')}}
       </div>
       {!! Form::text('name', setting('name'), ['class' => 'form-control']) !!}
     </div>
   </div>
 
   <div class="setting">
-    <h2>Application logo</h2>
+    <h2>{{ __('Application logo')}}</h2>
     <div class="form-group">
       <div class="setting-help">
-        This logo will be used in the top left area and as a favicon
+        {{ __('This logo will be used in the top left area and as a favicon')}}
       </div>
       <input name="logo" id="logo" type="file" title="{{trans('messages.select_one_file')}}">
     </div>
   </div>
 
   <div class="setting">
-    <h2>Home page presentation text : for anonymous users</h2>
+    <h2>{{ __('Home page presentation text : for anonymous users')}}</h2>
     <div class="form-group">
       <div class="setting-help">
-        This is shown on the homepage for non connected user. Make it attractive :-)
+        {{ __('This is shown on the homepage for non connected user. Make it attractive :-)')}}
       </div>
       {!! Form::textarea('homepage_presentation', setting('homepage_presentation'), ['class' => 'form-control wysiwyg']) !!}
     </div>
   </div>
 
   <div class="setting">
-    <h2>Home page presentation text <em>for logged-in users</em></h2>
+    <h2>{{ __('Home page presentation text <em>for logged-in users</em>')}}</h2>
     <div class="setting-help">
-      You can use it for announcements for example
+      {{ __('You can use it for announcements for example')}}
     </div>
     <div class="form-group">
       {!! Form::textarea('homepage_presentation_for_members', setting('homepage_presentation_for_members'), ['class' => 'form-control wysiwyg']) !!}
@@ -51,9 +51,9 @@
 
   <div class="setting">
 
-    <h2>Help page</h2>
+    <h2>{{ __('Help page')}}</h2>
     <div class="setting-help">
-      A single help page you can customize, available on the user menu, for logged in user
+      {{ __('A single help page you can customize, available on the user menu, for logged in user')}}
     </div>
     <div class="form-group">
       {!! Form::textarea('help_text', setting('help_text'), ['class' => 'form-control wysiwyg']) !!}
@@ -62,34 +62,34 @@
 
   <div class="setting">
 
-    <h2>Group creation</h2>
+    <h2>{{ __('Group creation')}}</h2>
     <div class="form-group">
       {!! Form::checkbox('user_can_create_groups',1 , setting('user_can_create_groups')) !!}
-      Allow regular users to create groups (if you uncheck this box, only admins will be able to create groups)
+      {{ __('Allow regular users to create groups (if you uncheck this box, only admins will be able to create groups)')}}
     </div>
 
     <div class="form-group">
       {!! Form::checkbox('user_can_create_secret_groups',0 , setting('user_can_create_secret_groups')) !!}
-      Allow regular users to create <strong>secret</strong> groups (if you uncheck this box, only admins will be able to create secret groups)
+      {{ __('Allow regular users to create <strong>secret</strong> groups (if you uncheck this box, only admins will be able to create secret groups)')}}
     </div>
 
     <div class="form-group">
       {!! Form::checkbox('notify_admins_on_group_create',1 , setting('notify_admins_on_group_create')) !!}
-      Notify administrators when a new group is created
+      {{ __('Notify administrators when a new group is created')}}
     </div>
   </div>
 
 
   <div class="setting">
 
-    <h2>Post by email</h2>
+    <h2>{{ __('Post by email')}}</h2>
     <div class="help">
-      If this is enabled, user will be able to post to your groups using a predefined email address.
-      Each group received an email address based on it's name
+      {{ __('If this is enabled, user will be able to post to your groups using a predefined email address.
+      Each group received an email address based on it\'s name')}}
     </div>
     <div class="form-group">
       {!! Form::checkbox('user_can_post_by_email',1 , setting('user_can_post_by_email')) !!}
-      Allow users to post by email
+      {{ __('Allow users to post by email')}}
     </div>
 
     <div class="form-group">
