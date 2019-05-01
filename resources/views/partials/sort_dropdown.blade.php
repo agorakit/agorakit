052ad7c540abca3062f612a41b8705800d6e879e
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-01 22:03:50 +0200

    translations...

--- resources/views/partials/sort_dropdown.blade.php
+++ resources/views/partials/sort_dropdown.blade.php
@@ -5,33 +5,33 @@
         @if (request()->get('sort'))
             : {{ trans('messages.sort_by_' . request()->get('sort').'_'.request()->get('dir')) }}
         @else
-            : @lang('messages.sort_by_created_at_desc')
+            : {{trans('messages.sort_by_created_at_desc') }}
         @endif
     </button>
     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
         <a up-target="files" class="dropdown-item" href="{{request()->fullUrlWithQuery(['sort' => 'created_at', 'dir' => 'desc', 'page' => null])}}">
             <i class="fa fa-sort-amount-desc"></i>
-            @lang('messages.sort_by_created_at_desc')
+            {{trans('messages.sort_by_created_at_desc')}}
         </a>
         <a up-target="files" class="dropdown-item" href="{{request()->fullUrlWithQuery(['sort' => 'created_at', 'dir' => 'asc', 'page' => null])}}">
             <i class="fa fa-sort-amount-asc"></i>
-            @lang('messages.sort_by_created_at_asc')
+            {{trans('messages.sort_by_created_at_asc')}}
         </a>
         <a up-target="files" class="dropdown-item" href="{{request()->fullUrlWithQuery(['sort' => 'name', 'dir' => 'asc', 'page' => null])}}">
             <i class="fa fa-sort-alpha-asc"></i>
-            @lang('messages.sort_by_name_asc')
+            {{trans('messages.sort_by_name_asc')}}
         </a>
         <a up-target="files" class="dropdown-item" href="{{request()->fullUrlWithQuery(['sort' => 'name', 'dir' => 'desc', 'page' => null])}}">
             <i class="fa fa-sort-alpha-desc"></i>
-            @lang('messages.sort_by_name_desc')
+            {{trans('messages.sort_by_name_desc')}}
         </a>
         <a up-target="files" class="dropdown-item" href="{{request()->fullUrlWithQuery(['sort' => 'filesize', 'dir' => 'asc', 'page' => null])}}">
             <i class="fa fa-sort-numeric-asc"></i>
-            @lang('messages.sort_by_filesize_asc')
+            {{trans('messages.sort_by_filesize_asc')}}
         </a>
         <a up-target="files" class="dropdown-item" href="{{request()->fullUrlWithQuery(['sort' => 'filesize', 'dir' => 'desc', 'page' => null])}}">
             <i class="fa fa-sort-numeric-desc"></i>
-            @lang('messages.sort_by_filesize_desc')
+            {{trans('messages.sort_by_filesize_desc')}}
         </a>
 
     </div>
