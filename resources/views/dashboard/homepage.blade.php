2eabf8745573697fb6544df81acde03181ca3843
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-06 12:26:41 +0200

    some air

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -3,51 +3,52 @@
 @section('content')
 
 
-<h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ __('Homepage') }}</h1>
-@include ('partials.preferences-show')
+  <h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ __('Homepage') }}</h1>
+  <div class="my-4">
+    @include ('partials.preferences-show')
+  </div>
 
-
-<div class="row">
+  <div class="row">
     <div class="col-md-8">
 
 
-        @if ($discussions->count() > 0)
+      @if ($discussions->count() > 0)
         <h1 class="small"> {{ __('Latest discussions') }}</h1>
         <a class="badge badge-pill badge-primary mb-4" href="{{ route('discussions.create') }}">
-            <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
+          <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
         </a>
 
 
         <div class="discussions">
-            @forelse( $discussions as $discussion )
+          @forelse( $discussions as $discussion )
             @include('discussions.discussion')
-            @empty
+          @empty
             {{trans('messages.nothing_yet')}}
-            @endforelse
+          @endforelse
         </div>
 
-        @endif
+      @endif
     </div>
 
 
     <div class="col-md-4">
 
-        @if ($actions->count() > 0)
+      @if ($actions->count() > 0)
         <h1 class="small">{{ __('Calendar') }}</h1>
         <a class="badge badge-pill badge-primary mb-4" href="{{ route('actions.create') }}">
-            <i class="fa fa-plus"></i> {{trans('action.create_one_button')}}
+          <i class="fa fa-plus"></i> {{trans('action.create_one_button')}}
         </a>
 
         <div class="actions">
-            @forelse( $actions as $action)
+          @forelse( $actions as $action)
             @include('actions.action')
-            @empty
+          @empty
             {{trans('messages.nothing_yet')}}
-            @endforelse
+          @endforelse
         </div>
-        @endif
+      @endif
     </div>
-</div>
+  </div>
 
 
 
