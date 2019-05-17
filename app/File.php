fe666ae190dbf1131bd507ce9b70a34b6cbfc578
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-17 14:40:44 +0200

    woupsy

--- app/File.php
+++ app/File.php
@@ -10,80 +10,81 @@ use Watson\Validating\ValidatingTrait;
 
 class File extends Model
 {
-    use ValidatingTrait;
-    use SoftDeletes;
-    use RevisionableTrait;
-    use Taggable;
+  use ValidatingTrait;
+  use SoftDeletes;
+  use RevisionableTrait;
+  use Taggable;
 
-    protected $rules = [
-        'name'     => 'required',
-        'user_id'  => 'required|exists:users,id',
-        'group_id' => 'required|exists:groups,id',
-    ];
+  protected $rules = [
+    'name'     => 'required',
+    'user_id'  => 'required|exists:users,id',
+    'group_id' => 'required|exists:groups,id',
+  ];
 
-    protected $table = 'files';
-    public $timestamps = true;
-    protected $dates = ['deleted_at'];
-    protected $casts = ['user_id' => 'integer'];
+  protected $table = 'files';
+  public $timestamps = true;
+  protected $dates = ['deleted_at'];
+  protected $casts = ['user_id' => 'integer'];
 
-    protected $keepRevisionOf = ['name', 'path', 'filesize'];
+  protected $keepRevisionOf = ['name', 'path', 'filesize'];
 
-    // Item type can be :
-    // 0 : file (stored on the server)
-    // 1 : folder (virtual folders)
-    // 2 : link (to an etherpad or google doc for instance)
+  // Item type can be :
+  // 0 : file (stored on the server)
+  // 1 : folder (virtual folders)
+  // 2 : link (to an etherpad or google doc for instance)
 
-    const FILE = 0;
-    const FOLDER = 1;
-    const LINK = 2;
+  const FILE = 0;
+  const FOLDER = 1;
+  const LINK = 2;
 
-    public function user()
-    {
-        return $this->belongsTo(\App\User::class)->withTrashed();
+  public function user()
+  {
+    return $this->belongsTo(\App\User::class)->withTrashed();
+  }
+
+  public function group()
+  {
+    return $this->belongsTo(\App\Group::class)->withTrashed();
+  }
+
+  public function link()
+  {
+    return route('groups.files.download', [$this->group, $this]);
+  }
+
+  public function isFile()
+  {
+    return $this->item_type == $this::FILE;
+  }
+
+  public function isFolder()
+  {
+    return $this->item_type == $this::FOLDER;
+  }
+
+  public function isLink()
+  {
+    return $this->item_type == $this::LINK;
+  }
+
+  public function isImage()
+  {
+    if (in_array($this->mime, ['image/jpeg', 'image/png', 'image/gif'])) {
+      return true;
     }
 
-    public function group()
-    {
-        return $this->belongsTo(\App\Group::class)->withTrashed();
-    }
-
-    public function link()
-    {
-        return route('groups.files.download', [$this->group, $this]);
-    }
-
-    public function isFile()
-    {
-        return $this->item_type == $this::FILE;
-    }
-
-    public function isFolder()
-    {
-        return $this->item_type == $this::FOLDER;
-    }
-
-    public function isLink()
-    {
-        return $this->item_type == $this::LINK;
-    }
-
-    public function isImage()
-    {
-        if (in_array($this->mime, ['image/jpeg', 'image/png', 'image/gif'])) {
-            return true;
-        }
-
-        return false;
-    }
+    return false;
+  }
 
 
-    /**
-     * Permanently delete this file from storage
-     */
-    public function deleteFromStorage()
-    {
-      if (Storage::exists($this->path)) {
-        return Storage::delete($this->path)
+  /**
+  * Permanently delete this file from storage
+  */
+  public function deleteFromStorage()
+  {
+    if (Storage::exists($this->path)) {
+      return Storage::delete($this->path);
     }
     return false;
+  }
 }
