8a148c2eb67f05ef11894fc9b70749ef194720a0
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-20 16:13:31 +0200

    auto target blank for external links

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,4 @@
  */
 
 require('./bootstrap');
+require('./external_links');
---
+++ resources/js/external_links.js
@@ -0,0 +1,9 @@
+// Taken from : https://gist.github.com/CrocoDillon/7989214
+// vanilla JavaScript
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+  if (links[i].hostname != window.location.hostname) {
+    links[i].target = '_blank';
+  }
+}
