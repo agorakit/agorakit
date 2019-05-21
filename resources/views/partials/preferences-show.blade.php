b65757987c9d31d0e0f07f413d3031e46ae2c070
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-21 23:15:22 +0200

    smaller filter button

--- resources/views/partials/preferences-show.blade.php
+++ resources/views/partials/preferences-show.blade.php
@@ -1,24 +1,23 @@
 @auth
   @if (Auth::user()->getPreference('show', 'my') == 'my')
-
     <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{__('Show my groups')}}
+        <i class="far fa-eye"></i>
       </button>
       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a href="?set_preference=show&value=my" class="dropdown-item">{{trans('messages.my_groups')}}</a>
-        <a href="?set_preference=show&value=all" class="dropdown-item">{{trans('messages.all_groups')}}</a>
+        <a href="?set_preference=show&value=my" class="dropdown-item active">  {{__('Show my groups')}}</a>
+        <a href="?set_preference=show&value=all" class="dropdown-item">{{__('Show all groups')}}</a>
       </div>
     </div>
 
   @else
     <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{__('Show all groups')}}
+        <i class="far fa-eye"></i>
       </button>
       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a href="?set_preference=show&value=my" class="dropdown-item">{{trans('messages.my_groups')}}</a>
-        <a href="?set_preference=show&value=all" class="dropdown-item">{{trans('messages.all_groups')}}</a>
+        <a href="?set_preference=show&value=my" class="dropdown-item">{{__('Show my groups')}}</a>
+        <a href="?set_preference=show&value=all" class="dropdown-item active">{{__('Show all groups')}}</a>
       </div>
     </div>
 
