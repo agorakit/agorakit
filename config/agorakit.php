a18e01d6cb10555a1d6c5c13e1f0dc873fdd5822
Philippe Jadin <philippe.jadin@gmail.com>
2019-05-24 14:59:49 +0200

    1.1 : tags are everywhere!

--- config/agorakit.php
+++ config/agorakit.php
@@ -4,7 +4,7 @@
 
 return [
 'name'                          => env('APP_NAME', 'Agorakit'),
-'version'                       => '1.0', // do not update manually except when releasing a new version
+'version'                       => '1.1', // do not update manually except when releasing a new version
 'user_can_create_groups'        => true, // allow normal users to create new groups
 'notify_admins_on_group_create' => true, // as it says :-)
 
