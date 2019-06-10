965ca2c15f42ee99ffc3c53abf36709c0cba03d3
Philippe Jadin <philippe.jadin@gmail.com>
2019-06-10 14:30:09 +0200

    lastest files on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use Auth;
+use App\Action;
+use App\Discussion;
+use App\File;
+use App\Group;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 /**
- * This controller is the homepage and main entry point.
- */
+* This controller is the homepage and main entry point.
+*/
 class DashboardController extends Controller
 {
     public function __construct()
@@ -18,46 +22,56 @@ class DashboardController extends Controller
     }
 
     /**
-     * Main HOMEPAGE.
-     *
-     * @return Response
-     */
+    * Main HOMEPAGE.
+    *
+    * @return Response
+    */
     public function index(Request $request)
     {
         if (Auth::check()) {
             if (Auth::user()->getPreference('show') == 'all') {
                 // build a list of groups the user has access to
-        if (Auth::user()->isAdmin()) { // super admin sees everything
-          $groups = \App\Group::get()
-          ->pluck('id');
-        } else {
-            $groups = \App\Group::public()
-          ->get()
-          ->pluck('id')
-          ->merge(Auth::user()->groups()->pluck('groups.id'));
-        }
+                if (Auth::user()->isAdmin()) { // super admin sees everything
+                    $groups = Group::get()
+                    ->pluck('id');
+                } else {
+                    $groups = Group::public()
+                    ->get()
+                    ->pluck('id')
+                    ->merge(Auth::user()->groups()->pluck('groups.id'));
+                }
             } else {
                 $groups = Auth::user()->groups()->pluck('groups.id');
             }
 
-            $discussions = \App\Discussion::with('userReadDiscussion', 'group', 'user')
-      ->withCount('comments')
-      ->whereIn('group_id', $groups)
-      ->orderBy('updated_at', 'desc')->paginate(25);
+            $discussions = Discussion::with('userReadDiscussion', 'group', 'user')
+            ->withCount('comments')
+            ->whereIn('group_id', $groups)
+            ->orderBy('updated_at', 'desc')
+            ->take(25)
+            ->get();
 
-            $actions = \App\Action::with('group')
-      ->where('start', '>=', Carbon::now())
-      ->whereIn('group_id', $groups)
-      ->orderBy('start')
-      ->paginate(10);
+            $actions = Action::with('group')
+            ->where('start', '>=', Carbon::now())
+            ->whereIn('group_id', $groups)
+            ->orderBy('start')
+            ->take(10)
+            ->get();
+
+            $files = File::with('group')
+            ->has('group')
+            ->orderBy('updated_at', 'desc')
+            ->take(10)
+            ->get();
 
             return view('dashboard.homepage')
-      ->with('tab', 'homepage')
-      ->with('discussions', $discussions)
-      ->with('actions', $actions);
+            ->with('tab', 'homepage')
+            ->with('discussions', $discussions)
+            ->with('actions', $actions)
+            ->with('files', $files);
         } else {
             return view('dashboard.presentation')
-      ->with('tab', 'homepage');
+            ->with('tab', 'homepage');
         }
     }
 }
--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -3,63 +3,75 @@
 @section('content')
 
 
-  <div class="d-sm-flex">
-    <div class="d-flex mb-2">
-      <h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ __('Homepage') }}</h1>
-    </div>
-
-    <div class="ml-auto mb-2">
-      @include ('partials.preferences-show')
-    </div>
-  </div>
-
-  @if (setting('homepage_presentation_for_members', false))
-    <div class="alert" style="background-color: #eee">
-      {!! setting('homepage_presentation_for_members') !!}
-    </div>
-  @endif
-
-  <div class="row">
-    <div class="col-md-8">
-
-
-      @if ($discussions->count() > 0)
-        <h1 class="small"> {{ __('Latest discussions') }}</h1>
-        <a class="badge badge-pill badge-primary mb-4" href="{{ route('discussions.create') }}">
-          <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
-        </a>
-
-
-        <div class="discussions">
-          @forelse( $discussions as $discussion )
-            @include('discussions.discussion')
-          @empty
-            {{trans('messages.nothing_yet')}}
-          @endforelse
+    <div class="d-sm-flex">
+        <div class="d-flex mb-2">
+            <h1><a href="{{ route('index') }}"><i class="fa fa-home"></i></a> <i class="fa fa-angle-right"></i> {{ __('Homepage') }}</h1>
         </div>
 
-      @endif
-    </div>
-
-
-    <div class="col-md-4">
-
-      @if ($actions->count() > 0)
-        <h1 class="small">{{ __('Calendar') }}</h1>
-        <a class="badge badge-pill badge-primary mb-4" href="{{ route('actions.create') }}">
-          <i class="fa fa-plus"></i> {{trans('action.create_one_button')}}
-        </a>
-
-        <div class="actions">
-          @forelse( $actions as $action)
-            @include('actions.action')
-          @empty
-            {{trans('messages.nothing_yet')}}
-          @endforelse
+        <div class="ml-auto mb-2">
+            @include ('partials.preferences-show')
         </div>
-      @endif
     </div>
-  </div>
+
+    @if (setting('homepage_presentation_for_members', false))
+        <div class="alert" style="background-color: #eee">
+            {!! setting('homepage_presentation_for_members') !!}
+        </div>
+    @endif
+
+    <div class="row">
+        <div class="col-md-8">
+
+
+            @if ($discussions->count() > 0)
+                <h1 class="small"> {{ __('Latest discussions') }}</h1>
+                <a class="badge badge-pill badge-primary mb-4" href="{{ route('discussions.create') }}">
+                    <i class="fa fa-plus"></i> {{trans('discussion.create_one_button')}}
+                </a>
+
+
+                <div class="discussions">
+                    @forelse( $discussions as $discussion )
+                        @include('discussions.discussion')
+                    @empty
+                        {{trans('messages.nothing_yet')}}
+                    @endforelse
+                </div>
+
+            @endif
+        </div>
+
+
+        <div class="col-md-4">
+
+            @if ($actions->count() > 0)
+                <h1 class="small">{{ __('Calendar') }}</h1>
+                <a class="badge badge-pill badge-primary mb-4" href="{{ route('actions.create') }}">
+                    <i class="fa fa-plus"></i> {{trans('action.create_one_button')}}
+                </a>
+
+                <div class="actions">
+                    @forelse( $actions as $action)
+                        @include('actions.action')
+                    @empty
+                        {{trans('messages.nothing_yet')}}
+                    @endforelse
+                </div>
+            @endif
+
+            @if ($files->count() > 0)
+                <h1 class="small">{{ __('Files') }}</h1>
+
+                <div class="files">
+                    @forelse( $files as $file)
+                        @include('files.file')
+                    @empty
+                        {{trans('messages.nothing_yet')}}
+                    @endforelse
+                </div>
+            @endif
+        </div>
+    </div>
 
 
 
