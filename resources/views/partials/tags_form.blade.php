59f09dd8a7aa9b04c063a072792fa0bb6acf90ba
Philippe Jadin <philippe.jadin@gmail.com>
2019-06-24 12:12:47 +0200

    tagd form partial suport for limited tags in groups wip

--- resources/views/partials/tags_form.blade.php
+++ resources/views/partials/tags_form.blade.php
@@ -3,31 +3,50 @@
 
 
 @push('js')
-  <script>
-  $( document ).ready(function() {
-    $('.tags').selectize({
-      persist: true,
-      create: true
+    <script>
+    $( document ).ready(function() {
+        $('.tags').selectize({
+            persist: true,
+            @if (isset($group) && $group->tagsAreLimited())
+            create: false
+            @else
+            create: true
+            @endif
+        });
     });
-  });
-  </script>
+    </script>
 @endpush
 
 
 <div class="form-group">
-  <label for="tags">{{trans('messages.tags')}}</label>
-  <select class="form-control tags" name="tags[]" multiple="multiple">
-    @if (isset($model_tags))
-      @foreach ($model_tags as $tag)
-        <option selected="selected" value="{{$tag->name}}">{{$tag->name}}</option>
-      @endforeach
-    @endif
+    <label for="tags">{{trans('messages.tags')}}</label>
+    <select class="form-control tags" name="tags[]" multiple="multiple">
 
-    @if (isset($group) && $group->exists)
-      @foreach ($group->allowedTags() as $tag)
-        <option value="{{$tag->name}}">{{$tag->name}}</option>
-      @endforeach
-    @endif
-  </select>
-  <span id="tagshelp" class="help-block">{{trans('messages.tags_help')}}</span>
+        @if (isset($group) && $group->tagsAreLimited())
+            @if (isset($model_tags))
+                @foreach ($model_tags as $tag)
+                    <option selected="selected" value="{{$tag->name}}">{{$tag->name}}</option>
+                @endforeach
+            @endif
+
+            @foreach ($group->allowedTags() as $tag)
+                <option value="{{$tag->name}}">{{$tag->name}}</option>
+            @endforeach
+        @else
+
+            @if (isset($model_tags))
+                @foreach ($model_tags as $tag)
+                    <option selected="selected" value="{{$tag->name}}">{{$tag->name}}</option>
+                @endforeach
+            @endif
+
+            @if (isset($group) && $group->exists)
+                @foreach ($group->tagsUsed() as $tag)
+                    <option value="{{$tag->name}}">{{$tag->name}}</option>
+                @endforeach
+            @endif
+        @endif
+
+    </select>
+    <span id="tagshelp" class="help-block">{{trans('messages.tags_help')}}</span>
 </div>
