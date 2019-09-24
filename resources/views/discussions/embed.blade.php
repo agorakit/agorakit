ebf870a9a130bd56bcb625d2761a471a9ad69eff
Philippe Jadin <philippe.jadin@gmail.com>
2019-09-24 17:24:00 +0200

    ugly but woring files and discussion embeds with better preview - wip

--- resources/views/discussions/embed.blade.php
+++ resources/views/discussions/embed.blade.php
@@ -1,4 +1,7 @@
 
-<a href="{{ route('groups.discussions.show', [$discussion->group, $discussion]) }}" class="badge badge-warning">
-  <i class="far fa-comment"></i> {{ $discussion->name }}
+<a href="{{ route('groups.discussions.show', [$discussion->group, $discussion]) }}">
+  <div class="discussion-embed">
+    <div class="title"><i class="far fa-comment"></i> {{ $discussion->name }}</div>
+    <div class="summary">{{ summary($discussion->body) }}</div>
+  </div>
 </a>
--- resources/views/files/embed.blade.php
+++ resources/views/files/embed.blade.php
@@ -1,3 +1,18 @@
-<a href="{{ route('groups.files.show', [$file->group, $file]) }}" class="badge badge-info">
-  <i class="fas fa-paperclip"></i> {{ $file->name }}
-</a>
+<div class="file-embed">
+  <div class="thumbnail">
+    @if ($file->isLink())
+      <a class="mr-1" href="{{ route('groups.files.download', [$file->group, $file]) }}" target="_blank">
+        <img src="{{ route('groups.files.thumbnail', [$file->group, $file]) }}"/>
+      </a>
+    @else
+      <a href="{{ route('groups.files.show', [$file->group, $file]) }}">
+        <img src="{{ route('groups.files.preview', [$file->group, $file]) }}"/>
+      </a>
+    @endif
+  </div>
+
+  <div class="name">
+    {{ $file->name }}
+  </div>
+
+</div>
