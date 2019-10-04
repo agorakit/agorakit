f610fcf0847cfa73d57912a24b13032d4f46181a
Philippe Jadin <philippe.jadin@gmail.com>
2019-10-04 13:19:30 +0200

    composer update and igngition error page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,10 +9,10 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
+    * A list of the exception types that should not be reported.
+    *
+    * @var array
+    */
     protected $dontReport = [
         \Illuminate\Auth\AuthenticationException::class,
         \Illuminate\Auth\Access\AuthorizationException::class,
@@ -23,40 +23,40 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param \Exception $exception
-     *
-     * @return void
-     */
+    * Report or log an exception.
+    *
+    * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+    *
+    * @param \Exception $exception
+    *
+    * @return void
+    */
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
 
     /**
-     * Render an exception into an HTTP response.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Exception               $exception
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Render an exception into an HTTP response.
+    *
+    * @param \Illuminate\Http\Request $request
+    * @param \Exception               $exception
+    *
+    * @return \Illuminate\Http\Response
+    */
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
     }
 
     /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param \Illuminate\Http\Request                 $request
-     * @param \Illuminate\Auth\AuthenticationException $exception
-     *
-     * @return \Illuminate\Http\Response
-     */
+    * Convert an authentication exception into an unauthenticated response.
+    *
+    * @param \Illuminate\Http\Request                 $request
+    * @param \Illuminate\Auth\AuthenticationException $exception
+    *
+    * @return \Illuminate\Http\Response
+    */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
@@ -65,4 +65,14 @@ class Handler extends ExceptionHandler
 
         return redirect()->guest('login');
     }
+
+    protected function whoopsHandler()
+    {
+        try {
+            return app(\Whoops\Handler\HandlerInterface::class);
+        } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+            return parent::whoopsHandler();
+        }
+    }
+
 }
