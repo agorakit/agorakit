4b311a5737f6b7de41556a48a5b0e978e53954cf
James Moore <jamesm0025@gmail.com>
2019-10-05 14:07:06 -0400

    when a user gets a 403 error it will redirect them to the login page instead of the 403 page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception->getStatusCode() == 403) {
+            return redirect('/login'); // this will be on a 403 exception
+        }
+
         return parent::render($request, $exception);
     }
 
