4a0365ab11173d5445b8c58223faca42947a3331
Philippe Jadin <philippe.jadin@gmail.com>
2019-10-14 20:47:16 +0200

    update reset password forms

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,41 +1,36 @@
-@extends('app')
+
+@extends('dialog')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{trans('messages.change_my_password')}}</div>
-				<div class="panel-body">
-					@if (session('status'))
-						<div class="alert alert-success">
-							{{ session('status') }}
-						</div>
-					@endif
+	<div class="container-fluid">
+
+		<h1>{{trans('messages.change_my_password')}}</h1>
 
 
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{ trans('messages.email') }}</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" required="required"  value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									{{ trans('messages.send_my_recover_email') }}
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
+		@if (session('status'))
+			<div class="alert alert-success">
+				{{ session('status') }}
 			</div>
-		</div>
+		@endif
+
+		<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/email') }}">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+			<div class="form-group">
+				<label> {{ trans('messages.email') }}</label>
+
+					<input type="email" class="form-control" name="email" required="required"  value="{{ old('email') }}">
+
+			</div>
+
+			<div class="form-group">
+
+					<button type="submit" class="btn btn-primary">
+						{{ trans('messages.send_my_recover_email') }}
+					</button>
+
+			</div>
+		</form>
 	</div>
-</div>
+
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,50 +1,38 @@
-@extends('app')
+@extends('dialog')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">{{ trans('messages.password_reset') }}</div>
-				<div class="panel-body">
+
+	<h1>{{ trans('messages.password_reset') }}</h1>
 
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<input type="hidden" name="token" value="{{ $token }}">
+	<form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+		<input type="hidden" name="_token" value="{{ csrf_token() }}">
+		<input type="hidden" name="token" value="{{ $token }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{ trans('messages.mail') }}</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+		<div class="form-group">
+			<label>{{ trans('messages.mail') }}</label>
+			<input type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{ trans('messages.password') }}</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+		</div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">{{ trans('messages.confirm_password') }}</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+		<div class="form-group">
+			<label>{{ trans('messages.password') }}</label>
+			<input type="password" class="form-control" name="password" required>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-								{{ trans('messages.change_my_password') }}
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
+		</div>
+
+		<div class="form-group">
+			<label>{{ trans('messages.confirm_password') }}</label>
+			<input type="password" class="form-control" name="password_confirmation" required>
+		</div>
+
+		<div class="form-group">
+			<div>
+				<button type="submit" class="btn btn-primary">
+					{{ trans('messages.change_my_password') }}
+				</button>
 			</div>
 		</div>
-	</div>
-</div>
+	</form>
+
 @endsection
