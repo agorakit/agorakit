065bba9891155eadaec6ade862c4d335b6cb7c4a
Philippe Jadin <philippe.jadin@gmail.com>
2020-04-03 14:21:22 +0200

    laravel7 wip

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,77 +2,54 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
-    * A list of the exception types that should not be reported.
-    *
-    * @var array
-    */
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
     protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
+        //
     ];
 
     /**
-    * Report or log an exception.
-    *
-    * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-    *
-    * @param \Exception $exception
-    *
-    * @return void
-    */
-    public function report(Exception $exception)
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Throwable  $exception
+     * @return void
+     *
+     * @throws \Exception
+     */
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
     /**
-    * Render an exception into an HTTP response.
-    *
-    * @param \Illuminate\Http\Request $request
-    * @param \Exception               $exception
-    *
-    * @return \Illuminate\Http\Response
-    */
-    public function render($request, Exception $exception)
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
-
-    /**
-    * Convert an authentication exception into an unauthenticated response.
-    *
-    * @param \Illuminate\Http\Request                 $request
-    * @param \Illuminate\Auth\AuthenticationException $exception
-    *
-    * @return \Illuminate\Http\Response
-    */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest('login');
-    }
-
-    protected function whoopsHandler()
-    {
-        try {
-            return app(\Whoops\Handler\HandlerInterface::class);
-        } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
-            return parent::whoopsHandler();
-        }
-    }
-
 }
