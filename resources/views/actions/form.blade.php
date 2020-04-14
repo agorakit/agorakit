e7b70b7a257ae7d04a1b1d2559376b59deacffe4
Philippe Jadin <philippe.jadin@gmail.com>
2020-04-14 15:12:37 +0200

    remove required from actions form to allow ckeditor to work

--- resources/views/actions/form.blade.php
+++ resources/views/actions/form.blade.php
@@ -7,7 +7,6 @@
     {!! Form::label('body', trans('messages.description')) !!}
     {!! Form::textarea('body', null, [
         'class' => 'form-control wysiwyg' ,
-        'required',
         'data-mention-files' => route('groups.files.mention', $group),
         'data-mention-discussions' => route('groups.discussions.mention', $group),
         'data-mention-users' => route('groups.users.mention', $group)
