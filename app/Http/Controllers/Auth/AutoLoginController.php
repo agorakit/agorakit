d086b55305071123e6dff22a30c7686d53ff4b14
Philippe Jadin <philippe.jadin@gmail.com>
2020-04-15 22:37:12 +0200

    verify user on autologin

--- app/Http/Controllers/Auth/AutoLoginController.php
+++ app/Http/Controllers/Auth/AutoLoginController.php
@@ -14,12 +14,17 @@ class AutoLoginController extends Controller
         if (! $request->hasValidSignature()) {
             abort(401, 'Invalid or expired signature');
         }
-        $user = User::where('username', $username)->first();
+        $user = User::where('username', $username)->firstOrFail();
         Auth::login($user, true);
+
+        $user->verified = 1;
+        $user->save();
 
         if ($request->get('redirect')) {
             return redirect($request->get('redirect'));
         }
+
+        return redirect('/');
     }
 
 }
