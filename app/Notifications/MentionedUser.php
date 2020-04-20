d8b26ebb1f9bb142e853d86d8fbc93bb0482f219
Philippe Jadin <philippe.jadin@gmail.com>
2020-04-20 17:05:12 +0200

    send notifications from discussion inbox if available

--- app/Notifications/MentionedUser.php
+++ app/Notifications/MentionedUser.php
@@ -13,10 +13,10 @@ class MentionedUser extends Notification
     use Queueable;
 
     /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
+    * Create a new notification instance.
+    *
+    * @return void
+    */
     public function __construct(Comment $comment, User $user)
     {
         $this->comment = $comment;
@@ -24,47 +24,57 @@ class MentionedUser extends Notification
     }
 
     /**
-     * Get the notification's delivery channels.
-     *
-     * @param mixed $notifiable
-     *
-     * @return array
-     */
+    * Get the notification's delivery channels.
+    *
+    * @param mixed $notifiable
+    *
+    * @return array
+    */
     public function via($notifiable)
     {
         return ['mail', 'database'];
     }
 
     /**
-     * Get the mail representation of the notification.
-     *
-     * @param mixed $notifiable
-     *
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
+    * Get the mail representation of the notification.
+    *
+    * @param mixed $notifiable
+    *
+    * @return \Illuminate\Notifications\Messages\MailMessage
+    */
     public function toMail($notifiable)
     {
-        return (new MailMessage())
-        ->from(config('mail.noreply'), config('mail.from.name'))
+
+        $message =  (new MailMessage())
         ->subject('['.$this->comment->discussion->group->name.'] '.trans('messages.you_have_been_mentionned_by').' '.$this->user->name)
         ->line(trans('messages.you_have_been_mentionned_by').' '.$this->user->name.' '.trans('messages.in_the_discussion').' '.$this->comment->discussion->name.' : ')
-        ->line($this->comment->body)
+        ->line(html_entity_decode(strip_tags(filter($this->comment->body))))
         ->action(trans('messages.reply'), route('groups.discussions.show', [$this->comment->discussion->group, $this->comment->discussion]))
         ->line(trans('messages.dont_reply_to_this_email'));
+
+        // send notification directly from discussion inbox if there is one
+        if ($this->comment->discussion->inbox()) {
+            $message->from($this->comment->discussion->inbox(), $this->comment->discussion->group->name);
+        }
+        else {
+            $message->from(config('mail.noreply'), config('mail.from.name'));
+        }
+
+        return $message;
     }
 
     /**
-     * Get the array representation of the notification.
-     *
-     * @param mixed $notifiable
-     *
-     * @return array
-     */
+    * Get the array representation of the notification.
+    *
+    * @param mixed $notifiable
+    *
+    * @return array
+    */
     public function toArray($notifiable)
     {
         return [
-          'comment' => $this->comment->toArray(),
-          'user'    => $this->user->toArray(),
+            'comment' => $this->comment->toArray(),
+            'user'    => $this->user->toArray(),
         ];
     }
 }
