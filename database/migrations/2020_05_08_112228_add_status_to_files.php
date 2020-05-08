2c47ceba27110367de9c846fad6c9a47081e77d5
Boris Hermans <boris.her@gmail.com>
2020-05-08 14:08:28 +0200

    Add migrations to add new status columns
    Signed-off-by: Boris Hermans <boris.her@gmail.com>

---
+++ database/migrations/2020_05_08_112228_add_status_to_files.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Traits\ContentStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToFiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->integer('status')->default(ContentStatus::NORMAL);
+            $table->index('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
---
+++ database/migrations/2020_05_08_123537_add_status_to_groups.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Traits\ContentStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->integer('status')->default(ContentStatus::NORMAL);
+            $table->index('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
