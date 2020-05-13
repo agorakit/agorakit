388ccb8edc09fb97634309911a9d22a92f3af068
Philippe Jadin <philippe.jadin@gmail.com>
2020-05-13 22:43:04 +0200

    image max width 100% everywhere

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -13,6 +13,8 @@
         <span class="navbar-toggler-icon"></span>
     </button>
 
+    <a href="groups" up-drawer=".main-container">Test menu</a>
+
     <div class="collapse navbar-collapse" id="agorakit_navbar">
 
         <div class="navbar-nav mr-auto">
