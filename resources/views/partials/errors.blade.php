0353ae1eaaec9e590d828984ed42e0a66e8520b0
Philippe Jadin <philippe.jadin@gmail.com>
2020-05-21 22:58:06 +0200

    better errors, always shown

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,57 +1,53 @@
-@if (isset(Auth::user()->verified) && (Auth::user()->verified == 0))
-  <div class="alert alert-primary" role="alert">
-    <h4 class="alert-heading"><i class="fa fa-envelope-open-text"></i> {{__('Please verify your email address')}}</h4>
-    {{trans('messages.email_not_verified')}}
-    <br/>
-    <a class="alert-link" href="{{route('users.sendverification', Auth::user())}}">{{trans('messages.email_not_verified_send_again_verification')}}</a>
-  </div>
-@endif
-
-@if (Auth::user() && Auth::user()->invites->count() > 0)
-  <div class="alert alert-primary" role="alert">
-    <h4 class="alert-heading"><i class="fa fa-hand-point-right"></i>
-      {{__('You have pending group invites')}}
-    </h4>
-
-    <a class="alert-link" href="{{route('invites.index')}}" up-modal=".dialog">
-      {{__('Click here to accept or deny the invitation(s)')}}</a>
-  </div>
-@endif
+<div class="alerts" style="position: sticky; top: 3rem; width: 100%; z-index: 2000" up-hungry>
 
 
-@if (count($errors) > 0)
-  <div class="alert alert-danger" role="alert">
-    <strong><i class="fa fa-exclamation-triangle"></i>{{ trans('messages.howdy') }}</strong> {{ trans('messages.something_wrong') }}<br><br>
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  </div>
-@endif
-
-@if ( Session::has('message') )
-  <div class="alert alert-primary" role="alert">
-    <i class="fa fa-info-circle"></i>
-    {!! Session::get('message') !!}
-  </div>
-@endif
-
-
-@if ( Session::has('error') )
-  <div class=" alert alert-danger" role="alert">
-    <i class="fa fa-exclamation-triangle"></i>
-    {{ Session::get('error') }}
-  </div>
-@endif
-
-
-@if ( Session::has('messages') )
-  @foreach (session('messages') as $message)
+  @if (isset(Auth::user()->verified) && (Auth::user()->verified == 0))
     <div class="alert alert-primary" role="alert">
-      <i class="fa fa-info-circle"></i>
-      {!!$message!!}
-      <?php session()->pull('messages'); ?>
+      <h4 class="alert-heading">
+        <i class="fa fa-envelope-open-text"></i> {{__('Please verify your email address')}}
+      </h4>
+      {{trans('messages.email_not_verified')}}
+      <br/>
+      <a class="alert-link" href="{{route('users.sendverification', Auth::user())}}">
+        {{trans('messages.email_not_verified_send_again_verification')}}
+      </a>
     </div>
-  @endforeach
-@endif
+  @endif
+
+  @if (Auth::user() && Auth::user()->invites->count() > 0)
+    <div class="alert alert-primary" role="alert">
+      <h4 class="alert-heading"><i class="fa fa-hand-point-right"></i>
+        {{__('You have pending group invites')}}
+      </h4>
+
+      <a class="alert-link" href="{{route('invites.index')}}" up-modal=".dialog">
+        {{__('Click here to accept or deny the invitation(s)')}}
+      </a>
+    </div>
+  @endif
+
+
+
+  @if ( Session::has('messages') )
+    @foreach (session('messages') as $message)
+      <div class="alert alert-primary" role="alert">
+        {!!$message!!}
+        <?php session()->pull('messages'); ?>
+      </div>
+    @endforeach
+  @endif
+
+
+
+  @if ($errors->any())
+    @foreach ($errors->all() as $error)
+      <div class="alert alert-danger" role="alert">
+        {{ $error }}
+      </div>
+    @endforeach
+  @endif
+
+
+
+
+</div>
