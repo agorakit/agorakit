00d2f1ce07ce9b907a09dd376553060f16cf90ad
Philippe Jadin <philippe.jadin@gmail.com>
2020-05-21 15:23:36 +0200

    rollback purgecss (some classes were removed while still needed, tooltips and dropdown menu for instance)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,13 @@
 let mix = require('laravel-mix');
 
-require('laravel-mix-purgecss');
+//require('laravel-mix-purgecss');
 
 
 mix.js('resources/js/app.js', 'public/js')
 
 
-mix.sass('resources/sass/app.scss', 'public/css')
-.purgeCss();
+mix.sass('resources/sass/app.scss', 'public/css');
+//.purgeCss();
 
 mix.browserSync({proxy: '127.0.0.1:8000'});
 
