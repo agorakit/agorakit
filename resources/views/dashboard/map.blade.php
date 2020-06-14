40020966ff729215db3cab6ac95ddd20f56e820c
Philippe Jadin <philippe.jadin@gmail.com>
2020-06-14 23:47:36 +0200

    the return of leaflet wip

--- resources/views/dashboard/map.blade.php
+++ resources/views/dashboard/map.blade.php
@@ -1,13 +1,18 @@
 @extends('app')
 
 @section('head')
-  <script src='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.js'></script>
-  <link href='https://api.mapbox.com/mapbox.js/v3.1.1/mapbox.css' rel='stylesheet' />
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+  integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+  crossorigin=""/>
+  <!-- Make sure you put this AFTER Leaflet's CSS -->
+  <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+  integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+  crossorigin=""></script>
 @endsection
 
 @section('css')
   <style>
-  #map { height: 600px; }
+  #mapid { height: 600px; }
 </style>
 @endsection
 
@@ -15,15 +20,37 @@
 
   <script>
 
-  L.mapbox.accessToken = '{{ config('map.mapbox_token', 'set_your_access_token_in_dot_env') }}';
-  var map = L.mapbox.map('map', 'mapbox.streets');
-
-  var myLayer = L.mapbox.featureLayer()
-  .loadURL('map.geojson')
-  .on('ready', function() {
-    map.fitBounds(myLayer.getBounds());
+  // load all our points using geojson
+  // taken from https://medium.com/@maptastik/loading-external-geojson-a-nother-way-to-do-it-with-jquery-c72ae3b41c01
+  var points = $.ajax({
+    url:"{{ route('map.geojson') }}",
+    dataType: "json",
+    success: console.log("points data successfully loaded."),
+    error: function (xhr) {
+      alert(xhr.statusText)
+    }
   })
-  .addTo(map);
+
+
+  // When loading is complete :
+  $.when(points).done(function() {
+    var map = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Add requested external GeoJSON to map
+    var kyCounties = L.geoJSON(points.responseJSON).addTo(map);
+
+    // todo get lat and long fonr the json
+    var bounds = new L.LatLngBounds(points.responseJSON);
+
+    map.fitBounds(bounds);
+
+  });
+
+
+
 
 </script>
 @endsection
@@ -48,7 +75,7 @@
 
 
 
-  <div id="map"></div>
+  <div id="mapid"></div>
 
 
 
