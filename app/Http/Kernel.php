c68972ba3a82c56040ed60f8b02eaeeb9ae2a4b2
Philippe Jadin <philippe.jadin@gmail.com>
2020-10-28 14:50:37 +0100

    always handle user preferences changes (via middleware)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLocale::class,
+            \App\Http\Middleware\HandleUserPreference::class
         ],
 
         'api' => [
