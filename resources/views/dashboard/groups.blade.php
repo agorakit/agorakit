08dc29616c6d0ac0665f4181e648a647716f6358
Philippe Jadin <philippe.jadin@gmail.com>
2020-10-29 21:29:22 +0100

    trying to fix pagination number of links to no avail

--- resources/views/dashboard/groups.blade.php
+++ resources/views/dashboard/groups.blade.php
@@ -27,12 +27,13 @@
 
 <div class="groups">
     @if($groups)
-        {!! $groups->appends(request()->query())->links() !!}
+        {!! $groups->links() !!}
         <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-4">
             @foreach($groups as $group)
                 @include('groups.group')
             @endforeach
         </div>
+        {!! $groups->links() !!}
     @else
         <div class="alert alert-info" role="alert">
             {{ trans('messages.nothing_yet') }}
@@ -43,7 +44,7 @@
 
 
 
-{!! $groups->appends(request()->query())->links() !!}
+
 
 
 
