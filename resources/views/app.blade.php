ef0352f64f09253fa8f7c364eb1276b988136ab9
Philippe Jadin <philippe.jadin@gmail.com>
2020-11-02 09:37:27 +0100

    remove uneeded popper js (already included by bootstrap it seems)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -63,7 +63,6 @@
     </footer>
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="{{ asset('js/unpoly.js') }}"></script>
     <script src="{{ asset('js/fullcalendar.js') }}"></script>
