2333eeed79acdcdb7850c97002753df228bff6e7
Philippe Jadin <philippe.jadin@gmail.com>
2020-11-16 07:34:35 +0100

    settings page wip

--- resources/views/groups/form.blade.php
+++ resources/views/groups/form.blade.php
@@ -1,3 +1,5 @@
+<h2>{{trans('groups.basic_group_information_title')}}</h2>
+
 <div class="form-group">
     {!! Form::label('name', trans('group.name')) !!}
     {!! Form::text('name', null, ['class' => 'form-control', 'required']) !!}
@@ -17,15 +19,21 @@
 
 <div class="form-group">
     {!! Form::label('address', trans('messages.address') . ':') !!}
-    {!! Form::text('address', null, ['class' => 'form-control']) !!}
-    <div class="alert alert-info mt-2">
-        <small>{{trans('messages.address_privacy_and_help')}}</small>
+    <div class="small-help">
+        <i class="fas fa-info-circle"></i>
+        {{trans('messages.address_privacy_and_help')}}
     </div>
-
+    {!! Form::text('address', null, ['class' => 'form-control']) !!}
 </div>
+
+<h2>{{trans('groups.privacy_title')}}</h2>
 
 <div class="form-group">
     {!! Form::label('group_type', trans('group.type')) !!}
+     <div class="small-help">
+            <i class="fas fa-info-circle"></i>
+            {{trans('groups.group_type_help')}}
+        </div>
     @if (setting('user_can_create_secret_groups') == 1 || Auth::user()->isAdmin())
         {!! Form::select('group_type', ['0' => trans('group.open'), '1' => trans('group.closed'), '2' => trans('group.secret')], null, ['class' => 'form-control']) !!}
     @else
@@ -36,6 +44,10 @@
 @can('changeGroupStatus', $group)
     <div class="form-group">
         {!! Form::label('status', trans('group.status')) !!}
+         <div class="small-help">
+            <i class="fas fa-info-circle"></i>
+            {{trans('groups.status_help')}}
+        </div>
         {!! Form::select('status', ['0' => '', '10' => trans('group.pinned'), '-10' => trans('group.archived')], null, ['class' => 'form-control']) !!}
     </div>
 @endcan
