2d9ff4863daad8524fa020e79447239cd2eada0d
Philippe Jadin <philippe.jadin@gmail.com>
2020-11-18 20:31:48 +0100

    dicussion & file small improvements on mobile

--- resources/views/discussions/discussion.blade.php
+++ resources/views/discussions/discussion.blade.php
@@ -3,110 +3,111 @@
     <img class="h-12 w-12 rounded-full object-cover mx-1  flex-shrink-0"
         src="{{ route('users.cover', [$discussion->user, 'small']) }}" />
 
-    <div class="mx-2 flex-grow">
+    <div class="mx-2 min-w-0 flex-grow">
 
-        <div class="text-gray-900 text-sm sm:text-base">
-            <a
-                href="{{ route('groups.discussions.show', [$discussion->group, $discussion]) }}">
-                {{ summary($discussion->name) }}
-            </a>
+
+        <div class="text-gray-900 text-lg truncate min-w-0">
+        <a 
+            href="{{ route('groups.discussions.show', [$discussion->group, $discussion]) }}">
+            {{ summary($discussion->name) }}
+        </a>
         </div>
 
 
-        @if($discussion->getSelectedTags()->count() > 0)
-            <div class="text-gray-600 text-xs overflow-hidden my-1 h-5">
-                @foreach($discussion->getSelectedTags() as $tag)
-                    @include('tags.tag')
-                @endforeach
-            </div>
-        @endif
-
 
         <div class="text-gray-600 text-xs">
+            {{ trans('messages.started_by') }}
             {{ $discussion->user->name }}
             {{ trans('messages.in') }}
             {{ $discussion->group->name }}
             {{ $discussion->updated_at->diffForHumans() }}
         </div>
 
-         <div class="text-gray-800">
-            {{ summary($discussion->body, 100) }}
+        <div class="text-gray-600 truncate min-w-0 h-6">
+            {{ summary($discussion->body) }}
         </div>
+
+        @if($discussion->getSelectedTags()->count() > 0)
+        <div class="text-gray-600 text-xs overflow-hidden h-5">
+            @foreach($discussion->getSelectedTags() as $tag)
+            @include('tags.tag')
+            @endforeach
+        </div>
+        @endif
 
     </div>
 
     @if($discussion->unReadCount() > 0)
-        <div class="rounded-full bg-red-700 text-xs text-red-100 justify-center px-2 py-1 flex flex-shrink-0">
-            {{ $discussion->unReadCount() }} {{trans('messages.new')}}
-        </div>
+    <div class="rounded-full bg-red-700 text-xs text-red-100 justify-center px-2 py-1 flex flex-shrink-0">
+        {{ $discussion->unReadCount() }} {{trans('messages.new')}}
+    </div>
     @else
-        @if($discussion->comments_count > 0)
-            <div class="rounded-full bg-gray-700 text-xs text-gray-100 px-2 py-1 justify-center flex flex-shrink-0">
-                {{ $discussion->comments_count }}
-            </div>
-        @endif
+    @if($discussion->comments_count > 0)
+    <div class="rounded-full bg-gray-700 text-xs text-gray-100 px-2 py-1 justify-center flex flex-shrink-0">
+        {{ $discussion->comments_count }}
+    </div>
+    @endif
     @endif
 
 
     @can('update', $discussion)
 
-        <div class="dropdown ml-4">
-            <a class="text-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                aria-expanded="false">
-                <i class="fas fa-ellipsis-h"></i>
+    <div class="dropdown ml-4">
+        <a class="text-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-ellipsis-h"></i>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+
+            @can('update', $discussion)
+            <a class="dropdown-item" href="{{ route('groups.discussions.edit', [$discussion->group, $discussion]) }}">
+                <i class="fa fa-pencil"></i>
+                {{ trans('messages.edit') }}
             </a>
-
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-
-                @can('update', $discussion)
-                    <a class="dropdown-item"
-                        href="{{ route('groups.discussions.edit', [$discussion->group, $discussion]) }}">
-                        <i class="fa fa-pencil"></i>
-                        {{ trans('messages.edit') }}
-                    </a>
-                @endcan
+            @endcan
 
 
-                @can('delete', $discussion)
-                    <a up-modal=".dialog" class="dropdown-item"
-                        href="{{ route('groups.discussions.deleteconfirm', [$discussion->group, $discussion]) }}">
-                        <i class="fa fa-trash"></i>
-                        {{ trans('messages.delete') }}
-                    </a>
-                @endcan
+            @can('delete', $discussion)
+            <a up-modal=".dialog" class="dropdown-item"
+                href="{{ route('groups.discussions.deleteconfirm', [$discussion->group, $discussion]) }}">
+                <i class="fa fa-trash"></i>
+                {{ trans('messages.delete') }}
+            </a>
+            @endcan
 
-                @can('pin', $discussion)
-                    <a class="dropdown-item" up-follow up-cache="false" up-restore-scroll="true"
-                        href="{{ route('groups.discussions.pin', [$discussion->group, $discussion]) }}">
-                        <i class="fa fa-thumbtack"></i>
-                        @if($discussion->isPinned())
-                            {{ trans('messages.unpin') }}
-                        @else
-                            {{ trans('messages.pin') }}
-                        @endif
-                    </a>
-                @endcan
+            @can('pin', $discussion)
+            <a class="dropdown-item" up-follow up-cache="false" up-restore-scroll="true"
+                href="{{ route('groups.discussions.pin', [$discussion->group, $discussion]) }}">
+                <i class="fa fa-thumbtack"></i>
+                @if($discussion->isPinned())
+                {{ trans('messages.unpin') }}
+                @else
+                {{ trans('messages.pin') }}
+                @endif
+            </a>
+            @endcan
 
-                @can('archive', $discussion)
-                    <a class="dropdown-item" up-follow up-cache="false" up-restore-scroll="true"
-                        href="{{ route('groups.discussions.archive', [$discussion->group, $discussion]) }}">
-                        <i class="fa fa-archive"></i>
-                        @if($discussion->isArchived())
-                            {{ trans('messages.unarchive') }}
-                        @else
-                            {{ trans('messages.archive') }}
-                        @endif
-                    </a>
-                @endcan
+            @can('archive', $discussion)
+            <a class="dropdown-item" up-follow up-cache="false" up-restore-scroll="true"
+                href="{{ route('groups.discussions.archive', [$discussion->group, $discussion]) }}">
+                <i class="fa fa-archive"></i>
+                @if($discussion->isArchived())
+                {{ trans('messages.unarchive') }}
+                @else
+                {{ trans('messages.archive') }}
+                @endif
+            </a>
+            @endcan
 
-                
-                    <a class="dropdown-item"
-                        href="{{ route('groups.discussions.history', [$discussion->group, $discussion]) }}"><i
-                            class="fa fa-history"></i> {{ trans('messages.show_history') }}</a>
-                
-            </div>
+
+            <a class="dropdown-item"
+                href="{{ route('groups.discussions.history', [$discussion->group, $discussion]) }}"><i
+                    class="fa fa-history"></i> {{ trans('messages.show_history') }}</a>
 
         </div>
+
+    </div>
     @endcan
 
 
--- resources/views/files/file.blade.php
+++ resources/views/files/file.blade.php
@@ -1,15 +1,16 @@
-<div class="py-3 border-gray-300 border-b flex @if ($file->isArchived()) status-archived @endif  @if ($file->isPinned()) status-pinned @endif" up-expand>
-    
-        @if ($file->isLink())
-            <a class="mr-4 flex-shrink-0" href="{{ route('groups.files.download', [$file->group, $file]) }}" target="_blank">
-                <img class="rounded w-12 h-12" src="{{ route('groups.files.thumbnail', [$file->group, $file]) }}"/>
-            </a>
-        @else
-            <a class="mr-4 flex-shrink-0" up-follow href="{{ route('groups.files.show', [$file->group, $file]) }}">
-                <img class="rounded w-12 h-12" src="{{ route('groups.files.thumbnail', [$file->group, $file]) }}"/>
-            </a>
-        @endif
-    
+<div class="py-3 border-gray-300 border-b flex @if ($file->isArchived()) status-archived @endif  @if ($file->isPinned()) status-pinned @endif"
+    up-expand>
+
+    @if ($file->isLink())
+    <a class="mr-4 flex-shrink-0" href="{{ route('groups.files.download', [$file->group, $file]) }}" target="_blank">
+        <img class="rounded w-12 h-12" src="{{ route('groups.files.thumbnail', [$file->group, $file]) }}" />
+    </a>
+    @else
+    <a class="mr-4 flex-shrink-0" up-follow href="{{ route('groups.files.show', [$file->group, $file]) }}">
+        <img class="rounded w-12 h-12" src="{{ route('groups.files.thumbnail', [$file->group, $file]) }}" />
+    </a>
+    @endif
+
 
     <div class="w-100 flex-grow">
         <div class="">
@@ -19,40 +20,34 @@
             </a>
         </div>
 
-    
-        @if ($file->getSelectedTags()->count() > 0)
-        <div class="mb-2">
-                @foreach ($file->getSelectedTags() as $tag)
-                    @include('tags.tag')
-                @endforeach
-                </div>
-            @endif
 
-            
+       
+
+
 
         <div class="text-xs text-gray-600">
             <div>
                 @if ($file->isPinned())
-                    <div class="badge badge-primary" style="min-width: 2em; margin: 0 2px;">
-                        <i class="fas fa-thumbtack"></i>
-                        {{__('Pinned')}}
-                    </div>
+                <div class="badge badge-primary" style="min-width: 2em; margin: 0 2px;">
+                    <i class="fas fa-thumbtack"></i>
+                    {{__('Pinned')}}
+                </div>
                 @endif
                 @if ($file->isArchived())
-                    <div class="badge badge-secondary" style="min-width: 2em; margin: 0 2px;">
-                        <i class="fa fa-archive"></i>
-                        {{__('Archived')}}
-                    </div>
+                <div class="badge badge-secondary" style="min-width: 2em; margin: 0 2px;">
+                    <i class="fa fa-archive"></i>
+                    {{__('Archived')}}
+                </div>
                 @endif
             </div>
             <div>
                 <a up-follow href="{{ route('groups.show', [$file->group_id]) }}">
                     @if ($file->group->isOpen())
-                        <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
+                    <i class="fa fa-globe" title="{{trans('group.open')}}"></i>
                     @elseif ($file->group->isClosed())
-                        <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
+                    <i class="fa fa-lock" title="{{trans('group.closed')}}"></i>
                     @else
-                        <i class="fa fa-eye-slash" title="{{trans('group.secret')}}"></i>
+                    <i class="fa fa-eye-slash" title="{{trans('group.secret')}}"></i>
                     @endif
                     {{ $file->group->name }}
                 </a>
@@ -70,64 +65,77 @@
 
             <div>
                 @if ($file->isFile())
-                    <i class="fa fa-database"></i> {{sizeForHumans($file->filesize)}}
+                <i class="fa fa-database"></i> {{sizeForHumans($file->filesize)}}
                 @endif
             </div>
             <div>
                 @if ($file->isLink())
-                    <i class="fas fa-link"></i> {{$file->path}}
+                <i class="fas fa-link"></i> {{$file->path}}
                 @endif
             </div>
+
+            @if ($file->getSelectedTags()->count() > 0)
+            <div class="">
+            <i class="fa fa-tags"></i>
+            @foreach ($file->getSelectedTags() as $tag)
+            @include('tags.tag')
+            @endforeach
+            </div>
+            @endif
+
         </div>
 
     </div>
 
     @can('update', $file)
-        <div class="ml-auto dropdown">
-            <a class="text-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-ellipsis-h"></i>
+    <div class="ml-auto dropdown">
+        <a class="text-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <i class="fas fa-ellipsis-h"></i>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+
+            @can('update', $file)
+            <a class="dropdown-item" href="{{ route('groups.files.edit', [$file->group, $file]) }}">
+                <i class="fa fa-pencil"></i>
+                {{trans('messages.edit')}}
             </a>
+            @endcan
 
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+            @can('delete', $file)
+            <a class="dropdown-item" href="{{ route('groups.files.deleteconfirm', [$file->group, $file]) }}">
+                <i class="fa fa-trash"></i>
+                {{trans('messages.delete')}}
+            </a>
+            @endcan
 
-                @can('update', $file)
-                    <a class="dropdown-item" href="{{ route('groups.files.edit', [$file->group, $file]) }}">
-                        <i class="fa fa-pencil"></i>
-                        {{trans('messages.edit')}}
-                    </a>
-                @endcan
+            @can('pin', $file)
+            <a up-follow up-cache="false" up-restore-scroll="true" class="dropdown-item"
+                href="{{ route('groups.files.pin', [$file->group, $file]) }}">
+                <i class="fa fa-thumbtack"></i>
+                @if($file->isPinned())
+                {{trans('messages.unpin')}}
+                @else
+                {{trans('messages.pin')}}
+                @endif
+            </a>
+            @endcan
 
-                @can('delete', $file)
-                    <a class="dropdown-item" href="{{ route('groups.files.deleteconfirm', [$file->group, $file]) }}">
-                        <i class="fa fa-trash"></i>
-                        {{trans('messages.delete')}}
-                    </a>
-                @endcan
+            @can('archive', $file)
+            <a up-follow up-cache="false" up-restore-scroll="true" class="dropdown-item"
+                href="{{ route('groups.files.archive', [$file->group, $file]) }}">
+                <i class="fa fa-archive"></i>
+                @if($file->isArchived())
+                {{trans('messages.unarchive')}}
+                @else
+                {{trans('messages.archive')}}
+                @endif
+            </a>
+            @endcan
 
-                @can('pin', $file)
-                    <a up-follow up-cache="false" up-restore-scroll="true" class="dropdown-item" href="{{ route('groups.files.pin', [$file->group, $file]) }}">
-                        <i class="fa fa-thumbtack"></i>
-                        @if($file->isPinned())
-                            {{trans('messages.unpin')}}
-                        @else
-                            {{trans('messages.pin')}}
-                        @endif
-                    </a>
-                @endcan
-
-                @can('archive', $file)
-                    <a up-follow up-cache="false" up-restore-scroll="true" class="dropdown-item" href="{{ route('groups.files.archive', [$file->group, $file]) }}">
-                        <i class="fa fa-archive"></i>
-                        @if($file->isArchived())
-                            {{trans('messages.unarchive')}}
-                        @else
-                            {{trans('messages.archive')}}
-                        @endif
-                    </a>
-                @endcan
-
-            </div>
         </div>
+    </div>
     @endcan
 
 
