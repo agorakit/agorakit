bddc3b9c518c23eea5f910c5e1ee79d9272b9bf5
Philippe Jadin <philippe.jadin@gmail.com>
2021-03-30 16:22:16 +0200

    toolbar

--- public/js/compilers.js
+++ public/js/compilers.js
@@ -22,6 +22,17 @@ up.compiler('.wysiwyg', function (element, data) {
 
 	$(element).summernote({
 
+		toolbar: [
+			['style', ['style']],
+			['font', ['bold', 'underline', 'clear']],
+			/*['fontname', ['fontname']],*/
+			['color', ['color']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['table', ['table']],
+			['insert', ['link', 'picture', 'video']],
+			['view', ['fullscreen', 'codeview', 'help']],
+		  ],
+		  
 		callbacks: {
 			onImageUpload: function (files) {
 				// upload image to server and create imgNode...
