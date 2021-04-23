7600d37e584e67ebed4865af535d58a4d09551ef
Philippe Jadin <philippe.jadin@gmail.com>
2021-04-23 13:57:33 +0200

    migrate DB to  utf8mb4, emoji support!

---
+++ database/migrations/2021_04_23_134040_update_charset.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCharset extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('ALTER TABLE `discussions` CONVERT TO CHARACTER SET utf8mb4');
+        DB::unprepared('ALTER TABLE `comments` CONVERT TO CHARACTER SET utf8mb4');
+        DB::unprepared('ALTER TABLE `revisions` CONVERT TO CHARACTER SET utf8mb4');
+        DB::unprepared('ALTER TABLE `actions` CONVERT TO CHARACTER SET utf8mb4');
+        DB::unprepared('ALTER TABLE `users` CONVERT TO CHARACTER SET utf8mb4');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('ALTER TABLE `discussions` CONVERT TO CHARACTER SET utf8');
+        DB::unprepared('ALTER TABLE `comments` CONVERT TO CHARACTER SET utf8');
+        DB::unprepared('ALTER TABLE `revisions` CONVERT TO CHARACTER SET utf8');
+        DB::unprepared('ALTER TABLE `actions` CONVERT TO CHARACTER SET utf8');
+        DB::unprepared('ALTER TABLE `users` CONVERT TO CHARACTER SET utf8');
+    }
+}
