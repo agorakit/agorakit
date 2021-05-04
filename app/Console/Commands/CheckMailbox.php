49007abe0e896b47883993e5be4e9d88587a8dc4
Philippe Jadin <philippe.jadin@gmail.com>
2021-05-04 22:02:52 +0200

    wip

--- app/Console/Commands/CheckMailbox.php
+++ app/Console/Commands/CheckMailbox.php
@@ -17,30 +17,9 @@ use Michelf\Markdown;
 use EmailReplyParser\EmailReplyParser;
 
 /*
-Inbound  Email handler for Agorakit
+Inbound Email Importer for Agorakit
 
-High level overview :
-
-1. Sending email to a group
-- The user exists
-- The group exists
-- The user is a member of the group
-
-2. Replying to a discussion
-- discussion exists
-- user is a member of the group
-
-In all cases the mail is processed
-Bounced to user in case of failure
-Moved to a folder on the imap server
-
-Emails are generated as follow :  
-
-[INBOX_PREFIX][group-slug][INBOX_SUFFIX]
-
-[INBOX_PREFIX]reply-[discussion-id][INBOX_SUFFIX]
-
-Prefix and suffix is defined in the .env file
+Everytime it's run, emails found in the defined inbox are imported in the messages table
 
 */
 
@@ -82,10 +61,6 @@ class CheckMailbox extends Command
      */
     public function handle()
     {
-        if ($this->option('debug')) {
-            $this->debug = true;
-        }
-
         if (config('agorakit.inbox_host')) {
             // open mailbox
 
@@ -96,7 +71,7 @@ class CheckMailbox extends Command
 
             $mailboxes = $this->connection->getMailboxes();
 
-            if ($this->debug) {
+            if ($this->option('debug')) {
                 foreach ($mailboxes as $mailbox) {
                     // Skip container-only mailboxes
                     // @see https://secure.php.net/manual/en/function.imap-getmailboxes.php
@@ -119,7 +94,7 @@ class CheckMailbox extends Command
             $i = 0;
             foreach ($messages as $mailbox_message) {
 
-                $this->line('-----------------------------------');
+                $this->debug('-----------------------------------');
 
                 // limit to 100 messages at a time (I did no find a better way to handle this iterator)
                 if ($i > 100) {
@@ -127,12 +102,9 @@ class CheckMailbox extends Command
                 }
                 $i++;
 
+                $this->debug('Processing email "' . $mailbox_message->getSubject() . '"');
+                $this->debug('From ' . $mailbox_message->getFrom()->getAddress());
 
-                // debug message info
-                if ($this->debug) {
-                    $this->line('Processing email "' . $mailbox_message->getSubject() . '"');
-                    $this->line('From ' . $mailbox_message->getFrom()->getAddress());
-                }
 
                 $message = new Message;
 
@@ -143,13 +115,6 @@ class CheckMailbox extends Command
                 $message->saveOrFail();
 
                 $this->moveMessage($mailbox_message, 'stored');
-                
-                /*
-                $message->user = $this->extractUserFromMessage($message);
-                $message->group = $this->extractGroupFromMessage($message);
-                $message->discussion = $this->extractDiscussionFromMessage($message);
-                */
-
             }
             $this->connection->expunge();
         } else {
@@ -172,185 +137,6 @@ class CheckMailbox extends Command
 
 
     /**
-     * Tries to find a valid user in the $message (using from: email header)
-     * Else returns a new user with the email already set
-     */
-    public function extractUserFromMessage(ImapMessage $message)
-    {
-        $user = User::where('email', $message->getFrom()->getAddress())->firstOrNew();
-        if (!$user->exists) {
-            $user->email = $message->getFrom()->getAddress();
-            $this->debug('User does not exist, created from: '  . $user->email);
-        } else {
-            $this->debug('User exists, email: '  . $user->email);
-        }
-
-        return $user;
-    }
-
-    // tries to find a valid group in the $message (using to: email header)
-    public function extractGroupFromMessage(ImapMessage $message)
-    {
-        $recipients = $this->extractRecipientsFromMessage($message);
-        
-        foreach ($recipients as $to_email) {
-            // remove prefix and suffix to get the slug we need to check against
-            $to_email = str_replace(config('agorakit.inbox_prefix'), '', $to_email);
-            $to_email = str_replace(config('agorakit.inbox_suffix'), '', $to_email);
-
-            $to_emails[] = $to_email;
-
-            $this->debug('(group candidate) to: '  . $to_email);
-        }
-
-        $group = Group::whereIn('slug', $to_emails)->first();
-
-
-        if ($group) {
-            $this->debug('group found : ' . $group->name . ' (' . $group->id . ')');
-            return $group;
-        }
-        $this->debug('group not found');
-        return false;
-    }
-
-
-    // tries to find a valid discussion in the $message (using to: email header and message content)
-    public function extractDiscussionFromMessage(ImapMessage $message)
-    {
-        $recipients = $this->extractRecipientsFromMessage($message);
-        
-        foreach ($recipients as $to_email) {
-            preg_match('#' . config('agorakit.inbox_prefix') . 'reply-(\d+)' . config('agorakit.inbox_prefix') . '#', $to_email, $matches);
-            //dd($matches);
-            if (isset($matches[1])) {
-                $discussion = Discussion::where('id', $matches[1])->first();
-
-
-                if ($discussion) {
-                    $this->debug('discussion found');
-                    return $discussion;
-                }
-            }
-        }
-
-        $this->debug('discussion not found');
-        return false;
-    }
-
-    /**
-     * Returns a rich text represenation of the email, stripping away all quoted text, signatures, etc...
-     */
-    function extractTextFromMessage(ImapMessage $message)
-    {
-        $body_html = $message->getBodyHtml(); // this is the raw html content
-        $body_text = nl2br(EmailReplyParser::parseReply($message->getBodyText()));
-
-
-        // count the number of caracters in plain text :
-        // if we really have less than 5 chars in there using plain text,
-        // let's post the whole html mess, 
-        // converted to markdown, 
-        // then stripped with the same EmailReplyParser, 
-        // then converted from markdown back to html, pfeeew
-        if (strlen($body_text) < 5) {
-            $converter = new HtmlConverter();
-            $markdown = $converter->convert($body_html);
-            $result = Markdown::defaultTransform(EmailReplyParser::parseReply($markdown));
-        } else {
-            $result = $body_text;
-        }
-
-        return $result;
-    }
-
-    /** 
-     * Returns all recipients form the message, in the to: and cc: fields
-     */
-    function extractRecipientsFromMessage(ImapMessage $message)
-    {
-        $recipients = [];
-        
-        foreach ($message->getTo() as $to) {
-            $recipients[] = $to->getAddress();
-        }
-
-        foreach ($message->getCc() as $to) {
-            $recipients[] = $to->getAddress();
-        }
-
-        return $recipients;
-
-    }
-
-    function parse_rfc822_headers(string $header_string): array
-    {
-        // Reference:
-        // * Base: https://stackoverflow.com/questions/5631086/getting-x-mailer-attribute-in-php-imap/5631445#5631445
-        // * Improved regex: https://stackoverflow.com/questions/5631086/getting-x-mailer-attribute-in-php-imap#comment61912182_5631445
-        preg_match_all(
-            '/([^:\s]+): (.*?(?:\r\n\s(?:.+?))*)\r\n/m',
-            $header_string,
-            $matches
-        );
-        $headers = array_combine($matches[1], $matches[2]);
-        return $headers;
-    }
-
-
-
-
-    /**
-     * Returns true if message is an autoreply or vacation auto responder
-     */
-    public function isMessageAutomated(ImapMessage $message)
-    {
-        /*
-        TODO Detect automatic messages and discard them, see here : https://www.arp242.net/autoreply.html
-        */
-
-        $message_headers = $this->parse_rfc822_headers($message->getRawHeaders());
-
-        if (array_key_exists('Auto-Submitted', $message_headers)) {
-            return true;
-        }
-
-        if (array_key_exists('X-Auto-Response-Suppress', $message_headers)) {
-            return true;
-        }
-
-        if (array_key_exists('List-Id', $message_headers)) {
-            return true;
-        }
-
-        if (array_key_exists('List-Unsubscribe', $message_headers)) {
-            return true;
-        }
-
-        if (array_key_exists('Feedback-ID', $message_headers)) {
-            return true;
-        }
-
-        if (array_key_exists('X-NIDENT', $message_headers)) {
-            return true;
-        }
-
-        if (array_key_exists('Delivered-To', $message_headers)) {
-            if ($message_headers['Delivered-To'] == 'Autoresponder') {
-                return true;
-            }
-        }
-
-        if (array_key_exists('X-AG-AUTOREPLY', $message_headers)) {
-            return true;
-        }
-
-
-
-        return false;
-    }
-
-    /**
      * Move the provided $message to a folder named $folder
      */
     public function moveMessage(ImapMessage $message, $folder)
@@ -369,69 +155,4 @@ class CheckMailbox extends Command
         return $message->move($folder);
     }
 
-
-
-
-    public function processGroupExistsAndUserIsMember(Group $group, User $user, ImapMessage $message)
-    {
-        $discussion = new Discussion();
-
-        $discussion->name = $message->getSubject();
-        $discussion->body = $this->extractTextFromMessage($message);
-
-        $discussion->total_comments = 1; // the discussion itself is already a comment
-        $discussion->user()->associate($user);
-
-        if ($group->discussions()->save($discussion)) {
-            // update activity timestamp on parent items
-            $group->touch();
-            $user->touch();
-            $this->info('Discussion has been created with id : ' . $discussion->id);
-            $this->info('Title : ' . $discussion->name);
-            Log::info('Discussion has been created from email', ['mail' => $message, 'discussion' => $discussion]);
-
-            $this->moveMessage($message, 'processed');
-            return true;
-        } else {
-            $this->moveMessage($message, 'discussion_not_created');
-            Mail::to($user)->send(new MailBounce($message, 'Your discussion could not be created in the group, maybe your message was empty'));
-            return false;
-        }
-    }
-
-
-    public function processDiscussionExistsAndUserIsMember(Discussion $discussion, User $user, ImapMessage $message)
-    {
-        $discussion->body = $this->extractTextFromMessage($message);
-
-        $comment = new \App\Comment();
-        $comment->body = $body;
-        $comment->user()->associate($user);
-        if ($discussion->comments()->save($comment)) {
-            $discussion->total_comments++;
-            $discussion->save();
-
-            // update activity timestamp on parent items
-            $discussion->group->touch();
-            $discussion->touch();
-            $this->moveMessage($message, 'processed');
-            return true;
-        } else {
-            $this->moveMessage($message, 'comment_not_created');
-            return false;
-        }
-    }
-
-
-    public function processGroupExistsButUserIsNotMember(Group $group, User $user, ImapMessage $message)
-    {
-        Mail::to($user)->send(new MailBounce($message, 'You are not member of ' . $group->name . ' please join the group first before posting'));
-        $this->moveMessage($message, 'bounced');
-    }
-
-
-    public function processGroupNotFoundUserNotFound(User $user, ImapMessage $message)
-    {
-        $this->moveMessage($message, 'group_not_found_user_not_found');
-    }
 }
--- app/Message.php
+++ app/Message.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Venturecraft\Revisionable\RevisionableTrait;
 use Watson\Validating\ValidatingTrait;
 
+use ZBateson\MailMimeParser\MailMimeParser;
+use ZBateson\MailMimeParser\Message as MailMessage;
+use ZBateson\MailMimeParser\Header\HeaderConsts;
+
 use App\User;
 use Auth;
 
@@ -49,5 +53,93 @@ class Message extends Model
     {
         return $this->belongsTo(\App\Discussion::class);
     }
+
+    /**
+     * Returns true if message is an autoreply or vacation auto responder
+     */
+    public function isAutomated() 
+    {
+        /*
+        Detect automatic messages and discard them, see here : https://www.arp242.net/autoreply.html
+        */
+
+        $message_headers = $this->headers();
+
+        dd($message_headers);
+
+        if (array_key_exists('Auto-Submitted', $message_headers)) {
+            return true;
+        }
+
+        if (array_key_exists('X-Auto-Response-Suppress', $message_headers)) {
+            return true;
+        }
+
+        if (array_key_exists('List-Id', $message_headers)) {
+            return true;
+        }
+
+        if (array_key_exists('List-Unsubscribe', $message_headers)) {
+            return true;
+        }
+
+        if (array_key_exists('Feedback-ID', $message_headers)) {
+            return true;
+        }
+
+        if (array_key_exists('X-NIDENT', $message_headers)) {
+            return true;
+        }
+
+        if (array_key_exists('Delivered-To', $message_headers)) {
+            if ($message_headers['Delivered-To'] == 'Autoresponder') {
+                return true;
+            }
+        }
+
+        if (array_key_exists('X-AG-AUTOREPLY', $message_headers)) {
+            return true;
+        }
+
+
+
+        return false;
+    }
+
+    
+
+
+    /**
+     * Parse headers of the raw email message and return everything as an array
+     */
+    function headers()
+    {
+        // Reference:
+        // * Base: https://stackoverflow.com/questions/5631086/getting-x-mailer-attribute-in-php-imap/5631445#5631445
+        // * Improved regex: https://stackoverflow.com/questions/5631086/getting-x-mailer-attribute-in-php-imap#comment61912182_5631445
+        preg_match_all(
+            '/([^:\s]+): (.*?(?:\r\n\s(?:.+?))*)\r\n/m',
+            $this->raw,
+            $matches
+        );
+        $headers = array_combine($matches[1], $matches[2]);
+        return $headers;
+    }
+
+
+    /**
+     * Returns a parsed representation of this message
+     * So you can : 
+     * $message->parse()->getTextContent(); 
+     * $message->parse()->getHtmlContent(); 
+     * 
+     * See https://github.com/zbateson/mail-mime-parser
+     * 
+     */
+    function parse()
+    {
+        return MailMessage::from($this->raw);
+    }
+
 
 }
