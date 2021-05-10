4853a96e7ba7d9e276be8ce26e2997d86a635952
Philippe Jadin <philippe.jadin@gmail.com>
2021-05-10 22:20:28 +0200

    getting there slooooowly

--- app/Reaction.php
+++ app/Reaction.php
@@ -9,6 +9,11 @@ class Reaction extends Model
 {
     protected $fillable = ['reactable_type', 'reactable_id', 'user_id', 'type'];
 
+
+    /**
+     * Adds a reaction to any model
+     * $reaction_type must be one of the allowed reactions in config
+     */
     public static function reactTo($model, $reaction_type)
     {
         $reaction = Reaction::firstOrNew([
@@ -17,11 +22,18 @@ class Reaction extends Model
             'user_id'     => Auth::user()->id,
         ]);
 
-        $reaction->type = $reaction_type;
+        if (in_array($reaction_type, setting()->getArray('reactions'))) {
+            $reaction->type = $reaction_type;
+        } else {
+            abort(500, 'invalid reaction type, check config');
+        }
 
         return $reaction->save();
     }
 
+    /**
+     * Remove reaction from model
+     */
     public static function unReactTo($model)
     {
         return Reaction::where('reactable_type', get_class($model))
@@ -29,18 +41,42 @@ class Reaction extends Model
             ->where('user_id', Auth::user()->id)->delete();
     }
 
+    /**
+     * returns a summary of all reactions to $model, grouped by reaction type
+     */
+    public static function summary($model)
+    {
+        return $model->reactions->groupBy('type');
+        
+        /*
+        return $model->reactions->groupBy('type')->map(function ($val) {
+            return $val->count();
+        });
+        */
+    }
+
+
+    /**
+     * Returns the model this reaction is related to
+     */
     public function reactable()
     {
         return $this->morphTo()->withTrashed();
     }
 
+    /**
+     * Returns the model this reaction is related to
+     * Alias of the reactable() function
+     */
     public function model()
     {
         return $this->morphTo(__FUNCTION__, 'reactable_type', 'reactable_id')->withTrashed();
     }
 
 
-
+    /**
+     * Returns the user who reacted
+     */
     public function user()
     {
         return $this->belongsTo(\App\User::class)->withTrashed();
--- resources/views/reactions/show.blade.php
+++ resources/views/reactions/show.blade.php
@@ -10,12 +10,8 @@
 
     <a href="{{route('reaction.store', ['model' => 'comment', 'id' => $model->id, 'reaction'=> $reaction->type])}}">
         <div class="flex">
-
-
             <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-8 w-8" />
-
             <div>
-
             </div>
 
         </div>
