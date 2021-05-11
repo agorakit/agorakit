d51d756f809305dbea97020d5a3e2eb81bf1c8a6
Philippe Jadin <philippe.jadin@gmail.com>
2021-05-11 22:33:13 +0200

    nicer icons and ui

--- resources/views/components/reactions.blade.php
+++ resources/views/components/reactions.blade.php
@@ -1,41 +1,43 @@
 @props(['model'])
 
-<div class="flex">
+<div class="flex items-center">
     <div id="model-{{$model->id}}">
+
         @foreach ($model->reactions as $reaction)
-
-
         @if (Auth::check() && Auth::user()->is($reaction->user))
         <a up-target="#model-{{$model->id}}" up-reveal="false"
             href="{{route('reaction.unreact', ['model' => $model->modelName, 'id' => $model->id])}}">
-            <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-8 w-8"
+            <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-8 w-8 mr-2 p-1"
                 title="{{ $reaction->user->name }} - click to remove" />
         </a>
         @else
 
-        <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-8 w-8"
+        <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-8 w-8 mr-2 p-1"
             title="{{ $reaction->user->name }}" />
 
         @endif
         @endforeach
+
     </div>
 
     @can('react', $model)
     <div class="dropdown">
-        <a class="rounded-full hover:bg-gray-400 w-10 h-10 flex items-center justify-center" type="button"
-            id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-h"></i>
+        <a class="cursor-pointer" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+            <img src="{{asset('/images/reactions/reaction.svg')}}" class="image-cover h-8 w-8 hover:bg-gray-300 rounded p-1" title="Add a reaction" />
         </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
 
+        <div class="dropdown-menu dropdown-menu-left grid-cols-3 p-2" aria-labelledby="dropdownMenuButton">
 
             @foreach (setting()->getArray('reactions') as $reaction)
-            <a class="dropdown-item" up-target="#model-{{$model->id}}" up-reveal="false"
-                class="bg-gray-300 rounded p-1 text-sm mr-2"
+            <a up-target="#model-{{$model->id}}" up-reveal="false"
+                class="dropdown-items"
                 href="{{route('reaction.react', ['model' => $model->modelName, 'id' => $model->id, 'reaction'=> $reaction])}}">
-                <img src="{{asset('/images/reactions/' . $reaction . '.png')}}" class="image-cover h-8 w-8" />
+                <img src="{{asset('/images/reactions/' . $reaction . '.png')}}" class="image-cover h-8 w-8 hover:bg-gray-300 p-1 rounded"
+                    title="{{$reaction}}" />
             </a>
             @endforeach
+
         </div>
     </div>
     @endcan
