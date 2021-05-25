6667db018fdc18147d5509940314c5f2e7dc20ae
Philippe Jadin <philippe.jadin@gmail.com>
2021-05-25 08:25:36 +0200

    add group list to anonymous user homepage

--- resources/views/dashboard/presentation.blade.php
+++ resources/views/dashboard/presentation.blade.php
@@ -2,29 +2,45 @@
 
 @section('content')
 
-    <div class="tab_content">
+<div class="tab_content">
 
-        <h1>
-            {{setting('name')}}
-        </h1>
+    <h1>
+        {{setting('name')}}
+    </h1>
 
-        <div class="mb-3">
-            @if (setting()->localized()->get('homepage_presentation'))
-            {!! setting()->localized()->get('homepage_presentation') !!}
-            @else
-            {!! setting()->get('homepage_presentation', trans('documentation.intro'))!!}
-            @endif
+    <div class="mb-3">
+        @if (setting()->localized()->get('homepage_presentation'))
+        {!! setting()->localized()->get('homepage_presentation') !!}
+        @else
+        {!! setting()->get('homepage_presentation', trans('documentation.intro'))!!}
+        @endif
+    </div>
+
+    <a up-modal=".dialog" class="btn btn-primary btn-lg mr-2"
+        href="{{ url('login') }}">{{ trans('messages.login') }}</a>
+
+    @can('create', App\User::class)
+    <a up-modal=".dialog" class="btn btn-secondary btn-lg" href="{{ url('register') }}">
+        {{ trans('messages.register') }}
+    </a>
+    @endcan
+
+
+    @if($groups)
+    <div class="groups">
+
+        {!! $groups->links() !!}
+        <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-4">
+            @foreach($groups as $group)
+            @include('groups.group')
+            @endforeach
         </div>
-
-        <a up-modal=".dialog" class="btn btn-primary btn-lg mr-2" href="{{ url('login') }}">{{ trans('messages.login') }}</a>
-        
-        @can('create', App\User::class)
-            <a up-modal=".dialog" class="btn btn-secondary btn-lg" href="{{ url('register') }}">
-                {{ trans('messages.register') }}
-            </a>
-        @endcan
-
+        {!! $groups->links() !!}
 
     </div>
+    @endif
+
+
+</div>
 
 @endsection
