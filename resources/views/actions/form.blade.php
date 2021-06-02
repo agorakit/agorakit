cf00473f4930a8eae38d169175b1decc2959ff48
Philippe Jadin <philippe.jadin@gmail.com>
2021-06-02 22:26:33 +0200

    remove date picker and use browser native implementation

--- resources/views/actions/form.blade.php
+++ resources/views/actions/form.blade.php
@@ -24,44 +24,28 @@
 
 <div class="form-group">
     {{trans('messages.start_date')}}
-    {!! Form::text('start_date', $action->start->format('Y-m-d') , ['class' => 'form-control datepicker']) !!}
+    {!! Form::date('start_date', $action->start->format('Y-m-d') , ['class' => 'form-control']) !!}
 </div>
 
 <div class="form-group">
     {{trans('messages.start_time')}}
-    {!! Form::text('start_time', $action->start->format('H:i') , ['class' => 'form-control', 'required']) !!}
+    {!! Form::time('start_time', $action->start->format('H:i') , ['class' => 'form-control', 'required']) !!}
 </div>
 
 <div class="form-group">
     {{trans('messages.stop_time')}}
     @if (isset($action->stop))
-        {!! Form::text('stop_time', $action->stop->format('H:i') , ['class' => 'form-control', 'required']) !!}
+        {!! Form::time('stop_time', $action->stop->format('H:i') , ['class' => 'form-control', 'required']) !!}
     @else
-        {!! Form::text('stop_time', null , ['class' => 'form-control']) !!}
+        {!! Form::time('stop_time', null , ['class' => 'form-control']) !!}
     @endif
 </div>
 
 <div class="form-group">
     {{trans('messages.stop_date')}}
     @if ((isset($action->stop)) && ($action->stop->format('Y-m-d') <> $action->start->format('Y-m-d')))
-        {!! Form::text('stop_date', $action->stop->format('Y-m-d') , ['class' => 'form-control datepicker']) !!}
+        {!! Form::date('stop_date', $action->stop->format('Y-m-d') , ['class' => 'form-control']) !!}
     @else
-        {!! Form::text('stop_date', null , ['class' => 'form-control datepicker']) !!}
+        {!! Form::date('stop_date', null , ['class' => 'form-control']) !!}
     @endif
 </div>
-
-
-
-@section('css')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker3.min.css" />
-@endsection
-
-@push('js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
-
-    <script>
-    $('.datepicker').datepicker({
-        format: 'yyyy-mm-dd'
-    });
-    </script>
-@endpush
