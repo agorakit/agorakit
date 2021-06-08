201519ec9ae798e8d538ed9db03d946b42b8de92
Philippe Jadin <philippe.jadin@gmail.com>
2021-06-08 21:49:29 +0200

    less visible show dropdown

--- resources/views/partials/preferences-show.blade.php
+++ resources/views/partials/preferences-show.blade.php
@@ -1,23 +1,34 @@
 @auth
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle shadow" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="far fa-eye"></i>
-      </button>
-      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-        <a  href="?set_preference=show&value=my" class="dropdown-item @if (Auth::user()->getPreference('show', 'my') == 'my') active @endif">  
-        {{__('Show my groups')}}
-        </a>
-        
-        <a  href="?set_preference=show&value=all" class="dropdown-item @if (Auth::user()->getPreference('show', 'my') == 'all') active @endif">
-        {{__('Show all groups')}}
-        </a>
-        
-        @if (Auth::user()->isAdmin())
-        <a  href="?set_preference=show&value=admin" class="dropdown-item @if (Auth::user()->getPreference('show', 'my') == 'admin') active @endif">
-        {{__('Show all groups (admin overview)')}}
-        </a>
-        @endif
+<div class="dropdown">
+  <a class="dropdown-toggle cursor-pointer text-gray-700" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+    aria-haspopup="true" aria-expanded="false">
+    @if (Auth::user()->getPreference('show', 'my') == 'my')
+    {{__('Show my groups')}}
+    @elseif (Auth::user()->getPreference('show', 'my') == 'all')
+    {{__('Show all groups')}}
+    @elseif (Auth::user()->getPreference('show', 'my') == 'admin')
+    {{__('Show all groups (admin overview)')}}
+    @endif
 
-      </div>
-    </div>
+  </a>
+  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+    <a up-follow href="?set_preference=show&value=my"
+      class="dropdown-item @if (Auth::user()->getPreference('show', 'my') == 'my') active @endif">
+      {{__('Show my groups')}}
+    </a>
+
+    <a up-follow href="?set_preference=show&value=all"
+      class="dropdown-item @if (Auth::user()->getPreference('show', 'my') == 'all') active @endif">
+      {{__('Show all groups')}}
+    </a>
+
+    @if (Auth::user()->isAdmin())
+    <a up-follow href="?set_preference=show&value=admin"
+      class="dropdown-item @if (Auth::user()->getPreference('show', 'my') == 'admin') active @endif">
+      {{__('Show all groups (admin overview)')}}
+    </a>
+    @endif
+
+  </div>
+</div>
 @endauth
