c07a9bace2c4e4cf8e95c0f5a7eb8ef65e7bb492
Philippe Jadin <philippe@123piano.com>
2023-09-15 11:28:54 +0200

    responsive columns

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -6,13 +6,12 @@
             setting()->localized()->get('homepage_presentation_for_members'))
         <div class="overflow-y-auto border items-start  p-3 mb-4 rounded bg-gray-200 text-sm sm:text-base flex"
             style="max-height: 10em;">
-            <i class="fa fa-info-circle text-4xl mr-3 text-gray-500 hidden sm:block"></i>
-            <div class="text-secondary flex-grow -mb-3 mt-1">
+            <div class="">
 
                 @if (Auth::user()->isAdmin())
                     <a class="btn btn-secondary float-right" href="{{ url('/admin/settings') }}" >
-                        <i class="fa fa-cog"></i>
-                        <span class="hidden sm:inline">{{ __('Edit') }}</span>
+                        <i class="fa fa-cog me-2"></i>
+                        {{ __('Edit') }}
                     </a>
                 @endif
 
@@ -29,8 +28,7 @@
 
     @if (Auth::user()->groups()->count() > 0)
         <div class="row">
-
-            <div class="col">
+            <div class="col-lg-8 col-12">
                 @if ($discussions->count() > 0)
                     @if ($discussions)
                         @include('discussions.list', ['discussions' => $discussions])
@@ -39,11 +37,10 @@
             </div>
 
             @if ($actions->count() > 0)
-                <div class="col">
+                <div class="col-lg-4 col-12">
                     @include('actions.list', ['actions' => $actions])
                 </div>
             @endif
-
         </div>
     @else
         <h1>
