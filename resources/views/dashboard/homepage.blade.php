ba2da0df31cc77a6cb532fb3357341f372104182
Philippe Jadin <philippe@123piano.com>
2023-09-20 21:16:47 +0200

    move info message lower on the page

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -2,43 +2,39 @@
 
 @section('content')
 
-    @if (setting()->get('homepage_presentation_for_members') ||
-            setting()->localized()->get('homepage_presentation_for_members'))
-        <div class="overflow-y-auto border items-start  p-3 mb-4 rounded bg-gray-200 text-sm sm:text-base flex"
-            style="max-height: 10em;">
-            <div class="">
-
-                @if (Auth::user()->isAdmin())
-                    <a class="btn btn-secondary float-end" href="{{ url('/admin/settings') }}">
-                        <i class="fa fa-cog me-2"></i>
-                        {{ __('Edit') }}
-                    </a>
-                @endif
-
-                @if (setting()->localized()->get('homepage_presentation_for_members'))
-                    {!! setting()->localized()->get('homepage_presentation_for_members') !!}
-                @else
-                    {!! setting()->get('homepage_presentation_for_members') !!}
-                @endif
-
-            </div>
-
-        </div>
-    @endif
-
     @auth
         <div>
             @include('partials.preferences-show')
         </div>
     @endauth
-    
+
     @if (Auth::user()->groups()->count() > 0)
         <div class="row">
             <div class="col-lg-8 col-12">
-                @if ($discussions->count() > 0)
-                    @if ($discussions)
-                        @include('discussions.list', ['discussions' => $discussions])
+                <div>
+                    @if ($discussions->count() > 0)
+                        @if ($discussions)
+                            @include('discussions.list', ['discussions' => $discussions])
+                        @endif
                     @endif
+                </div>
+                @if (setting()->get('homepage_presentation_for_members') ||
+                        setting()->localized()->get('homepage_presentation_for_members'))
+                    <div class="alert alert-info" style="max-height: 10em; overflow-y: scroll;">
+
+                        @if (Auth::user()->isAdmin())
+                            <a class="btn btn-secondary float-end" href="{{ url('/admin/settings') }}">
+                                <i class="fa fa-cog me-2"></i>
+                                {{ __('Edit') }}
+                            </a>
+                        @endif
+
+                        @if (setting()->localized()->get('homepage_presentation_for_members'))
+                            {!! setting()->localized()->get('homepage_presentation_for_members') !!}
+                        @else
+                            {!! setting()->get('homepage_presentation_for_members') !!}
+                        @endif
+                    </div>
                 @endif
             </div>
 
