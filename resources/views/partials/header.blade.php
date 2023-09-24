5a9bbc4575f7bb0e351c15d0aa34bf525d97d137
Philippe Jadin <philippe@123piano.com>
2023-09-25 00:57:09 +0200

    defer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,7 +17,7 @@
 
 <!-- jquery -->
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"
-    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous" defer></script>
 
 <!-- additional css from modules -->
 
