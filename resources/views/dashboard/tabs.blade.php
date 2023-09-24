dce7a69cee9516afefa45ee04fe0767dd9ce2196
Philippe Jadin <philippe@123piano.com>
2023-09-24 23:19:45 +0200

    auth or not

--- resources/views/dashboard/tabs.blade.php
+++ resources/views/dashboard/tabs.blade.php
@@ -1,8 +1,7 @@
 <ul class="nav nav-pills nav-fill mb-5 mt-2">
     <li class="nav-item">
         <a href="{{ route('index') }}" class="nav-link @if (isset($tab) && $tab == 'homepage') active @endif">
-            <i class="fa fa-home"></i> <span
-                class="ms-2 d-none d-lg-inline">{{ trans('messages.presentation') }}</span>
+            <i class="fa fa-home"></i> <span class="ms-2 d-none d-lg-inline">{{ trans('messages.presentation') }}</span>
         </a>
     </li>
 
@@ -23,8 +22,7 @@
     <li class="nav-item">
         <a href="{{ action('ActionController@index') }}"
             class="nav-link @if (isset($tab) && $tab == 'actions') active @endif">
-            <i class="fa fa-calendar"></i> <span
-                class="ms-2 d-none d-lg-inline ">{{ trans('messages.agenda') }}</span>
+            <i class="fa fa-calendar"></i> <span class="ms-2 d-none d-lg-inline ">{{ trans('messages.agenda') }}</span>
         </a>
     </li>
 
@@ -34,23 +32,19 @@
                 <i class="fa fa-files-o"></i> <span class="ms-2 d-none d-lg-inline ">{{ trans('messages.files') }}</span>
             </a>
         </li>
-    @endauth
 
-    @auth
         <li class="nav-item">
             <a href="{{ action('UserController@index') }}"
                 class="nav-link @if (isset($tab) && $tab == 'users') active @endif">
-                <i class="fa fa-users"></i> <span
-                    class="ms-2 d-none d-lg-inline ">{{ trans('messages.members') }}</span>
+                <i class="fa fa-users"></i> <span class="ms-2 d-none d-lg-inline ">{{ trans('messages.members') }}</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="{{ action('MapController@index') }}" class="nav-link @if (isset($tab) && $tab == 'map') active @endif">
+                <i class="fa fa-map-marker"></i> <span class="ms-2 d-none d-lg-inline ">{{ trans('messages.map') }}</span>
             </a>
         </li>
     @endauth
-
-    <li class="nav-item">
-        <a href="{{ action('MapController@index') }}" class="nav-link @if (isset($tab) && $tab == 'map') active @endif">
-            <i class="fa fa-map-marker"></i> <span
-                class="ms-2 d-none d-lg-inline ">{{ trans('messages.map') }}</span>
-        </a>
-    </li>
 
 </ul>
