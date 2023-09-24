0ee6a1b566065903753284d914d5ae1769bf5cf2
Philippe Jadin <philippe@123piano.com>
2023-09-25 00:56:01 +0200

    defer js

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -28,23 +28,23 @@
 
 <!-- unpoly -->
 
-<script src="https://cdn.jsdelivr.net/npm/unpoly@3.3.0/unpoly.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/unpoly@3.3.0/unpoly-migrate.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/unpoly@3.3.0/unpoly.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/unpoly@3.3.0/unpoly-migrate.min.js" defer></script>
 <link href="https://cdn.jsdelivr.net/npm/unpoly@3.3.0/unpoly.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/unpoly@3.3.0/unpoly-bootstrap5.min.css" rel="stylesheet">
 
 
 
-<script src="{{ asset('js/fullcalendar.js') }}"></script>
-<script src="{{ asset('packages/summernote/summernote-lite.min.js') }}"></script>
-<script src="{{ asset('js/datatables.min.js') }}"></script>
-<script src="{{ asset('js/select2.min.js') }}"></script>
-<script src="{{ asset('js/compilers.js?v=' . filemtime(public_path('js/compilers.js'))) }}"></script>
+<script src="{{ asset('js/fullcalendar.js') }}" defer></script>
+<script src="{{ asset('packages/summernote/summernote-lite.min.js') }}" defer></script>
+<script src="{{ asset('js/datatables.min.js') }}" defer></script>
+<script src="{{ asset('js/select2.min.js') }}" defer></script>
+<script src="{{ asset('js/compilers.js?v=' . filemtime(public_path('js/compilers.js'))) }}" defer></script>
 
 
 <!-- tabler.io -->
 
-<script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta19/dist/js/tabler.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta19/dist/js/tabler.min.js" defer></script>
 <link href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta19/dist/css/tabler.min.css" rel="stylesheet">
 
 <link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
