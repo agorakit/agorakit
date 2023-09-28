b8a7133967292d2e45b8ac5b280205a4884aa0df
Philippe Jadin <philippe@123piano.com>
2023-09-28 22:11:01 +0200

    faster

--- public/js/compilers.js
+++ public/js/compilers.js
@@ -12,7 +12,7 @@ up.link.config.followSelectors.push('a[href]')
 //up.viewport.config.revealTop = true
 
 
-up.network.config.badResponseTime = 200
+up.network.config.badResponseTime = 300
 
 
 // Here we put custom compilers for unpoly
