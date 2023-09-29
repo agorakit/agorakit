eb248a9ab34a4d5645c96ade30308da58e148f92
Philippe Jadin <philippe.jadin@gmail.com>
2023-09-29 23:28:22 +0200

    first pass at better search ui

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,84 +13,129 @@ class SearchController extends Controller
         $this->middleware(['verified', 'auth']);
     }
 
+    /**
+     * Parameters in the request : 
+     * ?query : search terms
+     * ?type : discussions / groups / actions / files / users
+     * ?scope : my / all / admin
+     * ?order : recent / old / big / small
+     * 
+     */
     public function index(Request $request)
     {
+        // validate user input
+
         if ($request->get('query')) {
             $query = $request->get('query');
+        }
 
-            if (Auth::user()->getPreference('show', 'my') == 'admin') {
-                // build a list of groups the user has access to
-                if (Auth::user()->isAdmin()) { // super admin sees everything
-                    $allowed_groups = Group::get()
-                        ->pluck('id');
-                }
+        $type = '';
+        if ($request->get('type')) {
+            $type = $request->get('type');
+        }
+
+        if (!in_array($type, ['discussions', 'files', 'actions', 'groups', 'users', 'comments'])) {
+            $type = 'discussions';
+        }
+
+        $scope = '';
+        if ($request->get('scope')) {
+            $scope = $request->get('scope');
+        }
+
+        if (!in_array($scope, ['my', 'all', 'admin'])) {
+            $scope = Auth::user()->getPreference('show', 'my');
+        }
+
+        if ($scope == 'admin') {
+            // build a list of groups the user has access to
+            if (Auth::user()->isAdmin()) { // super admin sees everything
+                $allowed_groups = Group::get()
+                    ->pluck('id');
+            }
+        }
+
+        if ($scope == 'all') {
+            $allowed_groups = Group::public()
+                ->get()
+                ->pluck('id')
+                ->merge(Auth::user()->groups()->pluck('groups.id'));
+        }
+
+        if ($scope == 'my') {
+            $allowed_groups = Auth::user()->groups()->pluck('groups.id');
+        }
+
+        $groups = collect();
+        $users = collect();
+        $discussions = collect();
+        $actions = collect();
+        $files = collect();
+        $comments = collect();
+
+        // perform search
+        if (isset($query)) {
+
+            if ($type == 'groups') {
+                $groups = Group::whereIn('id', $allowed_groups)
+                    ->search($query)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(20);
             }
 
-            if (Auth::user()->getPreference('show', 'my') == 'all') {
-                $allowed_groups = Group::public()
-                    ->get()
-                    ->pluck('id')
-                    ->merge(Auth::user()->groups()->pluck('groups.id'));
+
+            if ($type == 'users') {
+                $users = \App\User::with('groups')
+                    ->search($query)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(20);
             }
 
-            if (Auth::user()->getPreference('show', 'my') == 'my') {
-                $allowed_groups = Auth::user()->groups()->pluck('groups.id');
+            if ($type == 'discussions') {
+                $discussions = \App\Discussion::whereIn('group_id', $allowed_groups)
+                    ->with('group')
+                    ->search($query)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(20);
             }
 
 
-            $groups = Group::whereIn('id', $allowed_groups)
-            ->search($query)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20, ['*'], 'groups')->withQueryString();
-            
+            if ($type == 'actions') {
+                $actions = \App\Action::whereIn('group_id', $allowed_groups)
+                    ->with('group')
+                    ->search($query)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(20, ['*'], 'actions');
+            }
 
-          
+            if ($type == 'files') {
+                $files = \App\File::whereIn('group_id', $allowed_groups)
+                    ->with('group')
+                    ->search($query)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(20);
+            }
 
-            $users = \App\User::with('groups')
-            ->search($query)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20, ['*'], 'users');
-            
+            if ($type == 'comments') {
+                $comments = \App\Comment::with('discussion', 'discussion.group')
+                    ->whereHas('discussion', function ($q) use ($allowed_groups) {
+                        $q->whereIn('group_id', $allowed_groups);
+                    })
+                    ->search($query)
+                    ->orderBy('updated_at', 'desc')
+                    ->paginate(20);
+            }
+        }
 
-            $discussions = \App\Discussion::whereIn('group_id', $allowed_groups)
-            ->with('group')
-            ->search($query)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20, ['*'], 'discussions');
-            
-            
-
-            $actions = \App\Action::whereIn('group_id', $allowed_groups)
-            ->with('group')
-            ->search($query)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20, ['*'], 'actions');
-
-            $files = \App\File::whereIn('group_id', $allowed_groups)
-            ->with('group')
-            ->search($query)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20, ['*'], 'files');
-
-            $comments = \App\Comment::with('discussion', 'discussion.group')
-            ->whereHas('discussion', function ($q) use ($allowed_groups) {
-                $q->whereIn('group_id', $allowed_groups);
-            })
-            ->search($query)
-            ->orderBy('updated_at', 'desc')
-            ->paginate(20, ['*'], 'comments');
-
-
-            return view('search.results')
+        return view('search.results')
             ->with('groups', $groups)
             ->with('users', $users)
             ->with('discussions', $discussions)
             ->with('files', $files)
             ->with('comments', $comments)
             ->with('actions', $actions)
-            ->with('query', $query);
-        } else {
-            return view('search.search');
-        }
+            ->with('query', $query)
+            ->with('scope', $scope)
+            ->with('type', $type);
     }
 }
--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -3,120 +3,100 @@
 @section('content')
 
     <div class="d-flex flex-wrap gap-2 justify-content-between">
-        <h1><i class="fa fa-search"></i> {{ trans('messages.your_search_for') }} <strong>"{{ $query }}"</strong></h1>
+        <h1><i class="fa fa-search"></i> {{ trans('messages.your_search_for') }} <strong>"{{ $query ?? '' }}"</strong></h1>
         @include('partials.preferences-show')
     </div>
 
+    <form action="{{ url('search') }}" method="get" role="search">
 
-    @include ('search.form')
-
-    <div class="search_results">
-        <ul class="nav nav-tabs mt-4">
-            @if ($groups->count() > 0)
-                <li class="nav-item">
-                    <button class="nav-link {{ $groups->class }}" href="#groups" aria-controls="groups" role="tab"
-                        data-bs-toggle="tab">{{ trans('messages.groups') }} <span
-                            class="badge badge-secondary ms-2">{{ $groups->count() }}</span></button>
-                </li>
-            @endif
-
-            @if ($discussions->count() > 0)
-                <li class="nav-item">
-                    <button class="nav-link {{ $discussions->class }}" href="#discussions" aria-controls="discussions"
-                        role="tab" data-bs-toggle="tab">{{ trans('messages.discussions') }} <span
-                            class="badge badge-secondary ms-2">{{ $discussions->count() }}</span></button>
-                </li>
-            @endif
-
-            @if ($actions->count() > 0)
-                <li class="nav-item">
-                    <button class="nav-link {{ $actions->class }}" data-bs-target="#actions" aria-controls="actions"
-                        role="tab" data-bs-toggle="tab">{{ trans('messages.actions') }} <span
-                            class="badge badge-secondary ms-2">{{ $actions->count() }}</span></button>
-                </li>
-            @endif
-
-            @if ($users->count() > 0)
-                <li class="nav-item">
-                    <button class="nav-link {{ $users->class }}" data-bs-target="#users" aria-controls="users" role="tab"
-                        data-bs-toggle="tab">{{ trans('messages.users') }} <span
-                            class="badge badge-secondary ms-2">{{ $users->count() }}</span></button>
-                </li>
-            @endif
-
-            @if ($comments->count() > 0)
-                <li class="nav-item">
-                    <button class="nav-link {{ $comments->class }}" data-bs-target="#comments" aria-controls="users"
-                        role="tab" data-bs-toggle="tab">{{ trans('messages.comments') }} <span
-                            class="badge badge-secondary ms-2"> {{ $comments->count() }}</span></button>
-                </li>
-            @endif
-
-            @if ($files->count() > 0)
-                <li class="nav-item">
-                    <button class="nav-link {{ $files->class }}" data-bs-target="#files" aria-controls="users" role="tab"
-                        data-bs-toggle="tab">{{ trans('messages.files') }} <span
-                            class="badge badge-secondary ms-2">{{ $files->count() }}</span></button>
-                </li>
-            @endif
-
-        </ul>
-
-        <div class="tab-content mt-4">
-
-            @if ($groups->count() > 0)
-                <div role="tabpanel" class="tab-pane {{ $groups->class }}" id="groups">
-                    @foreach ($groups as $group)
-                        @include('groups.group-list')
-                    @endforeach
-                    {!! $groups->render() !!}
-                </div>
-            @endif
-
-            @if ($discussions->count() > 0)
-                <div role="tabpanel" class="tab-pane {{ $discussions->class }}" id="discussions">
-                    @foreach ($discussions as $discussion)
-                        @include('discussions.discussion')
-                    @endforeach
-                    {!! $discussions->render() !!}
-                </div>
-            @endif
-
-            @if ($actions->count() > 0)
-                <div role="tabpanel" class="tab-pane {{ $actions->class }}" id="actions">
-                    @include('actions.list', ['actions' => $actions])
-                    {!! $actions->render() !!}
-                </div>
-            @endif
-
-            @if ($users->count() > 0)
-                <div role="tabpanel" class="tab-pane {{ $users->class }}" id="users">
-                    @foreach ($users as $user)
-                        @include('users.user-list')
-                    @endforeach
-                    {!! $users->render() !!}
-                </div>
-            @endif
-
-            @if ($comments->count() > 0)
-                <div role="tabpanel" class="tab-pane {{ $comments->class }}" id="comments">
-                    @foreach ($comments as $comment)
-                        @include('comments.comment')
-                    @endforeach
-                    {!! $comments->render() !!}
-                </div>
-            @endif
-
-            @if ($files->count() > 0)
-                <div role="tabpanel" class="tab-pane {{ $files->class }}" id="files">
-                    @foreach ($files as $file)
-                        @include('files.file')
-                    @endforeach
-                    {!! $files->render() !!}
-                </div>
-            @endif
-
+        <div class="form-group">
+            <input aria-label="Search" class="form-control" name="query" placeholder="{{ trans('messages.search') }}"
+                type="search" value="{{ request()->get('query') }}" />
         </div>
-    </div>
-    </div>
+
+        <div class="form-group">
+            <select class="form-control" name="type" required="required">
+                <option @if ($type == 'discussions') selected @endif value="discussions">
+                    {{ trans('messages.discussions') }}</option>
+                <option @if ($type == 'comments') selected @endif value="comments">{{ trans('messages.comments') }}
+                </option>
+                <option @if ($type == 'actions') selected @endif value="actions">{{ trans('messages.actions') }}
+                </option>
+                <option @if ($type == 'users') selected @endif value="users">{{ trans('messages.users') }}
+                </option>
+                <option @if ($type == 'files') selected @endif value="files">{{ trans('messages.files') }}
+                </option>
+                <option @if ($type == 'groups') selected @endif value="groups">{{ trans('messages.groups') }}
+                </option>
+
+            </select>
+        </div>
+
+        <div class="form-group">
+            <select class="form-control" name="scope">
+                <option @if ($scope == 'my') selected @endif value="my"> MY groups</option>
+                <option @if ($scope == 'all') selected @endif value="all"> All public groups</option>
+                @if (Auth::user()->isAdmin())
+                    <option @if ($scope == 'admin') selected @endif value="admin"> All groups (admin overview)
+                    </option>
+                @endif
+
+                @foreach (Auth::user()->groups as $group)
+                    <option @if ($scope == $group->id) selected @endif value="{{ $group->id }}">
+                        {{ $group->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+
+
+        <div class="form-group">
+            <button class="form-control btn btn-primary" type="submit">{{ trans('messages.search') }}</button>
+        </div>
+
+
+    </form>
+
+
+
+    @if ($groups->count() > 0)
+        @foreach ($groups as $group)
+            @include('groups.group-list')
+        @endforeach
+        {!! $groups->render() !!}
+    @endif
+
+    @if ($discussions->count() > 0)
+        @foreach ($discussions as $discussion)
+            @include('discussions.discussion')
+        @endforeach
+        {!! $discussions->render() !!}
+    @endif
+
+    @if ($actions->count() > 0)
+        @include('actions.list', ['actions' => $actions])
+        {!! $actions->render() !!}
+    @endif
+
+    @if ($users->count() > 0)
+        @foreach ($users as $user)
+            @include('users.user-list')
+        @endforeach
+        {!! $users->render() !!}
+    @endif
+
+    @if ($comments->count() > 0)
+        @foreach ($comments as $comment)
+            @include('comments.comment')
+        @endforeach
+        {!! $comments->render() !!}
+    @endif
+
+    @if ($files->count() > 0)
+        @foreach ($files as $file)
+            @include('files.file')
+        @endforeach
+        {!! $files->render() !!}
+    @endif
+
 @endsection
