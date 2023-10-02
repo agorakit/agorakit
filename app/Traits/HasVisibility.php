aaae27ed0b4df237b6381db577221427c54c2fb0
Philippe Jadin <philippe@123piano.com>
2023-10-02 15:47:19 +0200

    wip

---
+++ app/Traits/HasVisibility.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Traits;
+
+use App\Scopes\HasVisibilityScope;
+use Illuminate\Database\Eloquent\Builder;
+
+interface ContentVisibility
+{
+    const INHERITED = 0;
+    const PUBLIC = 10;
+    const PRIVATE = -10;
+}
+
+trait HasVisibility
+{
+
+    public function getVisibility(): int
+    {
+        return $this->{'visibility'};
+    }
+
+    public function setVisibility(int $visibility)
+    {
+        $this->{'visibility'} = $visibility;
+        return $this;
+    }
+
+    public function isPublic(): bool
+    {
+        return $this->getVisibility() === ContentVisibility::PUBLIC;
+    }
+
+    public function isPrivate(): bool
+    {
+        return $this->getVisibility() === ContentVisibility::PRIVATE;
+    }
+
+    public function makePublic(): self
+    {
+        return $this->setVisibility(ContentVisibility::PUBLIC);
+    }
+
+    public function makePrivate(): self
+    {
+        return $this->setVisibility(ContentVisibility::PRIVATE);
+    }
+
+
+    public function scopePublic(Builder $query): void
+    {
+        $query->where('visibility', '=', ContentVisibility::PUBLIC);
+    }
+
+    public function scopePrivate(Builder $query): void
+    {
+        $query->where('visibility', '=', ContentVisibility::PRIVATE);
+    }
+ 
+
+}
---
+++ database/migrations/2023_10_02_153943_add_visibility_to_actions.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Traits\ContentVisibility;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddVisibilityToActions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table->integer('visibility')->default(ContentVisibility::INHERITED);
+            $table->index('visibility');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('actions', function (Blueprint $table) {
+            $table->dropColumn('visibility');
+        });
+    }
+}
