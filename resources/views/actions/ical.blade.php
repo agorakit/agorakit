5c26cb6ddc111f986384af5b4777fe3ddb8632d6
Philippe Jadin <philippe@123piano.com>
2023-10-02 15:55:26 +0200

    ical links fix

--- resources/views/actions/ical.blade.php
+++ resources/views/actions/ical.blade.php
@@ -1,16 +1,16 @@
-<div class="my-5 sm:flex justify-content-between">
+<div class="my-5 d-flex gap-2 flex-wrap">
   
   <div class="mb-4">
-  <a class="btn btn-secondary" href="{{action('GroupIcalController@index', $group)}}">
-    <i class="far fa-calendar-alt"></i>
+  <a class="btn btn-outline-secondary" href="{{action('GroupIcalController@index', $group)}}"  target="_blank">
+    <i class="far fa-calendar-alt me-2"></i>
     {{trans('action.public_ical_group_link')}}
   </a>
   </div>
 
   @auth
   <div>
-    <a class="btn btn-secondary" href="{{URL::signedRoute('users.ical', ['user' => Auth::user()])}}">
-      <i class="far fa-calendar-alt"></i>
+    <a class="btn btn-outline-secondary" href="{{URL::signedRoute('users.ical', ['user' => Auth::user()])}}" target="_blank">
+      <i class="far fa-calendar-alt me-2"></i>
       {{trans('action.personal_ical_group_link')}}
     </a>
     </div>
