c28213a2c3c3dbbbc405c745a26d8c7493d6c1a2
Philippe Jadin <philippe@123piano.com>
2023-10-06 20:48:35 +0200

    quick fix correct add button

--- resources/views/actions/index.blade.php
+++ resources/views/actions/index.blade.php
@@ -1,7 +1,6 @@
 @extends('group')
 
 @section('content')
-
     @auth
         <div class="flex mb-2 justify-content-between">
 
@@ -20,27 +19,24 @@
         </div>
     @endauth
 
-    @can('create-action', $group)
-        <div class="mb-4">
-            <a class="btn btn-primary" href="{{ route('groups.actions.create', $group) }}">
-                {{ trans('action.create_one_button') }}
-            </a>
-        </div>
-    @endcan
+
 
     @if ($type == 'grid')
+        @can('create-action', $group)
+            <div class="mb-4">
+                <a class="btn btn-primary" href="{{ route('groups.actions.create', $group) }}">
+                    {{ trans('action.create_one_button') }}
+                </a>
+            </div>
+        @endcan
         <div class="js-calendar" id="calendar" data-json="{{ route('groups.actions.index.json', $group) }}"
             data-locale="{{ App::getLocale() }}" data-create-url="{{ route('groups.actions.create', $group) }}"></div>
     @endif
 
     @if ($type == 'list')
-        @if ($actions->count() > 0)
-            @include('actions.list', ['actions' => $actions])
-            {{ $actions->render() }}
-
-        @endif
+        @include('actions.list', ['actions' => $actions])
+        {{ $actions->render() }}
     @endif
 
-     @include('actions.ical')
-
+    @include('actions.ical')
 @endsection
