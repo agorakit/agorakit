3062f91654e0bbcdbee91f54e24f519939a13ae9
Philippe Jadin <philippe.jadin@gmail.com>
2023-10-11 20:19:37 +0200

    action name can be any lenght

--- app/Action.php
+++ app/Action.php
@@ -27,7 +27,7 @@ class Action extends Model
 
 
     protected $rules = [
-        'name'     => 'required|min:5',
+        'name'     => 'required',
         'user_id'  => 'required|exists:users,id',
         'group_id' => 'required|exists:groups,id',
         'start'    => 'required',
