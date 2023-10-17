0154e53c42d8265f13624297865a95c80abe7c05
Philippe Jadin <philippe.jadin@gmail.com>
2023-10-17 20:28:01 +0200

    action covers on the left on desktop

--- resources/views/actions/action.blade.php
+++ resources/views/actions/action.blade.php
@@ -3,7 +3,7 @@
     <div class="d-flex mb-2 align-items-center ">
         <div class="me-md-3 me-1">
             <div class="action-date">
-                <div class="fw-bold">{{ $action->start->format('d') }}</div>
+                <div class="fw-bold -mb-2">{{ $action->start->format('d') }}</div>
                 <div class="">{{ $action->start->isoFormat('MMM') }}</div>
             </div>
         </div>
@@ -21,59 +21,65 @@
         @include('actions.dropdown')
     </div>
 
-    @if ($action->hasCover())
-        <div class="mb-2">
-            <img class="rounded" src="{{ route('actions.cover.large', $action) }}" />
-        </div>
-    @endif
+    <div class="row">
 
-    <div class="mb-2">
-        {{ summary($action->body) }}
-    </div>
-
-    <div class="text-meta text-truncate mb-2">
-        <div>
-            @if ($action->isPublic())
-                <i class="fa fa-eye me-1"></i>{{ trans('messages.public') }}
-            @elseif ($action->isPrivate())
-                <i class="fa fa-eye me-1"></i>{{ trans('messages.private') }}
-            @endif
-        </div>
-        <div>
-            <i class="fa fa-clock-o me-1"></i> {{ $action->start->format('H:i') }} -
-            {{ $action->stop->format('H:i') }}
-        </div>
-
-        @if ($action->location)
-            <div>
-                <i class="fa fa-map-marker me-1"></i> {{ $action->location }}
+        @if ($action->hasCover())
+            <div class="col-12 col-sm-5 col-md-4 mb-2 order-sm-2">
+                <img class="rounded" src="{{ route('actions.cover.large', $action) }}" />
             </div>
         @endif
 
-        @if ($action->attending->count() > 0)
-            <div>
-                <i class="fa fa-users me-1"></i> {{ $action->attending->count() }}
-                {{ trans('participants') }}
+        <div class="col-12 col-sm-7 col-md-8">
+            <div class="mb-2">
+                {{ summary($action->body) }}
             </div>
-        @endif
-    </div>
 
+            <div class="text-meta text-truncate mb-2">
+                <div>
+                    @if ($action->isPublic())
+                        <i class="fa fa-eye me-1"></i>{{ trans('messages.public') }}
+                    @elseif ($action->isPrivate())
+                        <i class="fa fa-eye me-1"></i>{{ trans('messages.private') }}
+                    @endif
+                </div>
+                <div>
+                    <i class="fa fa-clock-o me-1"></i> {{ $action->start->format('H:i') }} -
+                    {{ $action->stop->format('H:i') }}
+                </div>
 
+                @if ($action->location)
+                    <div>
+                        <i class="fa fa-map-marker me-1"></i> {{ $action->location }}
+                    </div>
+                @endif
 
-    <div id="participate-{{ $action->id }}">
-        @if ($action->attending->count() > 0)
-            <div class="avatar-list avatar-list-stacked">
-                @foreach ($action->attending as $user)
-                    @include('users.avatar')
-                @endforeach
+                @if ($action->attending->count() > 0)
+                    <div>
+                        <i class="fa fa-users me-1"></i> {{ $action->attending->count() }}
+                        {{ trans('participants') }}
+                    </div>
+                @endif
             </div>
-        @endif
 
-        @can('participate', $action)
-            <div class="mt-2">
-                @include('participation.dropdown')
+
+
+            <div id="participate-{{ $action->id }}">
+                @if ($action->attending->count() > 0)
+                    <div class="avatar-list avatar-list-stacked">
+                        @foreach ($action->attending as $user)
+                            @include('users.avatar')
+                        @endforeach
+                    </div>
+                @endif
+
+                @can('participate', $action)
+                    <div class="mt-2">
+                        @include('participation.dropdown')
+                    </div>
+                @endcan
             </div>
-        @endcan
+        </div>
+
     </div>
 
 </div>
