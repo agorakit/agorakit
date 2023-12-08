4d7ffa7fa7cc472eb65d2885788227641cbe14d1
Philippe Jadin <philippe@123piano.com>
2023-12-08 10:46:15 +0100

    map work again

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -18,7 +18,7 @@
 
 {{-- jquery --}}
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"
-    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous" defer></script>
+    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 
 {{-- full calendar --}}
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js' defer></script>
--- resources/views/partials/map.blade.php
+++ resources/views/partials/map.blade.php
@@ -1,8 +1,6 @@
-<link href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" rel="stylesheet"
-    integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
-<!-- Make sure you put this AFTER Leaflet's CSS -->
-<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-    crossorigin=""></script>
+<link href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" rel="stylesheet" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
 <style>
     #mapid {
@@ -20,101 +18,98 @@
     }
 </style>
 
-<script>
-    // load all our points using geojson
-    // taken from https://medium.com/@maptastik/loading-external-geojson-a-nother-way-to-do-it-with-jquery-c72ae3b41c01
-    var points = $.ajax({
-        url: "{{ $json }}",
-        dataType: "json",
-        success: console.log("points data successfully loaded."),
-        error: function(xhr) {
-            alert(xhr.statusText)
-        }
-    })
-
-
-
-
-
-    // When loading is complete :
-    $.when(points).done(function() {
-        var map = L.map('mapid').setView([51.505, -0.09], 13);
-
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-
-
-
-
-        // from https://stackoverflow.com/questions/23567203/leaflet-changing-marker-color
-
-        const userIcon = L.divIcon({
-            className: "my-custom-pin",
-            iconAnchor: [0, 24],
-            labelAnchor: [-6, 0],
-            popupAnchor: [0, -36],
-            html: `<span style="background-color: #1e60c9" class="marker"></span>`
+<script defer>
+    $(document).ready(function() {
+        // load all our points using geojson
+        // taken from https://medium.com/@maptastik/loading-external-geojson-a-nother-way-to-do-it-with-jquery-c72ae3b41c01
+        var points = $.ajax({
+            url: "{{ $json }}",
+            dataType: "json",
+            success: console.log("points data successfully loaded."),
+            error: function(xhr) {
+                alert(xhr.statusText)
+            }
         })
 
-        const actionIcon = L.divIcon({
-            className: "my-custom-pin",
-            iconAnchor: [0, 24],
-            labelAnchor: [-6, 0],
-            popupAnchor: [0, -36],
-            html: `<span style="background-color: #871ec9" class="marker"></span>`
-        })
 
-        const groupIcon = L.divIcon({
-            className: "my-custom-pin",
-            iconAnchor: [0, 24],
-            labelAnchor: [-6, 0],
-            popupAnchor: [0, -36],
-            html: `<span style="background-color: #8dc91e" class="marker"></span>`
-        })
+        // When loading is complete :
+        $.when(points).done(function() {
+            var map = L.map('mapid').setView([51.505, -0.09], 13);
 
-        // from https://gist.github.com/geog4046instructor/80ee78db60862ede74eacba220809b64
-        // replace Leaflet's default blue marker with a custom icon
-        function createCustomIcon(feature, latlng) {
-            if (feature.properties.type == 'user') {
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+
+
+
+
+            // from https://stackoverflow.com/questions/23567203/leaflet-changing-marker-color
+
+            const userIcon = L.divIcon({
+                className: "my-custom-pin",
+                iconAnchor: [0, 24],
+                labelAnchor: [-6, 0],
+                popupAnchor: [0, -36],
+                html: `<span style="background-color: #1e60c9" class="marker"></span>`
+            })
+
+            const actionIcon = L.divIcon({
+                className: "my-custom-pin",
+                iconAnchor: [0, 24],
+                labelAnchor: [-6, 0],
+                popupAnchor: [0, -36],
+                html: `<span style="background-color: #871ec9" class="marker"></span>`
+            })
+
+            const groupIcon = L.divIcon({
+                className: "my-custom-pin",
+                iconAnchor: [0, 24],
+                labelAnchor: [-6, 0],
+                popupAnchor: [0, -36],
+                html: `<span style="background-color: #8dc91e" class="marker"></span>`
+            })
+
+            // from https://gist.github.com/geog4046instructor/80ee78db60862ede74eacba220809b64
+            // replace Leaflet's default blue marker with a custom icon
+            function createCustomIcon(feature, latlng) {
+                if (feature.properties.type == 'user') {
+                    return L.marker(latlng, {
+                        icon: userIcon
+                    })
+                }
+
+                if (feature.properties.type == 'action') {
+                    return L.marker(latlng, {
+                        icon: actionIcon
+                    })
+                }
+
+                if (feature.properties.type == 'group') {
+                    return L.marker(latlng, {
+                        icon: groupIcon
+                    })
+                }
+
                 return L.marker(latlng, {
                     icon: userIcon
                 })
             }
 
-            if (feature.properties.type == 'action') {
-                return L.marker(latlng, {
-                    icon: actionIcon
+            // Add requested external GeoJSON to map
+            var allPoints = L.geoJSON(points.responseJSON, {
+                    pointToLayer: createCustomIcon
                 })
-            }
-
-            if (feature.properties.type == 'group') {
-                return L.marker(latlng, {
-                    icon: groupIcon
-                })
-            }
-
-            return L.marker(latlng, {
-                icon: userIcon
-            })
-        }
+                .bindPopup(function(layer) {
+                    return "<strong>" + layer.feature.properties.title + '</strong><br/>' + layer.feature
+                        .properties.description;
+                }).addTo(map);
 
 
 
-        // Add requested external GeoJSON to map
-        var allPoints = L.geoJSON(points.responseJSON, {
-                pointToLayer: createCustomIcon
-            })
-            .bindPopup(function(layer) {
-                return "<strong>" + layer.feature.properties.title + '</strong><br/>' + layer.feature
-                    .properties.description;
-            }).addTo(map);
+            map.fitBounds(allPoints.getBounds());
 
-
-
-        map.fitBounds(allPoints.getBounds());
-
+        });
 
     });
 </script>
