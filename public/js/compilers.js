828b6aadfb03ad5d9085cd99ac45ea9962b7994b
Philippe Jadin <philippe.jadin@gmail.com>
2024-08-21 18:18:23 +0200

    inside unpoly compilers, use loadcss from https://github.com/filamentgroup/loadCSS

--- public/js/compilers.js
+++ public/js/compilers.js
@@ -48,34 +48,96 @@ function loadJS(url) {
 	}
 }
 
-// CSS loader. This one is refreshed on each request, maybe there is a better way.
-let cssLoaded = {};
-function loadCSS(url) {
 
-	function createScriptTag(url) {
-		let linkTag = document.createElement("link")
-		linkTag.setAttribute("rel", "stylesheet")
-		linkTag.setAttribute("type", "text/css")
-		linkTag.href = url
-		return linkTag
+/*! loadCSS. [c]2020 Filament Group, Inc. MIT License */
+(function (w) {
+	"use strict";
+	/* exported loadCSS */
+	var loadCSS = function (href, before, media, attributes) {
+		// Arguments explained:
+		// `href` [REQUIRED] is the URL for your CSS file.
+		// `before` [OPTIONAL] is the element the script should use as a reference for injecting our stylesheet <link> before
+		// By default, loadCSS attempts to inject the link after the last stylesheet or script in the DOM. However, you might desire a more specific location in your document.
+		// `media` [OPTIONAL] is the media type or query of the stylesheet. By default it will be 'all'
+		// `attributes` [OPTIONAL] is the Object of attribute name/attribute value pairs to set on the stylesheet's DOM Element.
+		var doc = w.document;
+		var ss = doc.createElement("link");
+		var ref;
+		if (before) {
+			ref = before;
+		}
+		else {
+			var refs = (doc.body || doc.getElementsByTagName("head")[0]).childNodes;
+			ref = refs[refs.length - 1];
+		}
+
+		var sheets = doc.styleSheets;
+		// Set any of the provided attributes to the stylesheet DOM Element.
+		if (attributes) {
+			for (var attributeName in attributes) {
+				if (attributes.hasOwnProperty(attributeName)) {
+					ss.setAttribute(attributeName, attributes[attributeName]);
+				}
+			}
+		}
+		ss.rel = "stylesheet";
+		ss.href = href;
+		// temporarily set media to something inapplicable to ensure it'll fetch without blocking render
+		ss.media = "only x";
+
+		// wait until body is defined before injecting link. This ensures a non-blocking load in IE11.
+		function ready(cb) {
+			if (doc.body) {
+				return cb();
+			}
+			setTimeout(function () {
+				ready(cb);
+			});
+		}
+		// Inject link
+		// Note: the ternary preserves the existing behavior of "before" argument, but we could choose to change the argument to "after" in a later release and standardize on ref.nextSibling for all refs
+		// Note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
+		ready(function () {
+			ref.parentNode.insertBefore(ss, (before ? ref : ref.nextSibling));
+		});
+		// A method (exposed on return object for external use) that mimics onload by polling document.styleSheets until it includes the new sheet.
+		var onloadcssdefined = function (cb) {
+			var resolvedHref = ss.href;
+			var i = sheets.length;
+			while (i--) {
+				if (sheets[i].href === resolvedHref) {
+					return cb();
+				}
+			}
+			setTimeout(function () {
+				onloadcssdefined(cb);
+			});
+		};
+
+		function loadCB() {
+			if (ss.addEventListener) {
+				ss.removeEventListener("load", loadCB);
+			}
+			ss.media = media || "all";
+		}
+
+		// once loaded, set link's media back to `all` so that the stylesheet applies once it loads
+		if (ss.addEventListener) {
+			ss.addEventListener("load", loadCB);
+		}
+		ss.onloadcssdefined = onloadcssdefined;
+		onloadcssdefined(loadCB);
+		return ss;
+	};
+	// commonjs
+	if (typeof exports !== "undefined") {
+		exports.loadCSS = loadCSS;
 	}
-	//
-	//	let cachedPromise = cssLoaded[url]
-	//	if (cachedPromise) {
-	//		console.log('css already loaded : ' + url)
-	//		return cachedPromise
-	//	} else {
-	let promise = new Promise((resolve, reject) => {
-		let linkTag = createScriptTag(url)
-		linkTag.addEventListener('load', resolve)
-		linkTag.addEventListener('error', reject)
-		document.body.appendChild(linkTag)
-	})
-	//console.log('css loaded : ' + promise)
-	cssLoaded[url] = promise
-	return promise
-	//	}
-}
+	else {
+		w.loadCSS = loadCSS;
+	}
+}(typeof global !== "undefined" ? global : this));
+
 
 function loadJquery() {
 	return loadJS('https://code.jquery.com/jquery-3.7.0.min.js')
