ab21859afd6b60aa3563221c4f1b905aa82980ba
Philippe Jadin <philippe@123piano.com>
2024-08-23 11:49:05 +0200

    avatar is also generated in controller, not too much logic in view

--- app/Http/Controllers/UserCoverController.php
+++ app/Http/Controllers/UserCoverController.php
@@ -19,7 +19,7 @@ class UserCoverController extends Controller
     public function show(User $user, $size = 'medium')
     {
         if ($user->hasCover()) {
-                
+
             if ($size == 'small') {
                 $cachedImage = Image::cache(function ($img) use ($user) {
                     return $img->make($user->getCover())->fit(64, 64);
@@ -43,8 +43,21 @@ class UserCoverController extends Controller
                 return $cachedImage->response();
             }
         } else {
-            // serve alternative
-            return redirect(url('/images/avatar.svg'));
+            // serve alternative using initials
+            $avatar = Avatar::create($user->name)->setBorder(0, false)
+                ->setShape('square');
+
+            if ($size == 'small') {
+                $avatar->setDimension(48)->setFontSize(20);
+            }
+
+            if ($size == 'medium') {
+                $avatar->setDimension(400)->setFontSize(200);
+            }
+            if ($size == 'large') {
+                $avatar->setDimension(800)->setFontSize(400);
+            }
+            return $avatar->getImageObject()->response();
         }
     }
 }
--- resources/views/users/avatar.blade.php
+++ resources/views/users/avatar.blade.php
@@ -1,9 +1,5 @@
 @if (isset($user))
-    @if ($user->hasCover())
-        <img class="avatar rounded-circle" src="{{ route('users.cover', [$user, 'small']) }}" title="{{ $user->name }}" style="max-width:40px;" width="40" height="40" />
-    @else
-        <img class="avatar rounded-circle" src="{{ Avatar::create($user->name)->setShape('square')->toBase64() }}" title="{{ $user->name }}" />
-    @endif
+    <img class="avatar rounded-circle" src="{{ route('users.cover', [$user, 'small']) }}" title="{{ $user->name }}" style="max-width:40px;" width="40" height="40" />
 @else
     <span class="avatar rounded-circle" title="Unknown user">?</span>
 @endif
