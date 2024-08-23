1463475e6832bbb6d14e235fbf17883558a455c0
Philippe Jadin <philippe@123piano.com>
2024-08-23 11:30:12 +0200

    return of the initials in avatars (quickfix)

--- resources/views/users/avatar.blade.php
+++ resources/views/users/avatar.blade.php
@@ -1,5 +1,9 @@
 @if (isset($user))
-    <img class="avatar rounded-circle" src="{{ route('users.cover', [$user, 'small']) }}" title="{{ $user->name }}" style="max-width:40px;" width="40" height="40" />
+    @if ($user->hasCover())
+        <img class="avatar rounded-circle" src="{{ route('users.cover', [$user, 'small']) }}" title="{{ $user->name }}" style="max-width:40px;" width="40" height="40" />
+    @else
+        <img class="avatar rounded-circle" src="{{ Avatar::create($user->name)->setShape('square')->toBase64() }}" title="{{ $user->name }}" />
+    @endif
 @else
     <span class="avatar rounded-circle" title="Unknown user">?</span>
 @endif
