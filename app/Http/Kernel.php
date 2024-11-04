d02cb0487c0321a357bc301a94b0c3407aa0dc8e
Philippe Jadin <philippe.jadin@gmail.com>
2024-11-04 21:14:43 +0100

    L9 correct trust proxy handling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \App\Http\Middleware\TrustProxies::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
