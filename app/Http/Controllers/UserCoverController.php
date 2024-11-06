87054f772e676827b62d351930f21fb69b2b4353
Philippe Jadin <philippe.jadin@gmail.com>
2024-11-06 20:21:18 +0100

    upgrade to intervention image 3, major pita

--- app/Http/Controllers/UserCoverController.php
+++ app/Http/Controllers/UserCoverController.php
@@ -3,48 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use File;
-use Image;
-use Redirect;
 use Avatar;
-use Storage;
 
 class UserCoverController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('cache.headers:public,max-age=300;etag');
+        //$this->middleware('cache.headers:public,max-age=300;etag');
     }
 
-    public function show(User $user, $size = 'medium')
+    public function show(User $user, $size = 'original')
     {
+
         if ($user->hasCover()) {
-
-            if ($size == 'small') {
-                $cachedImage = Image::cache(function ($img) use ($user) {
-                    return $img->make($user->getCover())->fit(64, 64);
-                }, 5, true);
-
-                return $cachedImage->response();
-            }
-            if ($size == 'medium') {
-                $cachedImage = Image::cache(function ($img) use ($user) {
-                    return $img->make($user->getCover())->widen(400);
-                }, 5, true);
-
-                return $cachedImage->response();
-            }
-
-            if ($size == 'large') {
-                $cachedImage = Image::cache(function ($img) use ($user) {
-                    return $img->make($user->getCover())->widen(800);
-                }, 5, true);
-
-                return $cachedImage->response();
-            }
+            return $user->getCover($size);
         } else {
+
             // serve alternative using initials
-            $avatar = Avatar::create($user->name)->setBorder(0, false)
+            $avatar = Avatar::create($user->name)->setBorder(0, '#aaa')
                 ->setShape('square');
 
             if ($size == 'small') {
@@ -57,7 +33,7 @@ class UserCoverController extends Controller
             if ($size == 'large') {
                 $avatar->setDimension(800)->setFontSize(400);
             }
-            return $avatar->getImageObject()->response();
+            return response((string) $avatar->getImageObject());
         }
     }
 }
