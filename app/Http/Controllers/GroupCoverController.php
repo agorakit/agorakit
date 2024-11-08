b641c23c1d1b724b510fca47860606a87edbe2c5
Philippe Jadin <philippe@123piano.com>
2024-11-08 12:06:26 +0100

    simplify group cover controller

--- app/Http/Controllers/GroupCoverController.php
+++ app/Http/Controllers/GroupCoverController.php
@@ -18,61 +18,10 @@ class GroupCoverController extends Controller
         $this->middleware('cache.headers:private,max-age=300;etag');
     }
 
-    public function show(Group $group, $size = 'original')
+    public function show(Group $group, $size = 'medium')
     {
         if ($group->hasCover()) {
             return $group->getCover($size);
-        } else {
-            abort(404);
-        }
-    }
-
-    public function small(Group $group)
-    {
-        $this->authorize('view', $group);
-
-        $path = storage_path() . '/app/groups/' . $group->id . '/cover.jpg';
-
-        if (File::exists($path)) {
-            $cachedImage = Image::cache(function ($img) use ($path) {
-                return $img->make($path)->fit(128, 128);
-            }, 5, true);
-
-            return $cachedImage->response();
-        } else {
-            abort(404);
-        }
-    }
-
-    public function medium(Group $group)
-    {
-        $this->authorize('view', $group);
-
-        $path = storage_path() . '/app/groups/' . $group->id . '/cover.jpg';
-
-        if (File::exists($path)) {
-            $cachedImage = Image::cache(function ($img) use ($path) {
-                return $img->make($path)->fit(400, 300);
-            }, 5, true);
-
-            return $cachedImage->response();
-        } else {
-            abort(404);
-        }
-    }
-
-    public function large(Group $group)
-    {
-        $this->authorize('view', $group);
-
-        $path = storage_path() . '/app/groups/' . $group->id . '/cover.jpg';
-
-        if (File::exists($path)) {
-            $cachedImage = Image::cache(function ($img) use ($path) {
-                return $img->make($path)->widen(800);
-            }, 5, true);
-
-            return $cachedImage->response();
         } else {
             abort(404);
         }
