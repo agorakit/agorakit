3afd1d8a632665f9e8b29e7e566c90c4e10efa04
Odile Bénassy <zerline@fluidinfo.fr>
2025-01-07 20:47:43 +0100

    All IMG tags require an 'alt' attribute.

--- resources/views/components/reactions.blade.php
+++ resources/views/components/reactions.blade.php
@@ -7,13 +7,13 @@
         @if (Auth::check() && Auth::user()->is($reaction->user))
         <a up-target="#model-{{$model->id}}" class="m-0 p-0 h-5 w-5 mr-1"
             href="{{route('reaction.unreact', ['model' => $model->modelName, 'id' => $model->id])}}">
-            <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-5 w-5"
+            <img alt="{{ $reaction->type }}" src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-5 w-5"
                 title="{{ $reaction->user->name }} - click to remove" />
         </a>
         @else
 
             @if ($reaction->user)
-            <img src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-5 w-5 mr-1"
+            <img alt="{{ $reaction->type }}" src="{{asset('/images/reactions/' . $reaction->type . '.png')}}" class="image-cover h-5 w-5 mr-1"
                 title="{{ $reaction->user->name }}" />
             @endif
 
@@ -25,7 +25,7 @@
         <div class="dropdown">
             <a class="cursor-pointer d-flex align-items-center bg-gray-s300 text-gray-700 rounded-lg text-xs opacity-25 hover:opacity-100"
                 type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img src="{{asset('/images/reactions/reaction.svg')}}" class="image-cover h-5 w-5"
+                <img alt="" src="{{asset('/images/reactions/reaction.svg')}}" class="image-cover h-5 w-5"
                     title="Add a reaction" />
 
             </a>
@@ -35,7 +35,7 @@
                 @foreach (setting()->getArray('reactions') as $reaction)
                 <a up-target="#model-{{$model->id}}" class="no-underline"
                     href="{{route('reaction.react', ['model' => $model->modelName, 'id' => $model->id, 'reaction'=> $reaction])}}">
-                    <img src="{{asset('/images/reactions/' . $reaction . '.png')}}"
+                    <img alt="{{ $reaction }}" src="{{asset('/images/reactions/' . $reaction . '.png')}}"
                         class="image-cover h-8 w-8 hover:bg-gray-300 p-1 rounded" title="{{$reaction}}" />
                 </a>
                 @endforeach
