b4846b921f1e5398fd78dc2d991deec4ba05d3d5
Zerline <zerline@fluidinfo.fr>
2025-02-19 16:01:39 +0100

    Add labels and small help.

--- resources/views/groups/form.blade.php
+++ resources/views/groups/form.blade.php
@@ -22,10 +22,22 @@
         {{trans('messages.location_privacy_and_help')}}
     </div>
     {!! Form::text('location', null, ['class' => 'form-control']) !!}
+    {!! Form::label('city', trans('messages.city') . ':') !!}
     {!! Form::text('city', null, ['class' => 'form-control']) !!}
+    {!! Form::label('county', trans('messages.county') . ':') !!}
+    <div class="small-help">
+        <i class="fas fa-info-circle"></i>
+        {{trans('messages.county_help')}}
+    </div>
     {!! Form::text('county', null, ['class' => 'form-control']) !!}
+    {!! Form::label('country', trans('messages.country') . ':') !!}
     {!! Form::text('country', null, ['class' => 'form-control']) !!}
+    {!! Form::label('location_name', trans('messages.location_name') . ':') !!}
+    <div class="small-help">
+        <i class="fas fa-info-circle"></i>
+        {{trans('messages.location_name_help')}}
     {!! Form::text('location_name', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
 
