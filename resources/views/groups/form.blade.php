ee07f825069dafebce223992d4aac91dda232840
Zerline <zerline@fluidinfo.fr>
2025-02-21 13:15:45 +0100

    Make a sub-template.

--- resources/views/groups/form.blade.php
+++ resources/views/groups/form.blade.php
@@ -16,30 +16,8 @@
 </div>
 
 <div class="form-group">
-    {!! Form::label('location', trans('messages.location') . ':') !!}
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{trans('messages.location_privacy_and_help')}}
-    </div>
-    {!! Form::text('street_address', null, ['class' => 'form-control']) !!}
-    {!! Form::label('city', trans('messages.city') . ':') !!}
-    {!! Form::text('city', null, ['class' => 'form-control']) !!}
-    {!! Form::label('county', trans('messages.county') . ':') !!}
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{trans('messages.county_help')}}
-    </div>
-    {!! Form::text('county', null, ['class' => 'form-control']) !!}
-    {!! Form::label('country', trans('messages.country') . ':') !!}
-    {!! Form::text('country', null, ['class' => 'form-control']) !!}
-    {!! Form::label('location_name', trans('messages.location_name') . ':') !!}
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{trans('messages.location_name_help')}}
-    {!! Form::text('location_name', null, ['class' => 'form-control']) !!}
-    </div>
+@include('traits.location_input')
 </div>
-
 
 <div class="form-group">
     {!! Form::label('group_type', trans('group.type')) !!}
