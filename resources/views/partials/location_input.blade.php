85b16416fa90c783c8fba46ae821f2372480db9e
Odile Bénassy <zerline@fluidinfo.fr>
2025-03-08 08:51:22 +0100

    A level of abstraction is required in order to have a partial form for location input.

--- resources/views/partials/location_input.blade.php
+++ resources/views/partials/location_input.blade.php
@@ -2,21 +2,21 @@
     <div class="small-help">
         <i class="fas fa-info-circle"></i>
         {{trans('messages.location_name_help')}}
-    {!! Form::text('location[name]', $group->location_data['name'], ['class' => 'form-control']) !!}
+    {!! Form::text('location[name]', $model->location_data['name'], ['class' => 'form-control']) !!}
     </div>
     <div class="small-help">
         <i class="fas fa-info-circle"></i>
         {{trans('messages.location_privacy_and_help')}}
     </div>
     {!! Form::label('location[street]', trans('Street Address') . ':') !!}
-    {!! Form::text('location[street]', $group->location_data['street'], ['class' => 'form-control', 'size' => '100']) !!}
+    {!! Form::text('location[street]', $model->location_data['street'], ['class' => 'form-control', 'size' => '100']) !!}
     {!! Form::label('location[city]', trans('City') . ':') !!}
-    {!! Form::text('location[city]', $group->location_data['city'], ['class' => 'form-control', 'size' => '30']) !!}
+    {!! Form::text('location[city]', $model->location_data['city'], ['class' => 'form-control', 'size' => '30']) !!}
     {!! Form::label('location[county]', trans('County') . ':') !!}
     <div class="small-help">
         <i class="fas fa-info-circle"></i>
         {{trans('messages.county_help')}}
     </div>
-    {!! Form::text('location[county]', $group->location_data['county'], ['class' => 'form-control', 'size' => '30']) !!}
+    {!! Form::text('location[county]', $model->location_data['county'], ['class' => 'form-control', 'size' => '30']) !!}
     {!! Form::label('location[country]', trans('Country') . ':') !!}
-    {!! Form::text('location[country]', $group->location_data['country'], ['class' => 'form-control', 'size' => '30']) !!}
+    {!! Form::text('location[country]', $model->location_data['country'], ['class' => 'form-control', 'size' => '30']) !!}
