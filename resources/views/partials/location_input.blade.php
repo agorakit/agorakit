2b6a229c0ce2a99684680fb17e244b720591f395
Zerline <zerline@fluidinfo.fr>
2025-03-09 09:38:43 +0100

    Change location[name] position in the form.

--- resources/views/partials/location_input.blade.php
+++ resources/views/partials/location_input.blade.php
@@ -1,9 +1,3 @@
-    {!! Form::label('location[name]', trans('Location Name') . ':') !!}
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{trans('messages.location_name_help')}}
-    {!! Form::text('location[name]', $model->location_data['name'], ['class' => 'form-control']) !!}
-    </div>
     <div class="small-help">
         <i class="fas fa-info-circle"></i>
         {{trans('messages.location_privacy_and_help')}}
@@ -20,3 +14,9 @@
     {!! Form::text('location[county]', $model->location_data['county'], ['class' => 'form-control', 'size' => '30']) !!}
     {!! Form::label('location[country]', trans('Country') . ':') !!}
     {!! Form::text('location[country]', $model->location_data['country'], ['class' => 'form-control', 'size' => '30']) !!}
+    {!! Form::label('location[name]', trans('Location Name') . ':') !!}
+    <div class="small-help">
+        <i class="fas fa-info-circle"></i>
+        {{trans('messages.location_name_help')}}
+    {!! Form::text('location[name]', $model->location_data['name'], ['class' => 'form-control']) !!}
+    </div>
