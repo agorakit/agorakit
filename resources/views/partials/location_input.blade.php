cb4d6096dd6fe537f0043ebdd0bbaf94677e2153
Philippe Jadin <philippe.jadin@gmail.com>
2025-04-04 11:00:21 +0200

    Location fieldset UI (#590)
    Grouping all location fields in a fieldset, adding some margins and
    translations

--- resources/views/partials/location_input.blade.php
+++ resources/views/partials/location_input.blade.php
@@ -1,22 +1,25 @@
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{ trans('messages.location_privacy_and_help') }}
-    </div>
-    {!! Form::label('location[street]', trans('Street Address') . ':') !!}
-    {!! Form::text('location[street]', $model->location->street, ['class' => 'form-control', 'size' => '100']) !!}
-    {!! Form::label('location[city]', trans('City') . ':') !!}
-    {!! Form::text('location[city]', $model->location->city, ['class' => 'form-control', 'size' => '30']) !!}
-    {!! Form::label('location[county]', trans('County') . ':') !!}
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{ trans('messages.county_help') }}
-    </div>
-    {!! Form::text('location[county]', $model->location->county, ['class' => 'form-control', 'size' => '30']) !!}
-    {!! Form::label('location[country]', trans('Country') . ':') !!}
-    {!! Form::text('location[country]', $model->location->country, ['class' => 'form-control', 'size' => '30']) !!}
-    {!! Form::label('location[name]', trans('Location Name') . ':') !!}
-    <div class="small-help">
-        <i class="fas fa-info-circle"></i>
-        {{ trans('messages.location_name_help') }}
-        {!! Form::text('location[name]', $model->location->name, ['class' => 'form-control']) !!}
-    </div>
+    <fieldset class="form-fieldset">
+      <label class="form-label h3">{{trans('messages.location')}}</label>
+        <div class="small-help mb-3">
+            <i class="fas fa-info-circle"></i>
+            {{ trans('messages.location_privacy_and_help') }}
+        </div>
+        {!! Form::label('location[street]', trans('Street Address') . ':') !!}
+        {!! Form::text('location[street]', $model->location->street, ['class' => 'form-control mb-3', 'size' => '100']) !!}
+        {!! Form::label('location[city]', trans('City') . ':') !!}
+        {!! Form::text('location[city]', $model->location->city, ['class' => 'form-control mb-3', 'size' => '30']) !!}
+        {!! Form::label('location[county]', trans('County') . ':') !!}
+        <div class="small-help">
+            <i class="fas fa-info-circle"></i>
+            {{ trans('messages.county_help') }}
+        </div>
+        {!! Form::text('location[county]', $model->location->county, ['class' => 'form-control mb-3', 'size' => '30']) !!}
+        {!! Form::label('location[country]', trans('Country') . ':') !!}
+        {!! Form::text('location[country]', $model->location->country, ['class' => 'form-control mb-3', 'size' => '30']) !!}
+        {!! Form::label('location[name]', trans('Location Name') . ':') !!}
+        <div class="small-help">
+            <i class="fas fa-info-circle"></i>
+            {{ trans('messages.location_name_help') }}
+            {!! Form::text('location[name]', $model->location->name, ['class' => 'form-control']) !!}
+        </div>
+    </fieldset>
