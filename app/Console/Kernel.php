ae7b367a1ea2f1cf221a3115286b038a0199f4a7
Zerline <zerline@fluidinfo.fr>
2025-06-02 11:20:42 +0200

    Think about cleaning up this temporary file storage.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\CheckMailbox::class,
         \App\Console\Commands\DeleteFiles::class,
         \App\Console\Commands\CleanupDatabase::class,
+        \App\Console\Commands\CleanupDisk::class,
         \App\Console\Commands\SendReminders::class,
         \App\Console\Commands\ExportGroup::class,
         \App\Console\Commands\ImportGroup::class,
@@ -42,5 +43,8 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('agorakit:cleanupdatabase')
         ->daily();
+
+        $schedule->command('agorakit:cleanupdisk')
+        ->hourly();
     }
 }
