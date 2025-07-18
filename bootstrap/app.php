a2adcc36acf0712c292625763e6c7539d430fc4c
Zerline <zerline@fluidinfo.fr>
2025-07-18 20:15:12 +0200

    s/App/Agorakit in bootstrap file.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -31,17 +31,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Agorakit\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    Agorakit\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    Agorakit\Exceptions\Handler::class
 );
 
 /*
