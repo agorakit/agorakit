7d0c4371b134d6dced29a2bc4c08054ef137205e
Lincoln Russell <lincoln@icrontic.com>
2025-10-13 09:08:55 -0400

    fix: Update auth routes

--- routes/web.php
+++ routes/web.php
@@ -66,20 +66,30 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('confirm/{token}', 'Auth\RegisterController@confirmEmail');
 
     Route::middleware(ProtectAgainstSpam::class)->group(function () {
+        // Login
+        Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+        Route::post('login', 'Auth\LoginController@login');
+        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+        // Registration
         Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-        Route::post('register', 'Auth\RegisterController@handleRegistrationForm');
+        Route::post('register', 'Auth\RegisterController@register');
 
-        Route::get('register/password', 'Auth\RegisterController@showPasswordForm');
-        Route::post('register/password', 'Auth\RegisterController@handlePasswordForm');
-        Route::get('forgotpassword', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('passwordrequest');
+        // Password
+        Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+        Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+        Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+        Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
 
-        Route::get('login/email', 'Auth\LoginByEmailController@showLoginByEmailForm')->name('loginbyemail');
-        Route::post('login/email', 'Auth\LoginByEmailController@sendLoginByEmail')->name('sendloginbyemail');
+        // Email Verification
+        Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+        Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify'); // v6.x
+        Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
     });
 
     Route::get('autologin', 'Auth\AutoLoginController@login')->name('autologin');
-
-
 
     // PWA manifest.json
     Route::get('manifest.webmanifest', 'PwaController@index')->name('pwa.index');
