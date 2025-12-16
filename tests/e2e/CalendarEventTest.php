29b404963e7a69babed45e5f0b0eab02d187d902
Philippe Jadin <philippe@123piano.com>
2025-12-16 15:54:45 +0100

    CalendarEvent also

--- tests/e2e/CalendarEventTest.php
+++ tests/e2e/CalendarEventTest.php
@@ -3,75 +3,14 @@
 namespace Tests;
 
 use App;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Mail;
+use Tests\BaseTest;
 
-class CalendarEventTest extends BrowserKitTestCase
+class CalendarEventTest extends BaseTest
 {
-    /******************* Why is it done this way ? ***************/
-
-    /*
-    I want my tests runs on a clean DB, and each test in the right order, like I would do by hand.
-    The first tests migrates the testing DB
-    Sounds simplier like this for me, I don't want the database being remigrated after each test.
-    Only after the whole suite has been run.
-
-    You need a agorakit_testing DB available for those tests to run.
-
-
-
-    */
-
-    /* tests starts here : let's setup the DB
-    */
-    public function testSetupItAll()
-    {
-        Artisan::call('migrate:fresh --env=testing');
-
-        $this->visit('/')
-            ->see('Agorakit');
-    }
-
-    /**
-     * Register our first user.
-     */
-    public function testUserRegistration()
-    {
-        Mail::fake();
-
-        $this->visit('/register')
-            ->type('Admin', 'name')
-            ->type('admin@agorakit.local', 'email')
-            ->press('Register')
-            ->type('123456789', 'password')
-            ->type('123456789', 'password_confirmation')
-            ->press('Register')
-            ->see('Agorakit');
-
-        $this->seeInDatabase('users', ['email' => 'admin@agorakit.local']);
-    }
-
-    public function testGroupCreation()
-    {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $user->confirmEmail();
-
-        $this->actingAs($user)
-            ->visit('groups/create')
-            ->see('Create a new group')
-            ->type('Event test group', 'name')
-            ->type('this is a test group for event test', 'body')
-            ->type('France', 'location[country]')
-            ->press('Create the group')
-            ->see('Event test group');
-    }
-
     public function testEventWithDefiniteEndCreation()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
@@ -89,14 +28,13 @@ class CalendarEventTest extends BrowserKitTestCase
                 'start' => '2026-01-01 12:00:00',
                 'stop' => '2026-01-03 09:00:00'
             ])
-            ->see(trans('messages.create_event'));
+            ->see(trans('messages.ressource_created_successfully'));
     }
 
     public function testEventWithWrongStopTimeCreation()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
@@ -115,9 +53,8 @@ class CalendarEventTest extends BrowserKitTestCase
 
     public function testEventWithoutStopDateCreation()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
@@ -134,14 +71,13 @@ class CalendarEventTest extends BrowserKitTestCase
                 'start' => '2026-01-01 12:00:00',
                 'stop' => '2026-01-01 17:00:00'
             ])
-            ->see(trans('messages.create_event'));
+            ->see(trans('messages.ressource_created_successfully'));
     }
 
     public function testEventWithoutStopTimeCreation()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
@@ -158,14 +94,13 @@ class CalendarEventTest extends BrowserKitTestCase
                 'start' => '2026-01-01 12:00:00',
                 'stop' => '2026-01-03 12:00:00'
             ])
-            ->see(trans('messages.create_event'));
+            ->see(trans('messages.ressource_created_successfully'));
     }
 
     public function testEventWithUnknownEnd()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
@@ -179,14 +114,13 @@ class CalendarEventTest extends BrowserKitTestCase
             ->seeInDatabase('calendar_events', [
                 'name' => 'Test event with unknown end'
             ])
-            ->see(trans('messages.create_event'));
+            ->see(trans('messages.ressource_created_successfully'));
     }
 
     public function testEventWithLocationName()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
@@ -201,27 +135,26 @@ class CalendarEventTest extends BrowserKitTestCase
             ->seeInDatabase('calendar_events', [
                 'name' => 'Test event with location name'
             ])
-            ->see(trans('messages.create_event'));
+            ->see(trans('messages.ressource_created_successfully'));
     }
 
     public function testEventUsingLocationName()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $group = App\Group::where('name', 'Event test group')->first();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
 
         $this->actingAs($user)
             ->visit('/groups/' . $group->id . '/calendarevents/create')
             ->see('Add an event')
-            ->type('Test event us-ing location name', 'name')
+            ->type('Test event using location name', 'name')
             ->type('this is a test event in the calendar', 'body')
             ->select('My PlaceBruxelles', 'listed_location')
             ->type('2026-02-02', 'start_date')
             ->type('12:00', 'start_time')
             ->press('Create')
             ->seeInDatabase('calendar_events', [
-                'name' => 'Test event us-ing location name'
+                'name' => 'Test event using location name'
             ])
-            ->see(trans('messages.create_event'));
+            ->see(trans('messages.ressource_created_successfully'));
     }
 }
--- tests/e2e/FileUploadTest.php
+++ tests/e2e/FileUploadTest.php
@@ -4,72 +4,15 @@ namespace Tests;
 
 use App;
 use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Mail;
+use Tests\BaseTest;
 
-class FileUploadTest extends BrowserKitTestCase
+class FileUploadTest extends BaseTest
 {
-    /* tests starts here : let's setup the DB
-    */
-    public function testSetupItAll()
-    {
-        Artisan::call('migrate:fresh --env=testing');
-
-        $this->visit('/')
-            ->see('Agorakit');
-    }
-
-    /**
-     * Register our first user.
-     */
-    public function testUserRegistration()
-    {
-        Mail::fake();
-
-        $this->visit('/register')
-            ->type('Admin', 'name')
-            ->type('admin@agorakit.local', 'email')
-            ->press('Register')
-            ->type('123456789', 'password')
-            ->type('123456789', 'password_confirmation')
-            ->press('Register')
-            ->see('Agorakit');
-
-        $this->seeInDatabase('users', ['email' => 'admin@agorakit.local']);
-    }
-
-    public function testGroupCreation()
-    {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-
-        $user->confirmEmail();
-
-        $this->actingAs($user)
-            ->visit('groups/create')
-            ->see('Create a new group')
-            ->type('File upload test group', 'name')
-            ->type('this is a test group', 'body')
-            ->press('Create the group')
-            ->see('File upload test group');
-    }
-
-    public function testFolderCreation()
-    {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-        $group = App\Group::where('name', 'File upload test group')->firstOrFail();
-
-        $this->actingAs($user)
-            ->visit('/groups/' . $group->id . '/files/createfolder')
-            ->see('Create a folder')
-            ->type('Test folder', 'name')
-            ->press('Create')
-            ->see('Test folder');
-    }
-
     public function testFileUpload()
     {
-        $user = App\User::where('email', 'admin@agorakit.local')->first();
-        $group = App\Group::where('name', 'File upload test group')->firstOrFail();
+        $user = $this->admin();
+        $group = $this->getTestGroup();
+
 
         $file = UploadedFile::fake()->image('avatar.jpg');
         $pathname = stream_get_meta_data($file->tempFile)['uri'];
